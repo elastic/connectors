8af97b83efc1f55598c95fe88222b1e6ea189068
Tarek Ziadé <tarek@ziade.org>
2022-07-20 17:33:19 +0200

    forgot to add thise

---
+++ connectors/sources/tests/support.py
@@ -0,0 +1,17 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+#
+from connectors.source import DataSourceConfiguration
+
+
+class FakeConnector:
+    def __init__(self, config):
+        self.configuration = DataSourceConfiguration(config)
+
+
+def create_source(klass):
+    config = klass.get_default_configuration()
+    conn = FakeConnector(config)
+    return klass(conn)
