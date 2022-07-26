877e340d779e0db8aacadd9b4a386beab4dd8f50
Tarek Ziade <tarek@ziade.org>
2022-07-26 13:05:24 +0200

    starting a massive cleanup

--- connectors/byoei.py
+++ connectors/byoei.py
@@ -18,6 +18,159 @@ from connectors.logger import logger, set_extra_logger
 from connectors.utils import iso_utc
 
 
+class Bulker:
+    """Send bulk operations in batches by consuming a queue."""
+
+    def __init__(self, client, queue):
+        self.client = client
+        self.queue = queue
+        self.bulk_time = 0
+        self.bulking = False
+        self.indexed_pages_count = 0
+
+    async def batch_bulk(self, operations):
+        # todo treat result to retry errors like in async_streaming_bulk
+        start = time.time()
+        try:
+            res = await self.client.bulk(operations=operations)
+        finally:
+            self.bulk_time += time.time() - start
+        self.indexed_pages_count += int(len(operations) / 2)
+        logger.info(f"Indexed {self.indexed_pages_count}.")
+        return res
+
+    async def run(self):
+        batch = []
+        ops = []
+        self.bulk_time = 0
+        self.bulking = True
+        docs_ended = downloads_ended = False
+        while True:
+            doc = await self.queue.get()
+            if doc == "END_DOCS":
+                docs_ended = True
+            if doc == "END_DOWNLOADS":
+                downloads_ended = True
+
+            if docs_ended and downloads_ended:
+                break
+
+            if doc in ("END_DOCS", "END_DOWNLOADS"):
+                continue
+
+            batch.append(
+                {doc["_op_type"]: {"_index": doc["_index"], "_id": doc["_id"]}}
+            )
+            if doc["_op_type"] == "update":
+                batch.append({"doc": doc["doc"], "doc_as_upsert": True})
+            else:
+                batch.append({"doc": doc["doc"]})
+
+            if len(batch) >= self.chunk_size * 2:
+                ops.append(asyncio.create_task(self._batch_bulk(list(batch))))
+                batch.clear()
+
+        if len(batch) > 0:
+            ops.append(asyncio.create_task(self._batch_bulk(list(batch))))
+            batch.clear()
+
+        await asyncio.gather(*ops)
+
+
+class Fetcher:
+    """Grab data and add them in the queue for the bulker"""
+
+    def __init__(self, client, queue, index):
+        self.client = client
+        self.queue = queue
+        self.bulk_time = 0
+        self.bulking = False
+        self.index = index
+        self._downloads = []
+        self.loop = asyncio.get_event_loop()
+
+    def _add_download(self, coro):
+        # XXX use a queue to throttle downloads
+        self._downloads.append(coro)
+
+    # XXX this can be defferred
+    async def get_attachments(self):
+        for download in self._downloads:
+            data = await download
+            if data is None:
+                continue
+            await self.queue.put(
+                {
+                    "_op_type": "update",
+                    "_index": self.index,
+                    "_id": data.pop("_id"),
+                    "doc": data,
+                    "doc_as_upsert": True,
+                }
+            )
+            await asyncio.sleep(0)
+
+        await self.queue.put("END_DOWNLOADS")
+
+    async def run(self, generator, existing_ids, existing_timestamps):
+        t1 = self.loop.create_task(
+            self.get_docs(generator, existing_ids, existing_timestamps)
+        )
+        t2 = self.loop.create_task(self.get_attachments())
+        await asyncio.gather(t1, t2)
+
+    async def get_docs(self, generator, existing_ids, existing_timestamps):
+
+        seen_ids = set()
+        async for doc in generator:
+            doc, lazy_download = doc
+            doc_id = doc["id"] = doc.pop("_id")
+            logger.debug(f"Looking at {doc_id}")
+            seen_ids.add(doc_id)
+
+            # If the doc has a timestamp, we can use it to see if it has
+            # been modified. This reduces the bulk size a *lot*
+            #
+            # Some backends do not know how to do this so it's optional.
+            # For them we update the docs in any case.
+            if "timestamp" in doc:
+                if existing_timestamps.get(doc_id, "") == doc["timestamp"]:
+                    logger.debug(f"Skipping {doc_id}")
+                    await lazy_download(doit=False)
+                    continue
+            else:
+                doc["timestamp"] = iso_utc()
+
+            if lazy_download is not None:
+                self._add_download(
+                    self.loop.create_task(
+                        lazy_download(doit=True, timestamp=doc["timestamp"])
+                    )
+                )
+
+            await self.queue.put(
+                {
+                    "_op_type": "update",
+                    "_index": self.index,
+                    "_id": doc_id,
+                    "doc": doc,
+                    "doc_as_upsert": True,
+                }
+            )
+            await asyncio.sleep(0)
+
+        # We delete any document that existed in Elasticsearch that was not
+        # returned by the backend.
+        for doc_id in existing_ids:
+            if doc_id in seen_ids:
+                continue
+            await self.queue.put(
+                {"_op_type": "delete", "_index": self.index, "_id": doc_id}
+            )
+
+        await self.queue.put("END_DOCS")
+
+
 class ElasticServer:
     def __init__(self, elastic_config):
         logger.debug(f"ElasticServer connecting to {elastic_config['host']}")
@@ -75,64 +228,11 @@ class ElasticServer:
         ):
             yield doc["_source"]
 
-    def _add_download(self, coro):
-        self._downloads.append(coro)
-
-    async def _bulk(self, queue):
-        batch = []
-        ops = []
-        self.bulk_time = 0
-        self.bulking = True
-
-        async def batch_bulk(operations):
-            # todo treat result to retry errors like in async_streaming_bulk
-            start = time.time()
-            try:
-                res = await self.client.bulk(operations=operations)
-            finally:
-                self.bulk_time += time.time() - start
-            self.indexed_pages_count += int(len(operations) / 2)
-            logger.info(f"Indexed {self.indexed_pages_count}.")
-            return res
-
-        docs_ended = downloads_ended = False
-        while True:
-            doc = await queue.get()
-            if doc == "END_DOCS":
-                docs_ended = True
-            if doc == "END_DOWNLOADS":
-                downloads_ended = True
-
-            if docs_ended and downloads_ended:
-                break
-
-            if doc in ("END_DOCS", "END_DOWNLOADS"):
-                continue
-
-            batch.append(
-                {doc["_op_type"]: {"_index": doc["_index"], "_id": doc["_id"]}}
-            )
-            if doc["_op_type"] == "update":
-                batch.append({"doc": doc["doc"], "doc_as_upsert": True})
-            else:
-                batch.append({"doc": doc["doc"]})
-
-            if len(batch) >= self.chunk_size * 2:
-                ops.append(asyncio.create_task(batch_bulk(list(batch))))
-                batch.clear()
-
-        if len(batch) > 0:
-            ops.append(asyncio.create_task(batch_bulk(list(batch))))
-            batch.clear()
-
-        await asyncio.gather(*ops)
-
     async def async_bulk(self, index, generator):
         start = time.time()
         existing_ids = set()
         existing_timestamps = {}
         stream = asyncio.Queue()
-        ops = []
 
         async for es_doc in self.get_existing_ids(index):
             existing_ids.add(es_doc["id"])
@@ -142,84 +242,20 @@ class ElasticServer:
             f"Found {len(existing_ids)} docs in {index} (duration "
             f"{int(time.time() - start)} seconds)"
         )
-        seen_ids = set()
 
         res = defaultdict(int)
 
-        async def get_docs():
-            async for doc in generator:
-                doc, lazy_download = doc
-                doc_id = doc["id"] = doc.pop("_id")
-                logger.debug(f"Looking at {doc_id}")
-                seen_ids.add(doc_id)
-
-                # If the doc has a timestamp, we can use it to see if it has
-                # been modified. This reduces the bulk size a *lot*
-                #
-                # Some backends do not know how to do this so it's optional.
-                # For them we update the docs in any case.
-                if "timestamp" in doc:
-                    if existing_timestamps.get(doc_id, "") == doc["timestamp"]:
-                        logger.debug(f"Skipping {doc_id}")
-                        await lazy_download(doit=False)
-                        continue
-                else:
-                    doc["timestamp"] = iso_utc()
-
-                if lazy_download is not None:
-                    self._add_download(
-                        self.loop.create_task(
-                            lazy_download(doit=True, timestamp=doc["timestamp"])
-                        )
-                    )
-
-                await stream.put(
-                    {
-                        "_op_type": "update",
-                        "_index": index,
-                        "_id": doc_id,
-                        "doc": doc,
-                        "doc_as_upsert": True,
-                    }
-                )
-                await asyncio.sleep(0)
-
-            # We delete any document that existed in Elasticsearch that was not
-            # returned by the backend.
-            for doc_id in existing_ids:
-                if doc_id in seen_ids:
-                    continue
-                await stream.put({"_op_type": "delete", "_index": index, "_id": doc_id})
-
-            await stream.put("END_DOCS")
-
-        # XXX this can be defferred
-        async def get_attachments():
-            for download in self._downloads:
-                data = await download
-                if data is None:
-                    continue
-                await stream.put(
-                    {
-                        "_op_type": "update",
-                        "_index": index,
-                        "_id": data.pop("_id"),
-                        "doc": data,
-                        "doc_as_upsert": True,
-                    }
-                )
-                await asyncio.sleep(0)
-
-            await stream.put("END_DOWNLOADS")
-
-        # start all fetchers
-        ops.append(asyncio.create_task(get_docs()))
-        ops.append(asyncio.create_task(get_attachments()))
+        # start the fetcher
+        fetcher = Fetcher(self.client, stream, index)
+        fetcher_task = asyncio.create_task(
+            fetcher.run(generator, existing_ids, existing_timestamps)
+        )
 
         # start the bulker
-        ops.append(asyncio.create_task(self._bulk(stream)))
+        bulker = Bulker(self.client, stream)
+        bulker_task = asyncio.create_task(bulker.run())
 
-        await asyncio.gather(*ops)
+        await asyncio.gather(fetcher_task, bulker_task)
 
         # we return a number for each operation type.
         res = {}
