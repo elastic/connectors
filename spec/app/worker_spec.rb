46527dee868ca61437044a1202f75266707ddcc3
Tarek Ziadé <tarek@ziade.org>
2022-08-02 13:47:34 +0200

    added doc, this is a complex test

--- spec/app/worker_spec.rb
+++ spec/app/worker_spec.rb
@@ -25,32 +25,48 @@ describe App::Worker do
 
   shared_examples 'handle_warnings' do |disable_warnings, stderr|
     it 'should display warnings from the Elasticsearch lib' do
+      # This call will raise a 401 when the lib checks the server, and that will create a warning
       allow_any_instance_of(Elasticsearch::Client).to receive(:elasticsearch_validation_request).and_raise(Elastic::Transport::Transport::Errors::Unauthorized)
 
-      config = {
-        :disable_warnings => disable_warnings,
-        :service_type => 'stub_connector',
-        :connector_id => '1',
-        :elasticsearch => {
-        :api_key => 'key',
-        :hosts => 'http://notreallyaserver'
-        }
-      }
+      config = if disable_warnings
+                 # This is the default behavior, so we don't pass
+                 # disable_warnings to make sure it is set to true by default
+                 {
+                   :service_type => 'stub_connector',
+                   :connector_id => '1',
+                   :elasticsearch => {
+                     :api_key => 'key',
+                     :hosts => 'http://notreallyaserver'
+                   }
+                 }
+               else
+                 {
+                   :disable_warnings => false,
+                   :service_type => 'stub_connector',
+                   :connector_id => '1',
+                   :elasticsearch => {
+                     :api_key => 'key',
+                     :hosts => 'http://notreallyaserver'
+                   }
+                 }
+               end
 
       App::Config = config # rubocop:disable Naming/ConstantName
+
+      # mocking the worker so start! returns immediatly after the initial checks
       allow(App::Worker).to receive(:start_heartbeat_task)
       allow(App::Worker).to receive(:start_polling_jobs)
 
+      # mocking some of the conversation between the worker and Elasticsearch
       allow(Core::ElasticConnectorActions).to receive(:ensure_connectors_index_exists)
       allow(Core::ElasticConnectorActions).to receive(:ensure_content_index_exists)
       expect(Core::ConnectorSettings).to receive(:fetch).and_return(FakeSettings.new)
-
       stub_request(:head, 'http://notreallyaserver:9200/.elastic-connectors-sync-jobs-v1')
         .to_return(status: 404, body: YAML.dump({}), headers: {})
-
       stub_request(:put, 'http://notreallyaserver:9200/.elastic-connectors-sync-jobs-v1')
         .to_return(status: 200, body: YAML.dump({}), headers: {})
 
+      # now let's see what is displated in stderr
       expect { described_class.start! }.to output(stderr).to_stderr
     end
   end
