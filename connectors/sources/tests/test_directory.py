54edaaba1abdc61686c9204d5c3dfb93339d1364
Tarek Ziadé <tarek@ziade.org>
2022-08-09 19:04:57 +0200

    more tests

---
+++ connectors/sources/tests/test_directory.py
@@ -0,0 +1,22 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+#
+import pytest
+from connectors.sources.directory import DirectoryDataSource, HERE
+from connectors.sources.tests.support import create_source, assert_basics
+
+
+@pytest.mark.asyncio
+async def test_basics():
+    await assert_basics(DirectoryDataSource, "directory", HERE)
+
+
+@pytest.mark.asyncio
+async def test_get_docs(patch_logger):
+    source = create_source(DirectoryDataSource)
+    num = 0
+    async for (doc, dl) in source.get_docs():
+        num += 1
+    assert num > 3
