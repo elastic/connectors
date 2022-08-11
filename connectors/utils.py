3ef083b7b3d93daabcccea820a98b507c86437af
Tarek Ziadé <tarek@ziade.org>
2022-08-12 01:11:22 +0200

    debug info

--- connectors/utils.py
+++ connectors/utils.py
@@ -27,7 +27,11 @@ class ESClient:
                 raise KeyError("You can't use basic auth and Api Key at the same time")
             auth = config["user"], config["password"]
             options["basic_auth"] = auth
+            logger.debug(
+                f"Connecting using Basic Auth (user: {config['user']}, password: {config['password'][:3]}...)"
+            )
         elif "api_key" in config:
+            logger.debug(f"Connecting with an Api Key ({config['api_key'][:5]}...)")
             options["api_key"] = config["api_key"]
 
         if config.get("ssl", False):
