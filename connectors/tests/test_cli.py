ba16d73595cd5703b223df5078f84f52dd5ed531
Tarek Ziadé <tarek@ziade.org>
2022-09-01 16:20:54 -0700

    test coverage for SIGTERM

--- connectors/tests/test_cli.py
+++ connectors/tests/test_cli.py
@@ -3,6 +3,10 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
+import asyncio
+import os
+import signal
+
 from connectors.cli import main
 from connectors import __version__
 
@@ -11,3 +15,11 @@ def test_main(catch_stdout):
     assert main(["--version"]) == 0
     catch_stdout.seek(0)
     assert catch_stdout.read().strip() == __version__
+
+
+def test_main_and_kill(catch_stdout, patch_logger):
+    async def kill():
+        os.kill(os.getpid(), signal.SIGTERM)
+
+    asyncio.ensure_future(kill())
+    main([])
