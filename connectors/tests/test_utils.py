22a020b4bee6b4d4bdcf748f915be74e4614dc11
Tarek Ziadé <tarek@ziade.org>
2022-09-01 15:53:40 -0700

    remove unsupported syntax

--- connectors/tests/test_utils.py
+++ connectors/tests/test_utils.py
@@ -5,5 +5,6 @@ def test_next_run():
 
     assert next_run("1 * * * * *") < 70.0
     assert next_run("* * * * * *") == 0
+
     # this should get parsed
-    next_run("0/5 14,18,3-39,52 * ? JAN,MAR,SEP MON-FRI 2002-2010")
+    next_run("0/5 14,18,52 * ? JAN,MAR,SEP MON-FRI 2002-2010")
