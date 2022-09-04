c26ec5896879da4ba317ac3b999e429995c2bc7a
Tarek Ziadé <tarek@ziade.org>
2022-09-04 11:04:00 -0700

    check that the index name is valid

--- connectors/tests/test_utils.py
+++ connectors/tests/test_utils.py
@@ -1,4 +1,5 @@
-from connectors.utils import next_run
+import pytest
+from connectors.utils import next_run, validate_index_name, InvalidIndexNameError
 
 
 def test_next_run():
@@ -8,3 +9,18 @@ def test_next_run():
 
     # this should get parsed
     next_run("0/5 14,18,52 * ? JAN,MAR,SEP MON-FRI 2002-2010")
+
+
+def test_invalid_names():
+    for name in (
+        "index?name",
+        "index#name",
+        "_indexname",
+        "-indexname",
+        "+indexname",
+        "INDEXNAME",
+        "..",
+        ".",
+    ):
+        with pytest.raises(InvalidIndexNameError):
+            validate_index_name(name)
