4bda3af679ae12ae9f378493fd9ab07ac9a722f5
Parth <parth.modi3@crestdatasys.com>
2022-09-06 19:11:09 +0530

    Added primary key in table for E2E

--- connectors/sources/tests/fixtures/mysql/loadsample.py
+++ connectors/sources/tests/fixtures/mysql/loadsample.py
@@ -7,11 +7,12 @@ def main():
     """Method for generating 15k document for mysql"""
     database = connect(host="127.0.0.1", port=3306, user="root", password="changeme")
     cursor = database.cursor()
-    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DATABASE_NAME}")
+    cursor.execute(f"DROP DATABASE IF EXISTS {DATABASE_NAME}")
+    cursor.execute(f"CREATE DATABASE {DATABASE_NAME}")
     cursor.execute(f"USE {DATABASE_NAME}")
     for table in range(15):
         sql_query = (
-            f"CREATE TABLE IF NOT EXISTS customers_{table} (name VARCHAR(255), age int)"
+            f"CREATE TABLE IF NOT EXISTS customers_{table} (name VARCHAR(255), age int, PRIMARY KEY (name))"
         )
         cursor.execute(sql_query)
         raws = []
@@ -19,7 +20,7 @@ def main():
             raws.append((f"user_{raw_id}", raw_id))
         sql_query = f"INSERT INTO customers_{table}" + "(name, age) VALUES (%s, %s)"
         cursor.executemany(sql_query, raws)
-        database.commit()
+    database.commit()
 
 
 if __name__ == "__main__":
