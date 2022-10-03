727c2a9002125fcb3df2b5c8abbd8947f1c03f88
Tarek Ziade <tarek@ziade.org>
2022-10-03 08:06:11 -0700

    fix the delete op (#101)
    * fixed the parsed example
    
    * fix the delete op
    
    * fix the delete op

--- connectors/tests/ftest.sh
+++ connectors/tests/ftest.sh
@@ -16,6 +16,9 @@ $ROOT_DIR/bin/fake-kibana --index-name search-$NAME --service-type $NAME --debug
 make load-data
 
 $ROOT_DIR/bin/elastic-ingest --one-sync --sync-now
+
+make remove-data
+
 $ROOT_DIR/bin/elastic-ingest --one-sync --sync-now
 $ROOT_DIR/bin/python $ROOT_DIR/scripts/verify.py --index-name search-$NAME --service-type $NAME --size 3000
 
