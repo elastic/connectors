b87c324a5c5f08b01eb482e49aad8eb79e2e860f
Tarek Ziade <tarek@ziade.org>
2022-10-03 08:05:52 -0700

    fixed the parsed example (#102)

--- connectors/tests/test_utils.py
+++ connectors/tests/test_utils.py
@@ -19,7 +19,7 @@ def test_next_run():
     assert next_run("* * * * * *") == 0
 
     # this should get parsed
-    next_run("0/5 14,18,52 * ? JAN,MAR,SEP MON-FRI 2002-2010")
+    next_run("0/5 14,18,52 * ? JAN,MAR,SEP MON-FRI 2010-2030")
 
 
 def test_invalid_names():
