93ceb1d3daa109ff3af6b8fff9a00ea4129acd0b
Artem Shelkovnikov <lavatroublebubble@gmail.com>
2022-10-12 17:36:08 +0200

    Add NOTICE.txt + fix license headers (#121)

--- connectors/quartz.py
+++ connectors/quartz.py
@@ -1,3 +1,9 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+#
+
 # vendored version of https://pypi.org/project/cron-schedule-triggers/
 # the repo is gone
 # TODO: simplify this code then port it to ruby
