5148d5cfb3d1a964cf5658f56ff887272af754b1
jignesh-crest <jignesh.parmar@crestdatasys.com>
2022-12-14 10:57:23 +0530

    use anext and load default cert from file

--- connectors/sources/gcs.py
+++ connectors/sources/gcs.py
@@ -46,6 +46,16 @@ DEFAULT_FILE_SIZE_LIMIT = 10485760
 STORAGE_EMULATOR_HOST = os.getenv(
     key="STORAGE_EMULATOR_HOST", default=None
 )  # For end to end tests set this environment variable with target host.
+RUNNING_FTEST = bool(
+    os.getenv(key="RUNNING_FTEST", default=None)
+)  # Flag to check if a connector is run for ftest or not.
+DEFAULT_PEM_FILE = (
+    os.path.join(
+        os.path.dirname(__file__),
+        "tests/fixtures/gcs",
+        "service_account_dummy_cert.pem",
+    ),
+)[0]
 
 
 class GoogleCloudStorageDataSource(BaseDataSource):
@@ -68,7 +78,12 @@ class GoogleCloudStorageDataSource(BaseDataSource):
             .decode()
         )
         self.service_account_credentials = ServiceAccountCreds(
-            scopes=[CLOUD_STORAGE_READ_ONLY_SCOPE], **json.loads(self.credentials)
+            scopes=[CLOUD_STORAGE_READ_ONLY_SCOPE],
+            **json.loads(
+                self.configuration["service_account_credentials"]
+                if RUNNING_FTEST
+                else self.credentials
+            ),
         )
         self.user_project_id = self.service_account_credentials.project_id
         self.retry_count = int(
@@ -87,7 +102,7 @@ class GoogleCloudStorageDataSource(BaseDataSource):
         """
         return {
             "service_account_credentials": {
-                "value": '{"type": "service_account","project_id": "dummy_project_id","private_key_id": "abc","private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDY3E8o1NEFcjMM\nHW/5ZfFJw29/8NEqpViNjQIx95Xx5KDtJ+nWn9+OW0uqsSqKlKGhAdAo+Q6bjx2c\nuXVsXTu7XrZUY5Kltvj94DvUa1wjNXs606r/RxWTJ58bfdC+gLLxBfGnB6CwK0YQ\nxnfpjNbkUfVVzO0MQD7UP0Hl5ZcY0Puvxd/yHuONQn/rIAieTHH1pqgW+zrH/y3c\n59IGThC9PPtugI9ea8RSnVj3PWz1bX2UkCDpy9IRh9LzJLaYYX9RUd7++dULUlat\nAaXBh1U6emUDzhrIsgApjDVtimOPbmQWmX1S60mqQikRpVYZ8u+NDD+LNw+/Eovn\nxCj2Y3z1AgMBAAECggEAWDBzoqO1IvVXjBA2lqId10T6hXmN3j1ifyH+aAqK+FVl\nGjyWjDj0xWQcJ9ync7bQ6fSeTeNGzP0M6kzDU1+w6FgyZqwdmXWI2VmEizRjwk+/\n/uLQUcL7I55Dxn7KUoZs/rZPmQDxmGLoue60Gg6z3yLzVcKiDc7cnhzhdBgDc8vd\nQorNAlqGPRnm3EqKQ6VQp6fyQmCAxrr45kspRXNLddat3AMsuqImDkqGKBmF3Q1y\nxWGe81LphUiRqvqbyUlh6cdSZ8pLBpc9m0c3qWPKs9paqBIvgUPlvOZMqec6x4S6\nChbdkkTRLnbsRr0Yg/nDeEPlkhRBhasXpxpMUBgPywKBgQDs2axNkFjbU94uXvd5\nznUhDVxPFBuxyUHtsJNqW4p/ujLNimGet5E/YthCnQeC2P3Ym7c3fiz68amM6hiA\nOnW7HYPZ+jKFnefpAtjyOOs46AkftEg07T9XjwWNPt8+8l0DYawPoJgbM5iE0L2O\nx8TU1Vs4mXc+ql9F90GzI0x3VwKBgQDqZOOqWw3hTnNT07Ixqnmd3dugV9S7eW6o\nU9OoUgJB4rYTpG+yFqNqbRT8bkx37iKBMEReppqonOqGm4wtuRR6LSLlgcIU9Iwx\nyfH12UWqVmFSHsgZFqM/cK3wGev38h1WBIOx3/djKn7BdlKVh8kWyx6uC8bmV+E6\nOoK0vJD6kwKBgHAySOnROBZlqzkiKW8c+uU2VATtzJSydrWm0J4wUPJifNBa/hVW\ndcqmAzXC9xznt5AVa3wxHBOfyKaE+ig8CSsjNyNZ3vbmr0X04FoV1m91k2TeXNod\njMTobkPThaNm4eLJMN2SQJuaHGTGERWC0l3T18t+/zrDMDCPiSLX1NAvAoGBAN1T\nVLJYdjvIMxf1bm59VYcepbK7HLHFkRq6xMJMZbtG0ryraZjUzYvB4q4VjHk2UDiC\nlhx13tXWDZH7MJtABzjyg+AI7XWSEQs2cBXACos0M4Myc6lU+eL+iA+OuoUOhmrh\nqmT8YYGu76/IBWUSqWuvcpHPpwl7871i4Ga/I3qnAoGBANNkKAcMoeAbJQK7a/Rn\nwPEJB+dPgNDIaboAsh1nZhVhN5cvdvCWuEYgOGCPQLYQF0zmTLcM+sVxOYgfy8mV\nfbNgPgsP5xmu6dw2COBKdtozw0HrWSRjACd1N4yGu75+wPCcX/gQarcjRcXXZeEa\nNtBLSfcqPULqD+h7br9lEJio\n-----END PRIVATE KEY-----\n","client_email": "123-abc@developer.gserviceaccount.com","client_id": "123-abc.apps.googleusercontent.com","auth_uri": "https://accounts.google.com/o/oauth2/auth","token_uri": "http://localhost:443/token"}',
+                "value": f'{{"type": "service_account","project_id": "dummy_project_id","private_key_id": "abc","private_key": {json.dumps(open(DEFAULT_PEM_FILE).read())} ,"client_email": "123-abc@developer.gserviceaccount.com","client_id": "123-abc.apps.googleusercontent.com","auth_uri": "https://accounts.google.com/o/oauth2/auth","token_uri": "http://localhost:443/token"}}',
                 "label": "JSON string for Google cloud service account",
                 "type": "str",
             },
@@ -139,7 +154,7 @@ class GoogleCloudStorageDataSource(BaseDataSource):
                     storage_client = await google_client.discover(
                         api_name=API_NAME, api_version=API_VERSION
                     )
-                    if not sub_method and STORAGE_EMULATOR_HOST:
+                    if RUNNING_FTEST and not sub_method and STORAGE_EMULATOR_HOST:
                         # Redirecting calls to fake-gcs-server for e2e test.
                         storage_client.discovery_document["rootUrl"] = (
                             STORAGE_EMULATOR_HOST + "/"
@@ -179,13 +194,14 @@ class GoogleCloudStorageDataSource(BaseDataSource):
     async def ping(self):
         """Verify the connection with Google Cloud Storage"""
         try:
-            await self._api_call(
-                resource="projects",
-                method="serviceAccount",
-                sub_method="get",
-                projectId=self.user_project_id,
-            ).__anext__()
-
+            await anext(
+                self._api_call(
+                    resource="projects",
+                    method="serviceAccount",
+                    sub_method="get",
+                    projectId=self.user_project_id,
+                )
+            )
             logger.info("Successfully connected to the Google Cloud Storage.")
         except Exception:
             logger.exception("Error while connecting to the Google Cloud Storage.")
@@ -280,14 +296,16 @@ class GoogleCloudStorageDataSource(BaseDataSource):
             )
             return
 
-        content_text = await self._api_call(
-            resource="objects",
-            method="get",
-            bucket=blob["bucket_name"],
-            object=blob["name"],
-            alt="media",
-            userProject=self.user_project_id,
-        ).__anext__()
+        content_text = await anext(
+            self._api_call(
+                resource="objects",
+                method="get",
+                bucket=blob["bucket_name"],
+                object=blob["name"],
+                alt="media",
+                userProject=self.user_project_id,
+            )
+        )
 
         return {
             "_id": blob["id"],
