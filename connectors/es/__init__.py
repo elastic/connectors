b4a4b42f7ed03e309fb53e7af36e82d7aa7e5aad
Tarek Ziadé <tarek@ziade.org>
2022-12-20 13:45:15 -0800

    moved ESCLient to connectors.es

--- connectors/es/__init__.py
+++ connectors/es/__init__.py
@@ -3,6 +3,7 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
+from connectors.es.client import ESClient  # NOQA
 from connectors.es.index import ESIndex  # NOQA
 from connectors.es.settings import (  # NOQA
     DEFAULT_LANGUAGE,
