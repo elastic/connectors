57682ea2b79408e40e87cd05cb105c2e07814395
Tarek Ziadé <tarek@ziade.org>
2022-12-20 17:13:59 -0800

    added a nightly script

---
+++ .buildkite/run_dockerized_nigthly.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+BASEDIR=$(realpath $(dirname $0))
+ROOT=$(realpath $BASEDIR/../)
+
+cd $ROOT/connectors/sources/tests/fixtures/mysql
+
+make run-stack
+sleep 120
+
+make load-data
+
+cd $ROOT
+docker run --rm -v $ROOT:/ci -w=/ci \
+    -it \
+    python:3.10 \
+    /bin/bash -c  "/ci/.buildkite/nightly.sh"
+
+cd $ROOT/connectors/sources/tests/fixtures/mysql
+make stop-stack
