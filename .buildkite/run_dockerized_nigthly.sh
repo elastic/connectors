b0d4145e9118741ebd16783b0839027d259527b0
Tarek Ziadé <tarek@ziade.org>
2022-12-20 17:49:41 -0800

    make sure we have docker & docker-compose

--- .buildkite/run_dockerized_nigthly.sh
+++ .buildkite/run_dockerized_nigthly.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo apt install docker-compose
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+mkdir -p /etc/apt/keyrings
+
+# update docker and docker-compose
+ARCH=`dpkg --print-architecture`
+RELEASE=`lsb_release -cs`
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$ARCH signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $RELEASE stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl start docker
+
 
 BASEDIR=$(realpath $(dirname $0))
 ROOT=$(realpath $BASEDIR/../)
