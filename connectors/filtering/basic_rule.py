fca925fb4d47a46900f6fe7b71c906247bba4d9c
Tim Grein <tim.grein@elastic.co>
2022-12-21 16:22:33 +0100

    isort basic_rule.py

--- connectors/filtering/basic_rule.py
+++ connectors/filtering/basic_rule.py
@@ -3,11 +3,13 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
-from connectors.logger import logger
 import datetime
-from dateutil.parser import *
-from enum import Enum
 import re
+from enum import Enum
+
+from dateutil.parser import *
+
+from connectors.logger import logger
 
 
 class BasicRule:
