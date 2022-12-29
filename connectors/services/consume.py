f7d94484882cced6c1c5b99e0367cac3a512503f
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-12-29 17:41:38 +0100

    Move sync to a separate class

--- connectors/services/consume.py
+++ connectors/services/consume.py
@@ -22,6 +22,98 @@ from connectors.services.base import BaseService
 from connectors.source import DataSourceConfiguration, get_source_klass
 from connectors.utils import CancellableSleeps, trace_mem
 
+class Sync:
+    def __init__(sources, job, elastic_server, bulk_options):
+        self.sources = sources
+        self.job = job
+        self.elastic_server = elastic_server
+        self.bulk_options = bulk_options
+
+    async def execute():
+        job = self.job
+
+        job.claim()
+
+        start_time = time.time()
+        index_name = job.connector["index_name"]
+        service_type = job.connector["service_type"]
+        language_code = job.connector["language"]
+        pipeline = PipelineSettings(job.connector.get("pipeline", {}))
+
+
+        fqn = self.sources[service_type]
+        configuration = DataSourceConfiguration(job.connector["configuration"])
+        try:
+            source_klass = get_source_klass(fqn)
+        except Exception as e:
+            logger.critical(e, exc_info=True)
+            raise DataSourceError(f"Could not instantiate {fqn} for {service_type}")
+
+        data_provider = source_klass(configuration)
+
+        if not await data_provider.changed():
+            logger.debug(f"No change in {service_type} data provider, skipping...")
+            return
+
+        logger.debug(f"Syncing '{service_type}'")
+        try:
+            logger.debug(f"Pinging the {source_klass} backend")
+            await data_provider.ping()
+            await asyncio.sleep(0)
+
+            mappings, settings = defaults_for(
+                is_connectors_index=True, language_code=language_code
+            )
+            logger.debug("Preparing the index")
+            await self.elastic_server.prepare_index(
+                index_name, mappings=mappings, settings=settings
+            )
+            await asyncio.sleep(0)
+
+            result = await self.elastic_server.async_bulk(
+                index_name,
+                self.prepare_docs(pipeline, data_provider),
+                pipeline,
+                options=self.bulk_options,
+            )
+            await self._sync_done(job, result, start_time)
+
+        except Exception as e:
+            await self._sync_done(job, {}, start_time, exception=e)
+            raise
+        finally:
+            self._start_time = None
+
+    async def prepare_docs(self, pipeline, data_provider):
+        logger.debug(f"Using pipeline {pipeline}")
+
+        async for doc, lazy_download in data_provider.get_docs():
+            # adapt doc for pipeline settings
+            doc["_extract_binary_content"] = pipeline.extract_binary_content
+            doc["_reduce_whitespace"] = pipeline.reduce_whitespace
+            doc["_run_ml_inference"] = pipeline.run_ml_inference
+            yield doc, lazy_download
+
+    async def _sync_done(self, job, result, start_time, exception=None):
+        doc_updated = result.get("doc_updated", 0)
+        doc_created = result.get("doc_created", 0)
+        doc_deleted = result.get("doc_deleted", 0)
+        exception = result.get("fetch_error", exception)
+
+        indexed_count = doc_updated + doc_created
+
+        await job.done(indexed_count, doc_deleted, exception)
+
+        # Loading connector as late as possible so that there's less likely
+        # a version conflict.
+        connector = await self.connectors.fetch_by_id(job.connector_id)
+        await connector.job_done(job)
+
+        logger.info(
+            f"Sync done: {indexed_count} indexed, {doc_deleted} "
+            f" deleted. ({int(time.time() - start_time)} seconds)"
+        )
+
 
 class JobService(BaseService):
     def __init__(self, args):
@@ -55,9 +147,16 @@ class JobService(BaseService):
 
                 query = self.jobs.build_docs_query(native_service_types, connectors_ids)
 
-                synced_anything = False
+                synced_anything = False # just to make the one syncs in CI work for now
                 async for job in self.jobs.get_all_docs(query=query):
-                    await self.execute(job)
+
+                    sync = Sync(
+                            self.config["sources"],
+                            job,
+                            self.elastic_server,
+                            self.bulk_options)
+
+                    await sync.execute()
                     synced_anything = True
 
                 if one_sync and synced_anything:
@@ -68,86 +167,6 @@ class JobService(BaseService):
         finally:
             self.stop()
         return 0
-
-    async def execute(self, job):
-        await job.claim()
-
-        start_time = time.time()
-        index_name = job.connector["index_name"]
-        service_type = job.connector["service_type"]
-        pipeline = PipelineSettings(job.connector.get("pipeline", {}))
-
-        connector = await self.connectors.fetch_by_id(job.connector_id)
-
-        fqn = self.config["sources"][service_type]
-        configuration = DataSourceConfiguration(job.connector["configuration"])
-        try:
-            source_klass = get_source_klass(fqn)
-        except Exception as e:
-            logger.critical(e, exc_info=True)
-            raise DataSourceError(f"Could not instantiate {fqn} for {service_type}")
-
-        data_provider = source_klass(configuration)
-
-        if not await data_provider.changed():
-            logger.debug(f"No change in {service_type} data provider, skipping...")
-            return
-
-        logger.debug(f"Syncing '{service_type}'")
-        try:
-            logger.debug(f"Pinging the {source_klass} backend")
-            await data_provider.ping()
-            await asyncio.sleep(0)
-
-            mappings, settings = defaults_for(
-                is_connectors_index=True, language_code=self.language_code
-            )
-            logger.debug("Preparing the index")
-            await self.elastic_server.prepare_index(
-                index_name, mappings=mappings, settings=settings
-            )
-            await asyncio.sleep(0)
-
-            result = await self.elastic_server.async_bulk(
-                index_name,
-                self.prepare_docs(pipeline, data_provider),
-                pipeline,
-                options=self.bulk_options,
-            )
-            await self._sync_done(job, connector, result, start_time)
-
-        except Exception as e:
-            await self._sync_done(job, connector, {}, start_time, exception=e)
-            raise
-        finally:
-            self._start_time = None
-
-    async def prepare_docs(self, pipeline, data_provider):
-        logger.debug(f"Using pipeline {pipeline}")
-
-        async for doc, lazy_download in data_provider.get_docs():
-            # adapt doc for pipeline settings
-            doc["_extract_binary_content"] = pipeline.extract_binary_content
-            doc["_reduce_whitespace"] = pipeline.reduce_whitespace
-            doc["_run_ml_inference"] = pipeline.run_ml_inference
-            yield doc, lazy_download
-
-    async def _sync_done(self, job, connector, result, start_time, exception=None):
-        doc_updated = result.get("doc_updated", 0)
-        doc_created = result.get("doc_created", 0)
-        doc_deleted = result.get("doc_deleted", 0)
-        exception = result.get("fetch_error", exception)
-
-        indexed_count = doc_updated + doc_created
-
-        await job.done(indexed_count, doc_deleted, exception)
-
-        await connector.job_done(job)
-
-        logger.info(
-            f"Sync done: {indexed_count} indexed, {doc_deleted} "
-            f" deleted. ({int(time.time() - start_time)} seconds)"
-        )
 
     def stop(self):
         logger.debug("Shutting down consumers")
