287ab6ab8efaa75d66ad15b6c939e8ddc5976730
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-12-30 12:26:22 +0100

    One more rename

--- connectors/byoc.py
+++ connectors/byoc.py
@@ -79,7 +79,7 @@ class ConnectorIndex(ESIndex):
     )
 
     def __init__(self, elastic_config):
-        logger.debug(f"ConnectorsIndex connecting to {elastic_config['host']}")
+        logger.debug(f"ConnectorIndex connecting to {elastic_config['host']}")
         # initilize ESIndex instance
         super().__init__(index_name=CONNECTORS_INDEX, elastic_config=elastic_config)
         # grab all bulk options
