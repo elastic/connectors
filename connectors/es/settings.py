b8f8bb6f09c01efb40874359270dcc4972e386fd
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-01-16 12:20:20 +0100

    Return back the Settings

--- connectors/es/settings.py
+++ connectors/es/settings.py
@@ -3,7 +3,10 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
+import copy
 from os import path
+
+import yaml
 
 ENUM_IGNORE_ABOVE = 2048
 
@@ -99,6 +102,166 @@ GENERIC_FILTERS = {
     },
     "bigram_max_size": {"type": "length", "min": 0, "max": 16},
 }
+
+NON_ICU_ANALYSIS_SETTINGS = {
+    "tokenizer_name": "standard",
+    "folding_filters": ["cjk_width", "lowercase", "asciifolding"],
+}
+
+ICU_ANALYSIS_SETTINGS = {
+    "tokenizer_name": "icu_tokenizer",
+    "folding_filters": ["icu_folding"],
+}
+
+
+class UnsupportedLanguageCode(Exception):
+    pass
+
+
+class Settings:
+    """Default settings for the Elasticsearch index"""
+
+    @property
+    def language_data(self):
+        if not self._language_data:
+            with open(LANGUAGE_DATA_FILE_PATH, "r") as f:
+                self._language_data = yaml.safe_load(f)
+        return self._language_data
+
+    def icu_settings(self, analysis_settings):
+        return ICU_ANALYSIS_SETTINGS if analysis_settings else NON_ICU_ANALYSIS_SETTINGS
+
+    @property
+    def stemmer_name(self):
+        return self.language_data[self.language_code].get("stemmer", None)
+
+    @property
+    def stop_words_name_or_list(self):
+        return self.language_data[self.language_code].get("stop_words", None)
+
+    @property
+    def custom_filter_definitions(self):
+        return self.language_data[self.language_code].get(
+            "custom_filter_definitions", {}
+        )
+
+    @property
+    def prepended_filters(self):
+        return self.language_data[self.language_code].get("prepended_filters", [])
+
+    @property
+    def postpended_filters(self):
+        return self.language_data[self.language_code].get("postpended_filters", [])
+
+    @property
+    def stem_filter_name(self):
+        return f"{self.language_code}-stem-filter"
+
+    @property
+    def stop_words_filter_name(self):
+        return f"{self.language_code}-stop-words-filter"
+
+    @property
+    def filter_definitions(self):
+        definitions = copy.deepcopy(GENERIC_FILTERS)
+
+        definitions[self.stem_filter_name] = {
+            "type": "stemmer",
+            "name": self.stemmer_name,
+        }
+
+        definitions[self.stop_words_filter_name] = {
+            "type": "stop",
+            "stopwords": self.stop_words_name_or_list,
+        }
+
+        definitions.update(self.custom_filter_definitions)
+        return definitions
+
+    @property
+    def analyzer_definitions(self):
+        definitions = {}
+
+        definitions["i_prefix"] = {
+            "tokenizer": self.analysis_settings["tokenizer_name"],
+            "filter": [*self.analysis_settings["folding_filters"], "front_ngram"],
+        }
+
+        definitions["q_prefix"] = {
+            "tokenizer": self.analysis_settings["tokenizer_name"],
+            "filter": [*self.analysis_settings["folding_filters"]],
+        }
+
+        definitions["iq_text_base"] = {
+            "tokenizer": self.analysis_settings["tokenizer_name"],
+            "filter": [
+                *self.analysis_settings["folding_filters"],
+                self.stop_words_filter_name,
+            ],
+        }
+
+        definitions["iq_text_stem"] = {
+            "tokenizer": self.analysis_settings["tokenizer_name"],
+            "filter": self.prepended_filters
+            + self.analysis_settings["folding_filters"]
+            + [
+                self.stop_words_filter_name,
+                self.stem_filter_name,
+            ]
+            + self.postpended_filters,
+        }
+
+        definitions["iq_text_delimiter"] = {
+            "tokenizer": "whitespace",
+            "filter": self.prepended_filters
+            + ["delimiter"]
+            + self.analysis_settings["folding_filters"]
+            + [self.stop_words_filter_name, self.stem_filter_name]
+            + self.postpended_filters,
+        }
+
+        definitions["i_text_bigram"] = {
+            "tokenizer": self.analysis_settings["tokenizer_name"],
+            "filter": self.analysis_settings["folding_filters"]
+            + [
+                self.stem_filter_name,
+                "bigram_joiner",
+                "bigram_max_size",
+            ],
+        }
+
+        definitions["q_text_bigram"] = {
+            "tokenizer": self.analysis_settings["tokenizer_name"],
+            "filter": self.analysis_settings["folding_filters"]
+            + [
+                self.stem_filter_name,
+                "bigram_joiner_unigrams",
+                "bigram_max_size",
+            ],
+        }
+
+        return definitions
+
+    def __init__(self, *, language_code=None, analysis_icu=False):
+        self._language_data = None
+        self.language_code = language_code or DEFAULT_LANGUAGE
+
+        if self.language_code not in self.language_data:
+            raise UnsupportedLanguageCode(
+                f"Language '{language_code}' is not supported"
+            )
+
+        self.analysis_icu = analysis_icu
+        self.analysis_settings = self.icu_settings(analysis_icu)
+
+    def to_hash(self):
+        return {
+            "analysis": {
+                "analyzer": self.analyzer_definitions,
+                "filter": self.filter_definitions,
+            },
+            "index": {"similarity": {"default": {"type": "BM25"}}},
+        }
 
 
 class Mappings:
--- connectors/tests/test_es_settings.py
+++ connectors/tests/test_es_settings.py
@@ -4,7 +4,16 @@
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
 
-from connectors.es.settings import Mappings
+import pytest
+
+from connectors.es.settings import (
+    DEFAULT_LANGUAGE,
+    ICU_ANALYSIS_SETTINGS,
+    NON_ICU_ANALYSIS_SETTINGS,
+    Mappings,
+    Settings,
+    UnsupportedLanguageCode,
+)
 
 EXPECTED_CONNECTORS_PROPS = ["id", "_subextracted_as_of", "_subextracted_version"]
 
@@ -86,3 +95,75 @@ def test_mappings_crawler_index():
     for prop in EXPECTED_CRAWLER_PROPS:
         assert prop in actual["properties"]
         assert isinstance(actual["properties"][prop], dict)
+
+
+def test_settings_analysis_icu_false():
+    """When analysis_icu is false"""
+    actual = Settings(language_code=DEFAULT_LANGUAGE, analysis_icu=False).to_hash()
+    assert isinstance(actual, dict)
+    assert "analysis" in actual
+    assert "analyzer" in actual["analysis"]
+
+    analyzer = actual["analysis"]["analyzer"]
+    assert EXPECTED_ANALYZER_KEYS.issubset(analyzer.keys())
+
+    non_icu_filters = NON_ICU_ANALYSIS_SETTINGS["folding_filters"]
+    icu_filters = ICU_ANALYSIS_SETTINGS["folding_filters"]
+
+    filters = {f for item in analyzer.values() for f in item["filter"]}
+    # should contain non-icu filters
+    assert set(non_icu_filters).issubset(filters)
+    # should not contain icu filters
+    assert not any(f in filters for f in icu_filters)
+
+
+def test_settings_analysis_icu_true():
+    """When analysis_icu is true"""
+    actual = Settings(language_code=DEFAULT_LANGUAGE, analysis_icu=True).to_hash()
+    assert isinstance(actual, dict)
+    assert "analysis" in actual
+    assert "analyzer" in actual["analysis"]
+
+    analyzer = actual["analysis"]["analyzer"]
+    assert EXPECTED_ANALYZER_KEYS.issubset(analyzer.keys())
+
+    non_icu_filters = NON_ICU_ANALYSIS_SETTINGS["folding_filters"]
+    icu_filters = ICU_ANALYSIS_SETTINGS["folding_filters"]
+
+    filters = {f for item in analyzer.values() for f in item["filter"]}
+    # should contain icu filters
+    assert set(icu_filters).issubset(filters)
+    # should not contain non-icu filters
+    assert not any(f in filters for f in non_icu_filters)
+
+
+def test_settings_unsupported_language():
+    """When the language_code is not supported"""
+    with pytest.raises(UnsupportedLanguageCode):
+        _ = Settings(
+            language_code="unsupported_language_code", analysis_icu=False
+        ).to_hash()
+
+
+def test_settings_supported_language():
+    """When the language_code is supported"""
+    language_code = "fr"
+    actual = Settings(language_code=language_code, analysis_icu=False).to_hash()
+    assert isinstance(actual, dict)
+    assert "analysis" in actual
+    assert "filter" in actual["analysis"]
+    filter = actual["analysis"]["filter"]
+
+    for k in (
+        f"{language_code}-stem-filter",
+        f"{language_code}-stop-words-filter",
+        f"{language_code}-elision",
+    ):
+        assert k in filter
+
+
+def test_settings_none_language():
+    """When the language_code is None"""
+    actual = Settings(language_code=None, analysis_icu=False).to_hash()
+    english = Settings(language_code="en", analysis_icu=False).to_hash()
+    assert actual == english
