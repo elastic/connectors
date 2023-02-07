8027897416cd3b81182cb1e3cef2c70e14eedc8c
Tarek Ziade <tarek@ziade.org>
2023-02-07 11:01:40 -0800

    fix filtering loading (#425)

--- connectors/kibana.py
+++ connectors/kibana.py
@@ -264,22 +264,17 @@ def _parser():
 
 
 def _load_filtering(filtering_file_path):
-    if not os.path.exists(filtering_file_path):
+    if not os.path.exists(filtering_file_path) and not os.path.isfile(
+        filtering_file_path
+    ):
         logger.warn(
             f"filtering file at '{filtering_file_path}' does not exist. Fallback to default filtering."
         )
         filtering = DEFAULT_FILTERING
     else:
-        filtering_file = open(
-            os.path.join(
-                os.path.dirname(__file__),
-                "sources",
-                "tests",
-                "fixtures",
-                filtering_file_path,
-            )
-        )
-        filtering = json.load(filtering_file)
+        with open(filtering_file_path) as f:
+            filtering = json.loads(f.read())
+
         logger.info(f"Successfully loaded filtering file from '{filtering_file_path}'.")
 
     return filtering
@@ -294,6 +289,7 @@ def main(args=None):
     if not os.path.exists(config_file):
         raise IOError(f"config file at '{config_file}' does not exist")
 
+    logger.info(f"Loading filters from {filtering_file}")
     filtering = _load_filtering(filtering_file)
 
     set_logger(args.debug and logging.DEBUG or logging.INFO)
