e6a1e9463f0633748c3cdf7a65021e5950221fb2
Tarek Ziade <tarek@ziade.org>
2023-02-08 17:49:24 -0800

    Make the directory connector a full connector (#422)

--- connectors/sources/directory.py
+++ connectors/sources/directory.py
@@ -12,22 +12,23 @@ import os
 from datetime import datetime, timezone
 from pathlib import Path
 
+from connectors.logger import logger
 from connectors.source import BaseDataSource
 from connectors.utils import TIKA_SUPPORTED_FILETYPES, get_base64_value
 
-HERE = os.path.dirname(__file__)
 DEFAULT_CONTENT_EXTRACTION = True
+DEFAULT_DIR = os.environ.get("SYSTEM_DIR", os.path.dirname(__file__))
 
 
 class DirectoryDataSource(BaseDataSource):
     """Directory"""
 
-    name = "Directory"
+    name = "System Directory"
     service_type = "dir"
 
     def __init__(self, configuration):
         super().__init__(configuration=configuration)
-        self.directory = self.configuration["directory"]
+        self.directory = os.path.abspath(self.configuration["directory"])
         self.pattern = self.configuration["pattern"]
         self.enable_content_extraction = self.configuration["enable_content_extraction"]
 
@@ -35,12 +36,12 @@ class DirectoryDataSource(BaseDataSource):
     def get_default_configuration(cls):
         return {
             "directory": {
-                "value": HERE,
-                "label": "Directory",
+                "value": DEFAULT_DIR,
+                "label": "Directory path",
                 "type": "str",
             },
             "pattern": {
-                "value": "**/*.py",
+                "value": "**/*.*",
                 "label": "File glob-like pattern",
                 "type": "str",
             },
@@ -72,12 +73,14 @@ class DirectoryDataSource(BaseDataSource):
         with open(file=path, mode="rb") as f:
             return {
                 "_id": self.get_id(path),
-                "timestamp": timestamp,
+                "_timestamp": timestamp,
                 "_attachment": get_base64_value(f.read()),
             }
 
     async def get_docs(self, filtering=None):
+        logger.debug(f"Reading {self.directory}...")
         root_directory = Path(self.directory)
+
         for path_object in root_directory.glob(self.pattern):
             if not path_object.is_file():
                 continue
@@ -86,13 +89,24 @@ class DirectoryDataSource(BaseDataSource):
             download_coro = functools.partial(self._download, str(path_object))
 
             # get the last modified value of the file
-            ts = path_object.stat().st_mtime
+            stat = path_object.stat()
+            ts = stat.st_mtime
             ts = datetime.fromtimestamp(ts, tz=timezone.utc)
 
             # send back as a doc
             doc = {
                 "path": str(path_object),
-                "timestamp": ts.isoformat(),
+                "last_modified_time": ts,
+                "inode_protection_mode": stat.st_mode,
+                "inode_number": stat.st_ino,
+                "device_inode_reside": stat.st_dev,
+                "number_of_links": stat.st_nlink,
+                "uid": stat.st_uid,
+                "gid": stat.st_gid,
+                "ctime": stat.st_ctime,
+                "last_access_time": stat.st_atime,
+                "size": stat.st_size,
+                "_timestamp": ts.isoformat(),
                 "_id": self.get_id(path_object),
             }
 
