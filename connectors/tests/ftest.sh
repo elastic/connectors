ad3237b6084b4368083ece6c060b932b31e9c369
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-02-09 15:51:58 +0100

    Revert change in ftest

--- connectors/tests/ftest.sh
+++ connectors/tests/ftest.sh
@@ -47,12 +47,12 @@ if [[ $PERF8 == "yes" ]]
 then
     if [[ $PLATFORM == "darwin" ]]
     then
-      $PERF8_BIN --refresh-rate $REFRESH_RATE -t $ROOT_DIR/perf8-report-$NAME --asyncstats --memray --psutil -c $ELASTIC_INGEST --log-level=DEBUG & PID=$!
+      $PERF8_BIN --refresh-rate $REFRESH_RATE -t $ROOT_DIR/perf8-report-$NAME --asyncstats --memray --psutil -c $ELASTIC_INGEST --debug & PID=$!
     else
-      $PERF8_BIN --refresh-rate $REFRESH_RATE -t $ROOT_DIR/perf8-report-$NAME --asyncstats --memray --psutil -c $ELASTIC_INGEST --log-level=DEBUG & PID=$!
+      $PERF8_BIN --refresh-rate $REFRESH_RATE -t $ROOT_DIR/perf8-report-$NAME --asyncstats --memray --psutil -c $ELASTIC_INGEST --debug & PID=$!
     fi
 else
-    $ELASTIC_INGEST --log-level=DEBUG & PID=$!
+    $ELASTIC_INGEST --debug & PID=$!
 fi
 
 $PYTHON fixture.py --name $NAME --action monitor --pid $PID
