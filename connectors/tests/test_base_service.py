a2b10df86bd97dadb3b3ec1d25d6f38030163435
Artem Shelkovnikov <lavatroublebubble@gmail.com>
2023-03-22 09:45:35 +0100

    Temporary solve the problem with cancellation (#514)

--- connectors/tests/test_base_service.py
+++ connectors/tests/test_base_service.py
@@ -17,6 +17,7 @@ class StubService:
         self.running = False
         self.cancelled = False
         self.exploding = False
+        self.run_sleep_delay = 0
         self.stopped = False
         self.handle_cancellation = True
 
@@ -35,7 +36,7 @@ class StubService:
         while self.running:
             if self.exploding:
                 raise Exception("Something went wrong")
-            await asyncio.sleep(0)
+            await asyncio.sleep(self.run_sleep_delay)
 
     def stop(self):
         self.running = False
@@ -43,23 +44,6 @@ class StubService:
 
     def explode(self):
         self.exploding = True
-
-
-@pytest.mark.asyncio
-async def test_multiservice_run_stops_all_services_when_one_stops():
-    service_1 = StubService()
-    service_2 = StubService()
-    service_3 = StubService()
-
-    multiservice = MultiService(service_1, service_2, service_3)
-
-    asyncio.get_event_loop().call_later(0.1, service_1.stop)
-
-    await multiservice.run()
-
-    assert not service_1.cancelled
-    assert service_2.cancelled
-    assert service_3.cancelled
 
 
 @pytest.mark.asyncio
@@ -99,26 +83,6 @@ async def test_multiservice_run_stops_all_services_when_shutdown_happens():
 
 
 @pytest.mark.asyncio
-async def test_multiservice_run_stops_all_services_when_shutdown_happens_and_some_dont_handle_cancellation():
-    service_1 = StubService()
-    service_2 = StubService()
-    service_3 = StubService()
-
-    service_3.handle_cancellation = False
-
-    multiservice = MultiService(service_1, service_2, service_3)
-
-    asyncio.get_event_loop().call_later(0.1, service_1.stop)
-
-    await multiservice.run()
-
-    assert service_1.stopped
-    assert service_2.cancelled
-    # We assert False for service_3 - it does not handle cancellation gracefully
-    assert not service_3.cancelled
-
-
-@pytest.mark.asyncio
 async def test_registry():
     ran = []
 
@@ -140,3 +104,25 @@ async def test_registry():
 
     await multiservice.run()
     assert ran == ["kool", "beans"]
+
+
+@pytest.mark.asyncio
+async def test_multiservice_stop_gracefully_stops_service_that_takes_too_long_to_run():
+    service_1 = StubService()
+
+    service_2 = StubService()
+    service_2.run_sleep_delay = 0.3  # large enough
+
+    multiservice = MultiService(service_1, service_2)
+
+    asyncio.get_event_loop().call_later(
+        0.1, functools.partial(multiservice.shutdown, "SIGTERM")
+    )
+
+    await multiservice.run()
+
+    assert service_1.stopped
+    assert service_2.stopped
+    assert (
+        not service_2.cancelled
+    )  # we're not supposed to cancel it as it's already stopping
