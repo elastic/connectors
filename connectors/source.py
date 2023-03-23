0f0f5bea47409e60f3e5422650eebe25df816f92
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2023-03-23 12:10:30 +0100

    Move default configuration out of loop

--- connectors/source.py
+++ connectors/source.py
@@ -130,22 +130,23 @@ class BaseDataSource:
     def get_simple_configuration(cls):
         """Used to return the default config to Kibana"""
         res = {}
+        default_configuration = {
+            "default_value": None,
+            "depends_on": [],
+            "display": "text",
+            "label": "",
+            "options": [],
+            "order": 1,
+            "required": True,
+            "sensitive": False,
+            "tooltip": None,
+            "type": "str",
+            "validations": [],
+            "value": "",
+        }
 
         for config_name, fields in cls.get_default_configuration().items():
-            entry = {
-                "default_value": None,
-                "depends_on": [],
-                "display": "text",
-                "label": "",
-                "options": [],
-                "order": 1,
-                "required": True,
-                "sensitive": False,
-                "tooltip": None,
-                "type": "str",
-                "validations": [],
-                "value": "",
-            }
+            entry = default_configuration
 
             for field_property, value in fields.items():
                 if field_property == "label":
