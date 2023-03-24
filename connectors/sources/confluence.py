09fe59fbe53912600d977f9525591b4cddebd7ba
Praveen Kukreja <90465691+praveen-elastic@users.noreply.github.com>
2023-03-24 21:58:24 +0530

    [Confluence] Add indexing functionality (#604)
    ---------
    
    Co-authored-by: moxarth-elastic <96762084+moxarth-elastic@users.noreply.github.com>

--- connectors/sources/confluence.py
+++ connectors/sources/confluence.py
@@ -5,19 +5,53 @@
 #
 """Confluence source module responsible to fetch documents from Confluence Cloud/Server.
 """
+import asyncio
 import os
+from copy import copy
+from functools import partial
 
+import aiofiles
 import aiohttp
+from aiofiles.os import remove
+from aiofiles.tempfile import NamedTemporaryFile
 from aiohttp.client_exceptions import ServerDisconnectedError
 
 from connectors.logger import logger
 from connectors.source import BaseDataSource
-from connectors.utils import CancellableSleeps, iso_utc, ssl_context
+from connectors.utils import (
+    TIKA_SUPPORTED_FILETYPES,
+    CancellableSleeps,
+    ConcurrentTasks,
+    MemQueue,
+    convert_to_b64,
+    iso_utc,
+    ssl_context,
+)
 
 FILE_SIZE_LIMIT = 10485760
 RETRY_INTERVAL = 2
+SPACE = "space"
+ATTACHMENT = "attachment"
+CONTENT = "content"
+DOWNLOAD = "download"
+SPACE_QUERY = "limit=100"
+ATTACHMENT_QUERY = "limit=100&expand=version"
+CONTENT_QUERY = (
+    "limit=100&expand=children.attachment,history.lastUpdated,body.storage,space"
+)
+
+URLS = {
+    SPACE: "rest/api/space?{api_query}",
+    CONTENT: "rest/api/content?{api_query}",
+    ATTACHMENT: "rest/api/content/{id}/child/attachment?{api_query}",
+}
 PING_URL = "rest/api/space?limit=1"
 MAX_CONCURRENT_DOWNLOADS = 50  # Max concurrent download supported by confluence
+CHUNK_SIZE = 1024
+MAX_CONCURRENCY = 50
+QUEUE_SIZE = 1024
+QUEUE_MEM_SIZE = 25 * 1024 * 1024  # Size in Megabytes
+END_SIGNAL = "FINISHED_TASK"
 
 
 class ConfluenceClient:
@@ -112,6 +146,34 @@ class ConfluenceClient:
                 )
                 await self._sleeps.sleep(RETRY_INTERVAL**retry_counter)
 
+    async def paginated_api_call(self, url_name, **url_kwargs):
+        """Make a paginated API call for Confluence objects using the passed url_name.
+        Args:
+            url_name (str): URL Name to identify the API endpoint to hit
+        Yields:
+            response: JSON response.
+        """
+        url = os.path.join(self.host_url, URLS[url_name].format(**url_kwargs))
+        while True:
+            try:
+                async for response in self.api_call(
+                    url=url,
+                ):
+                    json_response = await response.json()
+                    links = json_response.get("_links")
+                    yield json_response
+                    if links.get("next") is None:
+                        return
+                    url = os.path.join(
+                        self.host_url,
+                        links.get("next")[1:],
+                    )
+            except Exception as exception:
+                logger.warning(
+                    f"Skipping data for type {url_name} from {url}. Exception: {exception}."
+                )
+                break
+
 
 class ConfluenceDataSource(BaseDataSource):
     """Confluence"""
@@ -129,6 +191,10 @@ class ConfluenceDataSource(BaseDataSource):
         self.enable_content_extraction = self.configuration["enable_content_extraction"]
         self.concurrent_downloads = self.configuration["concurrent_downloads"]
         self.confluence_client = ConfluenceClient(configuration)
+
+        self.queue = MemQueue(maxsize=QUEUE_SIZE, maxmemsize=QUEUE_MEM_SIZE)
+        self.fetchers = ConcurrentTasks(max_concurrency=MAX_CONCURRENCY)
+        self.fetcher_count = 0
 
     @classmethod
     def get_default_configuration(cls):
@@ -256,6 +322,220 @@ class ConfluenceDataSource(BaseDataSource):
             logger.exception("Error while connecting to Confluence")
             raise
 
+    async def fetch_spaces(self):
+        """Get spaces with the help of REST APIs
+
+        Yields:
+            Dictionary: Space document to get indexed
+        """
+        async for response in self.confluence_client.paginated_api_call(
+            url_name=SPACE,
+            api_query=SPACE_QUERY,
+        ):
+            for space in response.get("results", []):
+                space_url = os.path.join(
+                    self.confluence_client.host_url, space["_links"]["webui"][1:]
+                )
+                yield {
+                    "_id": space["id"],
+                    "type": "Space",
+                    "title": space["name"],
+                    "_timestamp": iso_utc(),
+                    "url": space_url,
+                }
+
+    async def fetch_documents(self, api_query):
+        """Get pages and blog posts with the help of REST APIs
+
+        Args:
+            api_query (str): Query parameter for the API call
+
+        Yields:
+            Dictionary: Page or blog post to be indexed
+            Integer: Number of attachments in a page/blogpost
+        """
+        async for response in self.confluence_client.paginated_api_call(
+            url_name=CONTENT,
+            api_query=api_query,
+        ):
+            documents = response.get("results", [])
+            attachment_count = 0
+            for document in documents:
+                updated_time = document["history"]["lastUpdated"]["when"]
+                if document.get("children").get("attachment"):
+                    attachment_count = document["children"]["attachment"]["size"]
+                document_url = os.path.join(
+                    self.confluence_client.host_url, document["_links"]["webui"][1:]
+                )
+                yield {
+                    "_id": document["id"],
+                    "type": document["type"],
+                    "_timestamp": updated_time,
+                    "title": document["title"],
+                    "space": document["space"]["name"],
+                    "body": document["body"]["storage"]["value"],
+                    "url": document_url,
+                }, attachment_count
+
+    async def fetch_attachments(
+        self, content_id, parent_name, parent_space, parent_type
+    ):
+        """Fetches all the attachments present in the given content (pages and blog posts)
+
+        Args:
+            content_id (str): Unique identifier of Confluence content (pages and blogposts)
+            parent_name (str): Page/Blog post where the attachment is present
+            parent_space (str): Confluence Space where the attachment is present
+            parent_type (str): Type of the parent (Blog post or Page)
+
+        Yields:
+            Dictionary: Confluence attachment on the given page or blog post
+            String: Download link to get the content of the attachment
+        """
+        async for response in self.confluence_client.paginated_api_call(
+            url_name=ATTACHMENT,
+            api_query=ATTACHMENT_QUERY,
+            id=content_id,
+        ):
+            attachments = response.get("results", [])
+            for attachment in attachments:
+                attachment_url = os.path.join(
+                    self.confluence_client.host_url,
+                    attachment["_links"]["webui"][1:],
+                )
+                yield {
+                    "type": attachment["type"],
+                    "title": attachment["title"],
+                    "_id": attachment["id"],
+                    "space": parent_space,
+                    parent_type: parent_name,
+                    "_timestamp": attachment["version"]["when"],
+                    "size": attachment["extensions"]["fileSize"],
+                    "url": attachment_url,
+                }, attachment["_links"]["download"]
+
+    async def download_attachment(self, url, attachment, timestamp=None, doit=False):
+        """Downloads the content of the given attachment in chunks using REST API call
+
+        Args:
+            url (str): url endpoint to download the attachment content
+            attachment (dict): Dictionary containing details of the attachment
+            timestamp (timestamp, optional): Timestamp of Confluence last modified. Defaults to None.
+            doit (boolean, optional): Boolean value for whether to get content or not. Defaults to False.
+
+        Returns:
+            Dictionary: Document of the attachment to be indexed.
+        """
+        attachment_size = int(attachment["size"])
+        if not (self.enable_content_extraction and doit and attachment_size):
+            return
+        attachment_name = attachment["title"]
+        file_extension = os.path.splitext(attachment_name)[-1]
+        if file_extension not in TIKA_SUPPORTED_FILETYPES:
+            logger.warning(f"{attachment_name} can't be extracted")
+            return
+
+        if attachment_size > FILE_SIZE_LIMIT:
+            logger.warning(
+                f"File size {attachment_size} of file {attachment_name} is larger than {FILE_SIZE_LIMIT} bytes. Discarding file content"
+            )
+            return
+        logger.debug(f"Downloading {attachment_name} of size {attachment_size} bytes")
+        document = {"_id": attachment["_id"], "_timestamp": attachment["_timestamp"]}
+        source_file_name = ""
+        async with NamedTemporaryFile(mode="wb", delete=False) as async_buffer:
+            async for response in self.confluence_client.api_call(
+                url=os.path.join(self.confluence_client.host_url, url),
+            ):
+                async for data in response.content.iter_chunked(n=CHUNK_SIZE):
+                    await async_buffer.write(data)
+            source_file_name = str(async_buffer.name)
+
+        logger.debug(
+            f"Download completed for file: {attachment_name}. Calling convert_to_b64"
+        )
+        await asyncio.to_thread(
+            convert_to_b64,
+            source=source_file_name,
+        )
+        async with aiofiles.open(file=source_file_name, mode="r") as target_file:
+            # base64 on macOS will add a EOL, so we strip() here
+            document["_attachment"] = (await target_file.read()).strip()
+        await remove(source_file_name)
+        logger.debug(f"Downloaded {attachment_name} for {attachment_size} bytes ")
+        return document
+
+    async def _attachment_coro(self, document):
+        """Coroutine to add attachments to Queue and download content
+
+        Args:
+            document (dict): Formatted document of page/blogpost
+        """
+        async for attachment, download_link in self.fetch_attachments(
+            content_id=document["_id"],
+            parent_name=document["title"],
+            parent_space=document["space"],
+            parent_type=document["type"],
+        ):
+            await self.queue.put(
+                (  # pyright: ignore
+                    attachment,
+                    partial(
+                        self.download_attachment,
+                        download_link[1:],
+                        copy(attachment),
+                    ),
+                )
+            )
+        await self.queue.put(END_SIGNAL)  # pyright: ignore
+
+    async def _space_coro(self):
+        """Coroutine to add spaces documents to Queue"""
+        async for space in self.fetch_spaces():
+            await self.queue.put((space, None))  # pyright: ignore
+        await self.queue.put(END_SIGNAL)  # pyright: ignore
+
+    async def _page_blog_coro(self, api_query):
+        """Coroutine to add pages/blogposts to Queue
+
+        Args:
+            api_query (str): API Query Parameters for fetching page/blogpost
+        """
+        async for document, attachment_count in self.fetch_documents(api_query):
+            await self.queue.put((document, None))  # pyright: ignore
+            if attachment_count > 0:
+                await self.fetchers.put(partial(self._attachment_coro, copy(document)))
+                self.fetcher_count += 1
+        await self.queue.put(END_SIGNAL)  # pyright: ignore
+
+    async def _consumer(self):
+        """Async generator to process entries of the queue
+
+        Yields:
+            dictionary: Documents from Confluence.
+        """
+        while self.fetcher_count > 0:
+            _, item = await self.queue.get()
+            if item == END_SIGNAL:
+                self.fetcher_count -= 1
+            else:
+                yield item
+
     async def get_docs(self, filtering=None):
-        # yield dummy document to make the chunked PR work, subsequent PR will replace it with actual implementation
-        yield {"_id": "123", "timestamp": iso_utc()}, None
+        """Executes the logic to fetch Confluence content in async manner.
+
+        Args:
+            filtering (Filtering): Object of Class Filtering
+        Yields:
+            dictionary: dictionary containing meta-data of the content.
+        """
+        await self.fetchers.put(self._space_coro)
+        await self.fetchers.put(
+            partial(self._page_blog_coro, f"type=blogpost&{CONTENT_QUERY}")
+        )
+        await self.fetchers.put(partial(self._page_blog_coro, CONTENT_QUERY))
+        self.fetcher_count += 3
+
+        async for item in self._consumer():
+            yield item
+        await self.fetchers.join()
--- connectors/sources/tests/test_confluence.py
+++ connectors/sources/tests/test_confluence.py
@@ -5,16 +5,151 @@
 #
 """Tests the Confluence database source class methods"""
 import ssl
-from unittest.mock import patch
+from copy import copy
+from unittest import mock
+from unittest.mock import AsyncMock, patch
 
 import pytest
+from aiohttp import StreamReader
+from freezegun import freeze_time
 
 from connectors.source import DataSourceConfiguration
 from connectors.sources.confluence import ConfluenceClient, ConfluenceDataSource
 from connectors.sources.tests.support import create_source
+from connectors.tests.commons import AsyncIterator
 from connectors.utils import ssl_context
 
 HOST_URL = "http://127.0.0.1:5000"
+CONTENT_QUERY = "limit=1&expand=children.attachment,history.lastUpdated,body.storage"
+RESPONSE_SPACE = {
+    "results": [
+        {
+            "id": 4554779,
+            "name": "DEMO",
+            "_links": {
+                "webui": "/spaces/DM",
+            },
+        }
+    ],
+    "start": 0,
+    "limit": 1,
+    "size": 1,
+    "_links": {},
+}
+
+RESPONSE_PAGE = {
+    "results": [
+        {
+            "id": 4779,
+            "title": "ES-scrum",
+            "type": "page",
+            "history": {"lastUpdated": {"when": "2023-01-24T04:07:19.672Z"}},
+            "children": {"attachment": {"size": 2}},
+            "body": {"storage": {"value": "This is a test page"}},
+            "space": {"name": "DEMO"},
+            "_links": {
+                "webui": "/spaces/~1234abc/pages/4779/ES-scrum",
+            },
+        }
+    ],
+    "start": 0,
+    "limit": 1,
+    "size": 1,
+    "_links": {},
+}
+
+EXPECTED_PAGE = {
+    "_id": 4779,
+    "type": "page",
+    "_timestamp": "2023-01-24T04:07:19.672Z",
+    "title": "ES-scrum",
+    "body": "This is a test page",
+    "space": "DEMO",
+    "url": f"{HOST_URL}/spaces/~1234abc/pages/4779/ES-scrum",
+}
+
+EXPECTED_SPACE = {
+    "_id": 4554779,
+    "type": "Space",
+    "title": "DEMO",
+    "_timestamp": "2023-01-24T04:07:19+00:00",
+    "url": f"{HOST_URL}/spaces/DM",
+}
+
+RESPONSE_ATTACHMENT = {
+    "results": [
+        {
+            "id": "att3637249",
+            "title": "demo.py",
+            "type": "attachment",
+            "version": {"when": "2023-01-03T09:24:50.633Z"},
+            "extensions": {"fileSize": 230},
+            "_links": {
+                "download": "/download/attachments/1113/demo.py?version=1&modificationDate=1672737890633&cacheVersion=1&api=v2",
+                "webui": "/pages/viewpageattachments.action?pageId=1113&preview=demo.py",
+            },
+        }
+    ],
+    "start": 0,
+    "limit": 1,
+    "size": 1,
+    "_links": {},
+}
+
+EXPECTED_ATTACHMENT = {
+    "_id": "att3637249",
+    "type": "attachment",
+    "_timestamp": "2023-01-03T09:24:50.633Z",
+    "title": "demo.py",
+    "size": 230,
+    "space": "DEMO",
+    "page": "ES-scrum",
+    "url": f"{HOST_URL}/pages/viewpageattachments.action?pageId=1113&preview=demo.py",
+}
+
+RESPONSE_CONTENT = "# This is the dummy file"
+
+EXPECTED_CONTENT = {
+    "_id": "att3637249",
+    "_timestamp": "2023-01-03T09:24:50.633Z",
+    "_attachment": "IyBUaGlzIGlzIHRoZSBkdW1teSBmaWxl",
+}
+
+EXPECTED_BLOG = {
+    "_id": 4779,
+    "type": "blogpost",
+    "_timestamp": "2023-01-24T04:07:19.672Z",
+    "title": "demo-blog",
+    "body": "This is a test blog",
+    "space": "DEMO",
+    "url": f"{HOST_URL}/spaces/~1234abc/blogposts/4779/demo-blog",
+}
+
+EXPECTED_BLOG_ATTACHMENT = {
+    "_id": "att3637249",
+    "type": "attachment",
+    "_timestamp": "2023-01-03T09:24:50.633Z",
+    "title": "demo.py",
+    "size": 230,
+    "space": "DEMO",
+    "blog": "demo-blog",
+    "url": f"{HOST_URL}/pages/viewpageattachments.action?pageId=1113&preview=demo.py",
+}
+
+
+class JSONAsyncMock(AsyncMock):
+    def __init__(self, json, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._json = json
+
+    async def json(self):
+        return self._json
+
+
+class StreamReaderAsyncMock(AsyncMock):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.content = StreamReader
 
 
 @pytest.mark.asyncio
@@ -161,7 +296,175 @@ def test_validate_configuration_for_ssl_enabled(patch_logger):
         source._validate_configuration()
 
 
-def test_get_storage_client():
+@freeze_time("2023-01-24T04:07:19")
+@pytest.mark.asyncio
+async def test_fetch_spaces():
+    # Setup
+    source = create_source(ConfluenceDataSource)
+
+    async_response = AsyncMock()
+    async_response.__aenter__ = AsyncMock(return_value=JSONAsyncMock(RESPONSE_SPACE))
+
+    with mock.patch("aiohttp.ClientSession.get", return_value=async_response):
+        async for response in source.fetch_spaces():
+            assert response == EXPECTED_SPACE
+
+
+@pytest.mark.asyncio
+async def test_fetch_documents():
+    # Setup
+    source = create_source(ConfluenceDataSource)
+    async_response = AsyncMock()
+    async_response.__aenter__ = AsyncMock(return_value=JSONAsyncMock(RESPONSE_PAGE))
+
+    # Execute
+    with mock.patch("aiohttp.ClientSession.get", return_value=async_response):
+        async for response, _ in source.fetch_documents(api_query=""):
+            assert response == EXPECTED_PAGE
+
+
+@pytest.mark.asyncio
+async def test_fetch_attachments():
+    # Setup
+    source = create_source(ConfluenceDataSource)
+    async_response = AsyncMock()
+    async_response.__aenter__ = AsyncMock(
+        return_value=JSONAsyncMock(RESPONSE_ATTACHMENT)
+    )
+
+    # Execute
+    with mock.patch("aiohttp.ClientSession.get", return_value=async_response):
+        async for response, _ in source.fetch_attachments(
+            content_id=1113,
+            parent_name="ES-scrum",
+            parent_space="DEMO",
+            parent_type="page",
+        ):
+            assert response == EXPECTED_ATTACHMENT
+
+
+@pytest.mark.asyncio
+async def test_download_attachment():
+    # Setup
+    source = create_source(ConfluenceDataSource)
+
+    async_response = AsyncMock()
+    async_response.__aenter__ = AsyncMock(return_value=StreamReaderAsyncMock())
+
+    # Execute
+    with mock.patch("aiohttp.ClientSession.get", return_value=async_response):
+        with mock.patch(
+            "aiohttp.StreamReader.iter_chunked",
+            return_value=AsyncIterator([bytes(RESPONSE_CONTENT, "utf-8")]),
+        ):
+            response = await source.download_attachment(
+                url="download/attachments/1113/demo.py?version=1&modificationDate=1672737890633&cacheVersion=1&api=v2",
+                attachment=EXPECTED_ATTACHMENT,
+                doit=True,
+            )
+            assert response == EXPECTED_CONTENT
+
+
+@pytest.mark.asyncio
+async def test_download_attachment_when_filesize_is_large_then_download_skips():
+    """Tests the download attachments method for file size greater than max limit."""
+    # Setup
+    source = create_source(ConfluenceDataSource)
+
+    async_response = AsyncMock()
+    async_response.__aenter__ = AsyncMock(return_value=StreamReaderAsyncMock())
+
+    # Execute
+
+    with mock.patch("aiohttp.ClientSession.get", return_value=async_response):
+        with mock.patch(
+            "aiohttp.StreamReader.iter_chunked",
+            return_value=AsyncIterator([bytes(RESPONSE_CONTENT, "utf-8")]),
+        ):
+            attachment = copy(EXPECTED_ATTACHMENT)
+            attachment["size"] = 23000000
+
+            response = await source.download_attachment(
+                url="download/attachments/1113/demo.py?version=1&modificationDate=1672737890633&cacheVersion=1&api=v2",
+                attachment=attachment,
+                doit=True,
+            )
+            assert response is None
+
+
+@pytest.mark.asyncio
+async def test_download_attachment_when_unsupported_filetype_used_then_fail_download_skips():
+    """Tests the download attachments method for file type is not supported"""
+    # Setup
+    source = create_source(ConfluenceDataSource)
+
+    async_response = AsyncMock()
+    async_response.__aenter__ = AsyncMock(return_value=StreamReaderAsyncMock())
+
+    with mock.patch("aiohttp.ClientSession.get", return_value=async_response):
+        with mock.patch(
+            "aiohttp.StreamReader.iter_chunked",
+            return_value=AsyncIterator([bytes(RESPONSE_CONTENT, "utf-8")]),
+        ):
+            attachment = copy(EXPECTED_ATTACHMENT)
+
+            attachment["title"] = "batch.mysy"
+
+            response = await source.download_attachment(
+                url="download/attachments/1113/demo.py?version=1&modificationDate=1672737890633&cacheVersion=1&api=v2",
+                attachment=attachment,
+                doit=True,
+            )
+            assert response is None
+
+
+@pytest.mark.asyncio
+@mock.patch.object(
+    ConfluenceDataSource, "fetch_spaces", return_value=AsyncIterator([EXPECTED_SPACE])
+)
+@mock.patch.object(
+    ConfluenceDataSource,
+    "fetch_documents",
+    side_effect=[
+        (AsyncIterator([[EXPECTED_PAGE, 1]])),
+        (AsyncIterator([[EXPECTED_BLOG, 1]])),
+    ],
+)
+@mock.patch.object(
+    ConfluenceDataSource,
+    "fetch_attachments",
+    side_effect=[
+        (AsyncIterator([[EXPECTED_ATTACHMENT, "download-url"]])),
+        (AsyncIterator([[EXPECTED_BLOG_ATTACHMENT, "download-url"]])),
+    ],
+)
+@mock.patch.object(
+    ConfluenceDataSource,
+    "download_attachment",
+    return_value=AsyncIterator([[EXPECTED_CONTENT]]),
+)
+async def test_get_docs(spaces_patch, pages_patch, attachment_patch, content_patch):
+    """Tests the get_docs method"""
+
+    # Setup
+    source = create_source(ConfluenceDataSource)
+    expected_responses = [
+        EXPECTED_SPACE,
+        EXPECTED_PAGE,
+        EXPECTED_BLOG,
+        EXPECTED_ATTACHMENT,
+        EXPECTED_BLOG_ATTACHMENT,
+    ]
+
+    # Execute
+    documents = []
+    async for item, _ in source.get_docs():
+        documents.append(item)
+
+    assert documents == expected_responses
+
+
+def test_get_session():
     """Test that the instance of session returned is always the same for the datasource class."""
     source = create_source(ConfluenceDataSource)
     first_instance = source.confluence_client._get_session()
