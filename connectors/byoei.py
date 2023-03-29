7863de856d6440053fc3ae669d0d94c54b6025f6
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2023-03-29 15:01:57 +0200

    Move content extraction check to byoei.py

--- connectors/byoei.py
+++ connectors/byoei.py
@@ -186,6 +186,7 @@ class Fetcher:
     - existing_ids: a list of existing Elasticsearch document ids found in the index
     - filter_: an instance of `Filter` to apply on the fetched document -- default: `None`
     - sync_rules_enabled: if `True`, we apply rules -- default: `False`
+    - content_extraction_enabled: if `True`, download content
     - display_every -- display a log every `display_every` doc -- default: `DEFAULT_DISPLAY_EVERY`
     - concurrent_downloads: -- concurrency level for downloads -- default: `DEFAULT_CONCURRENT_DOWNLOADS`
     """
@@ -197,6 +198,7 @@ class Fetcher:
         existing_ids,
         filter_=None,
         sync_rules_enabled=False,
+        content_extraction_enabled=False,
         display_every=DEFAULT_DISPLAY_EVERY,
         concurrent_downloads=DEFAULT_CONCURRENT_DOWNLOADS,
     ):
@@ -218,6 +220,7 @@ class Fetcher:
         self.basic_rule_engine = (
             BasicRuleEngine(parse(filter_.basic_rules)) if sync_rules_enabled else None
         )
+        self.content_extraction_enabled = content_extraction_enabled
         self.display_every = display_every
         self.concurrent_downloads = concurrent_downloads
 
@@ -282,7 +285,7 @@ class Fetcher:
                     # For these, we update the docs in any case.
                     if TIMESTAMP_FIELD in doc and ts == doc[TIMESTAMP_FIELD]:
                         # cancel the download
-                        if lazy_download is not None:
+                        if self.content_extraction_enabled and lazy_download is not None:
                             await lazy_download(doit=False)
                         continue
 
@@ -296,7 +299,7 @@ class Fetcher:
                         doc[TIMESTAMP_FIELD] = iso_utc()
 
                 # if we need to call lazy_download we push it in lazy_downloads
-                if lazy_download is not None:
+                if self.content_extraction_enabled and lazy_download is not None:
                     await lazy_downloads.put(
                         functools.partial(
                             self._deferred_index, lazy_download, doc_id, doc, operation
@@ -434,6 +437,7 @@ class ElasticServer(ESClient):
         pipeline,
         filter_=None,
         sync_rules_enabled=False,
+        content_extraction_enabled=True,
         options=None,
     ):
         """Performs a batch of `_bulk` calls, given a generator of documents
@@ -444,6 +448,7 @@ class ElasticServer(ESClient):
         - pipeline: ingest pipeline settings to pass to the bulk API
         - filter_: an instance of `Filter` to apply on the fetched document  -- default: `None`
         - sync_rules_enabled: if enabled, applies rules -- default: `False`
+        - content_extraction_enabled: if enabled, will download content -- default: `True`
         - options: dict of options (from `elasticsearch.bulk` in the config file)
         """
         if filter_ is None:
@@ -476,6 +481,7 @@ class ElasticServer(ESClient):
             existing_ids,
             filter_=filter_,
             sync_rules_enabled=sync_rules_enabled,
+            content_extraction_enabled=content_extraction_enabled,
             display_every=display_every,
             concurrent_downloads=concurrent_downloads,
         )
