f6ac4c43bf7bd583d9b48bf5f65a72b6360d53f3
Chenhui Wang <wangch079@outlook.com>
2023-04-04 16:45:06 +0800

    Add update_by_script method

--- connectors/tests/test_es_index.py
+++ connectors/tests/test_es_index.py
@@ -3,6 +3,8 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
+from unittest.mock import AsyncMock, Mock
+
 import pytest
 from elasticsearch import ApiError, ConflictError
 
@@ -180,6 +182,20 @@ async def test_update_with_concurrency_control(mock_responses):
         await index.update(doc_id, {}, if_seq_no=1, if_primary_term=1)
 
     await index.close()
+
+
+@pytest.mark.asyncio
+async def test_update_by_script():
+    doc_id = "1"
+    script = {"source": ""}
+    index = ESIndex(index_name, config)
+    index.client = Mock()
+    index.client.update = AsyncMock()
+
+    await index.update_by_script(doc_id, script)
+    index.client.update.assert_awaited_once_with(
+        index=index_name, id=doc_id, script=script
+    )
 
 
 @pytest.mark.asyncio
