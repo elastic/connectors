9cec45e9144681ef1cb0389cbe5e46737a6e2be4
Tim Grein <tim.grein@elastic.co>
2023-04-17 13:40:29 +0200

    "Hashing id..." shouldn't be treated as an argument

--- connectors/sync_job_runner.py
+++ connectors/sync_job_runner.py
@@ -235,7 +235,7 @@ class SyncJobRunner:
                 logger.debug(
                     f"Document with id '{doc_id}' with a size of '{doc_id_size}' bytes could not be ingested. "
                     f"Elasticsearch has an upper limit of '{ES_ID_SIZE_LIMIT}' bytes for the '_id' field.",
-                    "Hashing id...",
+                    f"Hashing id...",
                 )
 
                 hashed_id = self.source_klass.hash_id(doc_id)
