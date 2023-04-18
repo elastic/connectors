df53a6fdc5cde985be81d61eff04bf1d639fd7e0
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-04-18 17:08:24 +0200

    Use custom docker images for our CI

--- .buildkite/run_nigthly.sh
+++ .buildkite/run_nigthly.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 set -exuo pipefail
 
-# TODO: convert all this install in a docker image we can just use
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install ca-certificates curl gnupg lsb-release -y
-sudo mkdir -p /etc/apt/keyrings
-
-echo "Installing Docker & Docker Compose"
-ARCH=`dpkg --print-architecture`
-RELEASE=`lsb_release -cs`
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$ARCH signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $RELEASE stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo systemctl start docker
-
-# installs Python 3.10
-echo "Installing Python 3.10"
-sudo apt-get remove python3-pip python3-setuptools -y
-sudo DEBIAN_FRONTEND=noninteractive apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo TZ=UTC DEBIAN_FRONTEND=noninteractive apt install --no-install-recommends python3.10 python3.10-dev python3.10-venv -y
-
-
-curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3.10
-
 echo "Starting test task"
 BASEDIR=$(realpath $(dirname $0))
 ROOT=$(realpath $BASEDIR/../)
