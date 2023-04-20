048895d5037612f1cdefcec95dece45b3048bbf7
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-04-20 17:39:02 +0200

    Maybe this is faster?

--- .buildkite/run_dockerized_test.sh
+++ .buildkite/run_dockerized_test.sh
@@ -9,14 +9,9 @@ ROOT=$(realpath $BASEDIR/../)
 cd $ROOT
 
 /usr/bin/python3.10 -m venv .
-export PERF8_BIN=$ROOT/bin/perf8
 export PYTHON=$ROOT/bin/python
 export PIP=$ROOT/bin/pip
 export DATA_SIZE=small
 export VERSION=8.8.0-SNAPSHOT
 
-$PIP install -r requirements/tests.txt
-$PIP install -r requirements/x86_64.txt
-$PYTHON setup.py develop
-
-make ftest NAME=mongodb DATA_SIZE=small PYTHON=$ROOT/bin/python
+make ftest NAME=dir DATA_SIZE=small PYTHON=$ROOT/bin/python
