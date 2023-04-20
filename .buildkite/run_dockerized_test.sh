182d9960147fe7cbf3130c118c7e69a88791c4e2
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-04-20 14:38:01 +0200

    Maybe like that?

--- .buildkite/run_dockerized_test.sh
+++ .buildkite/run_dockerized_test.sh
@@ -3,6 +3,20 @@
 # TODO: convert all this install in a docker image we can just use
 set -exuo pipefail
 
-make test
-make lint
+BASEDIR=$(realpath $(dirname $0))
+ROOT=$(realpath $BASEDIR/../)
+
+cd $ROOT
+
+/usr/bin/python3.10 -m venv .
+export PERF8_BIN=$ROOT/bin/perf8
+export PYTHON=$ROOT/bin/python
+export PIP=$ROOT/bin/pip
+export DATA_SIZE=small
+export VERSION=8.8.0-SNAPSHOT
+
+$PIP install -r requirements/tests.txt
+$PIP install -r requirements/x86_64.txt
+$PYTHON setup.py develop
+
 make ftest NAME=dir DATA_SIZE=small PYTHON=$ROOT/bin/python
