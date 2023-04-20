5c3709bfa2085efeeaa38e1346ca012a526aad32
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-04-20 17:53:06 +0200

    Less text

--- .buildkite/run_dockerized_test.sh
+++ .buildkite/run_dockerized_test.sh
@@ -3,15 +3,4 @@
 # TODO: convert all this install in a docker image we can just use
 set -exuo pipefail
 
-BASEDIR=$(realpath $(dirname $0))
-ROOT=$(realpath $BASEDIR/../)
-
-cd $ROOT
-
-/usr/bin/python3.10 -m venv .
-export PYTHON=$ROOT/bin/python
-export PIP=$ROOT/bin/pip
-export DATA_SIZE=small
-export VERSION=8.8.0-SNAPSHOT
-
-make ftest NAME=dir DATA_SIZE=small PYTHON=$ROOT/bin/python
+make ftest NAME=dir DATA_SIZE=small
--- .buildkite/run_nigthly.sh
+++ .buildkite/run_nigthly.sh
@@ -6,7 +6,9 @@ ROOT=$(realpath $BASEDIR/../)
 
 cd $ROOT
 
+make install
+
 export PIP=$ROOT/bin/pip
 
 $PIP install py-spy
-connectors/tests/ftest.sh $1 yes
+PERF8=yes NAME=$1 DATA_SIZE=small make ftest
