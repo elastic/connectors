f1a60428d707c1e7af60525c3925b331a79cbfdd
Tarek Ziadé <tarek@ziade.org>
2023-04-28 14:57:49 -0700

    wrong pos

--- connectors/tests/ftest.sh
+++ connectors/tests/ftest.sh
@@ -53,7 +53,7 @@ then
     then
       $PERF8_BIN --refresh-rate $REFRESH_RATE -t $ROOT_DIR/perf8-report-$NAME --asyncstats --memray --psutil --psutil-max-rss $MAX_RSS --max-duration $MAX_DURATION --description $DESCRIPTION -c $ELASTIC_INGEST --debug & PID=$!
     else
-      $PERF8_BIN --refresh-rate $REFRESH_RATE -t $ROOT_DIR/perf8-report-$NAME --asyncstats --memray --psutil --psutil-max-rss $MAX_RSS --max-duration $MAX_DURATION -c $ELASTIC_INGEST --description $DESCRIPTION --debug & PID=$!
+      $PERF8_BIN --refresh-rate $REFRESH_RATE -t $ROOT_DIR/perf8-report-$NAME --asyncstats --memray --psutil --psutil-max-rss $MAX_RSS --max-duration $MAX_DURATION --description $DESCRIPTION -c $ELASTIC_INGEST --debug & PID=$!
     fi
 else
     $ELASTIC_INGEST --debug & PID=$!
