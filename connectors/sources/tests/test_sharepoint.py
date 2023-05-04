805b00529ade4a48bf2fff8018ef04f53e18b3df
akanshi-elastic <125074214+akanshi-elastic@users.noreply.github.com>
2023-05-04 20:18:19 +0530

    [SharePoint]Add Support for ingesting Site Pages (#787)
    * add support for aspx
    
    * break the get_site_pages_content into chunks
    
    * remove * from drive url
    
    * Empty commit
    
    * fix minor nits
    
    * create constant for selected field
    
    * move aspx to utils
    
    ---------
    
    Co-authored-by: Sean Story <sean.j.story@gmail.com>

--- connectors/sources/tests/test_sharepoint.py
+++ connectors/sources/tests/test_sharepoint.py
@@ -373,6 +373,7 @@ async def test_get_list_items():
         list_id="620070a1-ee50-4585-b6a7-0f6210b1a69d",
         site_url="/sites/enterprise/ctest",
         server_relative_url="/sites/enterprise/site1",
+        selected_field="",
     ):
         expected_response.append(item)
     # Assert
@@ -462,6 +463,7 @@ async def test_get_drive_items():
         list_id="620070a1-ee50-4585-b6a7-0f6210b1a69d",
         site_url="/sites/enterprise/ctest",
         server_relative_url=None,
+        selected_field="",
     ):
         expected_response.append(item)
     # Assert
@@ -529,6 +531,51 @@ async def test_get_docs_drive_items():
         "LastItemModifiedDate": "2023-03-19T05:02:52Z",
         "ParentWebUrl": "/sites/enterprise/ctest/_api",
         "Title": "HelloWorld",
+    }
+    drive_content_response = {
+        "File": {
+            "Length": "3356",
+            "Name": "Home.aspx",
+            "ServerRelativeUrl": "/sites/enterprise/ctest/SitePages/Home.aspx",
+            "TimeCreated": "2022-05-02T07:20:33Z",
+            "TimeLastModified": "2022-05-02T07:20:34Z",
+            "Title": "Home.aspx",
+        },
+        "Folder": {"__deferred": {}},
+        "Modified": "2022-05-02T07:20:35Z",
+        "GUID": "111111122222222-c77f-4ed3-84ef-8a4dd87c80d0",
+        "Length": "3356",
+        "item_type": "File",
+    }
+    actual_response = []
+    source.sharepoint_client._fetch_data_with_query = Mock(return_value=AsyncIter([]))
+    source.sharepoint_client.get_lists = Mock(
+        return_value=AsyncIter([item_content_response])
+    )
+    source.sharepoint_client.get_drive_items = Mock(
+        return_value=AsyncIter((drive_content_response, None))
+    )
+    # Execute
+    async for document, _ in source.get_docs():
+        actual_response.append(document)
+    # Assert
+    assert len(actual_response) == 2
+
+
+@pytest.mark.asyncio
+async def test_get_docs_drive_items_for_web_pages():
+    # Setup
+    source = create_source(SharepointDataSource)
+    item_content_response = {
+        "RootFolder": {
+            "ServerRelativeUrl": "/sites/enterprise/ctest/_api",
+        },
+        "Created": "2023-03-19T05:02:52Z",
+        "BaseType": 1,
+        "Id": "f764b597-ed44-49be-8867-f8e9ca5d0a6e",
+        "LastItemModifiedDate": "2023-03-19T05:02:52Z",
+        "ParentWebUrl": "/sites/enterprise/ctest/_api",
+        "Title": "Site Pages",
     }
     drive_content_response = {
         "File": {
@@ -985,3 +1032,131 @@ def test_get_session():
     first_instance = source.sharepoint_client._get_session()
     second_instance = source.sharepoint_client._get_session()
     assert first_instance is second_instance
+
+
+@pytest.mark.asyncio
+async def test_get_site_pages_content():
+    # Setup
+    source = create_source(SharepointDataSource)
+    EXPECTED_ATTACHMENT = {
+        "id": 1,
+        "server_relative_url": "/url",
+        "_timestamp": "2022-06-20T10:37:44Z",
+        "size": "11",
+        "type": "sites",
+        "file_name": "dummy.pdf",
+    }
+    RESPONSE_DATA = {"WikiField": "This is a dummy sharepoint body response"}
+    EXPECTED_CONTENT = {
+        "_id": 1,
+        "_attachment": "VGhpcyBpcyBhIGR1bW15IHNoYXJlcG9pbnQgYm9keSByZXNwb25zZQ==",
+        "_timestamp": "2022-06-20T10:37:44Z",
+    }
+    # Execute
+    response_content = await source.sharepoint_client.get_site_pages_content(
+        document=EXPECTED_ATTACHMENT,
+        site_url="/site",
+        list_response=RESPONSE_DATA,
+        doit=True,
+    )
+    # Assert
+    assert response_content == EXPECTED_CONTENT
+
+
+async def coroutine_generator(item):
+    """create a method for returning fake coroutine value for
+    Args:
+        item: Value for converting into coroutine
+    """
+    return item
+
+
+@pytest.mark.asyncio
+async def test_get_site_pages_content_when_canvascontent_is_not_none():
+    # Setup
+    source = create_source(SharepointDataSource)
+    source.sharepoint_client.is_cloud = True
+    EXPECTED_ATTACHMENT = {
+        "id": 1,
+        "server_relative_url": "/url",
+        "_timestamp": "2022-06-20T10:37:44Z",
+        "size": "11",
+        "type": "sites",
+        "file_name": "dummy.pdf",
+    }
+    source.sharepoint_client.get_site_page_for_online = Mock(
+        return_value=coroutine_generator("<div>dummy</div>")
+    )
+    # Execute
+    response_content = await source.sharepoint_client.get_site_pages_content(
+        document=EXPECTED_ATTACHMENT,
+        site_url="/site",
+        list_response={"CanvasContent1": None},
+        doit=True,
+    )
+    # Assert
+    assert response_content["_attachment"] == "PGRpdj5kdW1teTwvZGl2Pg=="
+
+
+@pytest.mark.asyncio
+async def test_get_site_pages_content_when_canvascontent_is_none():
+    # Setup
+    source = create_source(SharepointDataSource)
+    source.sharepoint_client.is_cloud = True
+    EXPECTED_ATTACHMENT = {
+        "title": "Home.aspx",
+        "type": "File",
+        "size": "10000000",
+    }
+    source.sharepoint_client.get_site_page_for_online = Mock(
+        return_value=coroutine_generator(None)
+    )
+    # Execute
+    response_content = await source.sharepoint_client.get_site_pages_content(
+        document=EXPECTED_ATTACHMENT,
+        site_url="/site",
+        list_response={"CanvasContent1": None},
+        doit=True,
+    )
+    # Assert
+    assert response_content is None
+
+
+@pytest.mark.asyncio
+async def test_get_site_pages_content_for_is_cloud_when_size_big():
+    """Test the get site pages content method when is_cloud True when size is greater than 10MB"""
+    # Setup
+    source = create_source(SharepointDataSource)
+    source.sharepoint_client.is_cloud = True
+    async_response = {"value": [{"CanvasContent1": None}]}
+    EXPECTED_ATTACHMENT = {
+        "title": "Home.aspx",
+        "type": "File",
+        "size": "1000000000",
+    }
+    source.sharepoint_client._api_call = Mock(return_value=AsyncIter(async_response))
+    # Execute
+    response_content = await source.sharepoint_client.get_site_pages_content(
+        document=EXPECTED_ATTACHMENT,
+        site_url="/site",
+        list_response={},
+        doit=True,
+    )
+    # Assert
+    assert response_content is None
+
+
+@pytest.mark.asyncio
+async def test_get_site_pages_content_for_wikifiled_none():
+    # Setup
+    source = create_source(SharepointDataSource)
+    EXPECTED_ATTACHMENT = {"title": "Home.aspx", "type": "File", "size": "1000000"}
+    # Execute
+    response_content = await source.sharepoint_client.get_site_pages_content(
+        document=EXPECTED_ATTACHMENT,
+        site_url="/site",
+        list_response={"WikiField": None},
+        doit=True,
+    )
+    # Assert
+    assert response_content is None
