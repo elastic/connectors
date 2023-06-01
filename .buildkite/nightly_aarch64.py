fbde19af96f8fb8a42e91260d8a978ea9621e277
Tarek ZIADE <tarek@ziade.org>
2023-06-01 08:24:17 -0700

    use ubuntu arm

--- .buildkite/nightly_aarch64.py
+++ .buildkite/nightly_aarch64.py
@@ -6,9 +6,10 @@ agents:
   provider: "gcp"
   machineType: "t2a-standard-8"
   useVault: true
+  image: "ubuntu-2204-lts-arm64"
 """
 
-#image: family/enterprise-search-ubuntu-2204-connectors-py
+# image: family/enterprise-search-ubuntu-2204-connectors-py
 
 
 with open(os.path.join(os.path.dirname(__file__), 'nightly_steps.yml')) as f:
