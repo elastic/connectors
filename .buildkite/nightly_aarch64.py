f8fec2a5ebf9460879560ebd15c4611f564aeaa3
Tarek ZIADE <tarek@ziade.org>
2023-06-02 09:07:55 -0700

    try another image

--- .buildkite/nightly_aarch64.py
+++ .buildkite/nightly_aarch64.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
 import os
+import platform
 
-_AGENTS = """\
+machine = platform.uname().machine
+
+if machine in ("arm64", "aarch64"):
+    _AGENTS = """\
 agents:
   provider: aws
   instanceType: m6g.xlarge
-  imagePrefix: enterprise-search-ubuntu-2204-aarch64-connectors-py
+  imagePrefix: drivah-ubuntu-2204-aarch64
+"""
+else:
+    _AGENTS = """\
+agents:
+  provider: "gcp"
+  machineType: "n1-standard-8"
+  useVault: true
+  image: family/enterprise-search-ubuntu-2204-connectors-py
 """
 
-with open(os.path.join(os.path.dirname(__file__), 'nightly_steps.yml')) as f:
+#imagePrefix: enterprise-search-ubuntu-2204-aarch64-connectors-py
+
+with open(os.path.join(os.path.dirname(__file__), "nightly_steps.yml")) as f:
     steps = f.read().strip()
 
 
