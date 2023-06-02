6a29ba33bbf2eef123896bfe62fc4a63c17d63df
Tarek ZIADE <tarek@ziade.org>
2023-06-02 11:27:35 -0700

    needs to stay aarch64 specific

--- .buildkite/nightly_aarch64.py
+++ .buildkite/nightly_aarch64.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
 import os
-import platform
 
-machine = platform.uname().machine
 
-if machine in ("arm64", "aarch64"):
-    _AGENTS = """\
+_AGENTS = """\
 agents:
   provider: aws
   instanceType: m6g.xlarge
   imagePrefix: enterprise-search-ubuntu-2204-aarch64-connectors-py
-"""
-else:
-    _AGENTS = """\
-agents:
-  provider: "gcp"
-  machineType: "n1-standard-8"
-  useVault: true
-  image: family/enterprise-search-ubuntu-2204-connectors-py
 """
 
 with open(os.path.join(os.path.dirname(__file__), "nightly_steps.yml")) as f:
