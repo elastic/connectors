6f47b488d4b991358c3684b4399ac2ee9def29cf
moxarth-elastic <moxarth.rathod@elastic.co>
2023-06-05 11:59:31 +0530

    Implement indexing feature

--- connectors/sources/dropbox.py
+++ connectors/sources/dropbox.py
@@ -5,22 +5,38 @@
 #
 """Dropbox source module responsible to fetch documents from Dropbox online.
 """
-from functools import cached_property
+import asyncio
+import os
+from functools import cached_property, partial
 
+import aiofiles
 import dropbox
 import requests
+from aiofiles.os import remove
+from aiofiles.tempfile import NamedTemporaryFile
 from dropbox.exceptions import ApiError, AuthError, BadInputError
+from dropbox.files import FileMetadata
+from dropbox.sharing import FileLinkMetadata, SharedFileMetadata
 
 from connectors.logger import logger
 from connectors.source import BaseDataSource, ConfigurableFieldValueError
-from connectors.utils import CancellableSleeps, iso_utc
+from connectors.utils import (
+    TIKA_SUPPORTED_FILETYPES,
+    CancellableSleeps,
+    convert_to_b64,
+    iso_utc,
+)
 
 RETRY_COUNT = 3
+FILE_SIZE_LIMIT = 10485760  # Size in Bytes
+MAX_CONCURRENT_DOWNLOADS = 100
+LIMIT = 300  # Limit for fetching shared files per call
+PAPER = "paper"
+FILE = "File"
+FOLDER = "Folder"
 
 
 class DropboxClient:
-    """Dropbox client to handle API calls made to Dropbox"""
-
     def __init__(self, configuration):
         self._sleeps = CancellableSleeps()
         self.configuration = configuration
@@ -50,6 +66,54 @@ class DropboxClient:
     def check_path(self):
         return self._connection.files_get_metadata(path=self.path)
 
+    async def _pagination_wrapper(self, response, break_property, pagination_call):
+        while True:
+            for entry in response.entries:  # pyright: ignore
+                yield entry
+            if not getattr(response, break_property):
+                break
+            response = await asyncio.to_thread(
+                partial(
+                    pagination_call,
+                    cursor=response.cursor,  # pyright: ignore
+                )
+            )
+
+    async def get_files_folders(self, path):
+        response = await asyncio.to_thread(
+            partial(
+                self._connection.files_list_folder,
+                path=path,
+                recursive=True,
+            )
+        )
+        async for result in self._pagination_wrapper(
+            response=response,
+            break_property="has_more",
+            pagination_call=self._connection.files_list_folder_continue,
+        ):
+            yield result
+
+    async def get_shared_files(self):
+        shared_files = await asyncio.to_thread(
+            self._connection.sharing_list_received_files, limit=LIMIT
+        )
+        async for result in self._pagination_wrapper(
+            response=shared_files,
+            break_property="cursor",
+            pagination_call=self._connection.sharing_list_received_files_continue,
+        ):
+            yield result
+
+    def download_files(self, path):
+        return self._connection.files_download(path=path)
+
+    def download_paper_files(self, path):
+        return self._connection.files_export(path=path, export_format="markdown")
+
+    def download_shared_files(self, url):
+        return self._connection.sharing_get_shared_link_file(url=url)
+
     def close(self):
         self._sleeps.cancel()
         if self._session is not None:
@@ -71,6 +135,7 @@ class DropboxDataSource(BaseDataSource):
         """
         super().__init__(configuration=configuration)
         self.dropbox_client = DropboxClient(configuration=configuration)
+        self.concurrent_downloads = self.configuration["concurrent_downloads"]
 
     @classmethod
     def get_default_configuration(cls):
@@ -120,6 +185,19 @@ class DropboxDataSource(BaseDataSource):
                 "value": RETRY_COUNT,
                 "validations": [{"type": "less_than", "constraint": 10}],
             },
+            "concurrent_downloads": {
+                "default_value": MAX_CONCURRENT_DOWNLOADS,
+                "display": "numeric",
+                "label": "Maximum concurrent downloads",
+                "order": 6,
+                "required": False,
+                "type": "int",
+                "ui_restrictions": ["advanced"],
+                "validations": [
+                    {"type": "less_than", "constraint": MAX_CONCURRENT_DOWNLOADS + 1}
+                ],
+                "value": MAX_CONCURRENT_DOWNLOADS,
+            },
         }
 
     async def validate_config(self):
@@ -151,6 +229,14 @@ class DropboxDataSource(BaseDataSource):
         except Exception as exception:
             raise Exception(f"Something went wrong. Error: {exception}")
 
+    def tweak_bulk_options(self, options):
+        """Tweak bulk options as per concurrent downloads support by dropbox
+
+        Args:
+            options (dictionary): Config bulker options
+        """
+        options["concurrent_downloads"] = self.concurrent_downloads
+
     async def close(self):
         self.dropbox_client.close()
 
@@ -164,8 +250,130 @@ class DropboxDataSource(BaseDataSource):
             )
         except AuthError:
             raise ConfigurableFieldValueError("Configured Refresh Token is invalid")
+        except Exception:
+            raise
+
+    async def get_content(self, attachment, response=None, timestamp=None, doit=False):
+        """Extracts the content for allowed file types.
+
+        Args:
+            attachment (dictionary): Formatted attachment document.
+            response (object): Response of a shared file
+            timestamp (timestamp, optional): Timestamp of attachment last modified. Defaults to None.
+            doit (boolean, optional): Boolean value for whether to get content or not. Defaults to False.
+
+        Returns:
+            dictionary: Content document with _id, _timestamp and attachment content
+        """
+        attachment_size = int(attachment.size)
+        if not (doit and attachment_size > 0):
+            return
+
+        attachment_name = attachment.name
+        attachment_extension = os.path.splitext(attachment_name)[-1]
+
+        if attachment_extension == "":
+            logger.warning(
+                f"Files without extension are not supported by TIKA, skipping {attachment_name}."
+            )
+            return
+
+        elif attachment_extension not in TIKA_SUPPORTED_FILETYPES:
+            logger.warning(
+                f"Files with the extension {attachment_extension} are not supported by TIKA, skipping {attachment_name}."
+            )
+            return
+
+        if attachment_size > FILE_SIZE_LIMIT:
+            logger.warning(
+                f"File size {attachment_size} of file {attachment_name} is larger than {FILE_SIZE_LIMIT} bytes. Discarding file content"
+            )
+            return
+
+        logger.debug(f"Downloading {attachment_name}")
+
+        document = {
+            "_id": attachment.id,
+            "_timestamp": attachment.server_modified,
+        }
+        temp_filename = ""
+        async with NamedTemporaryFile(mode="wb", delete=False) as async_buffer:
+            if isinstance(attachment, FileLinkMetadata):
+                await async_buffer.write(response.content)
+            elif attachment.is_downloadable:
+                _, data = self.dropbox_client.download_files(  # pyright: ignore
+                    path=attachment.path_display
+                )
+                await async_buffer.write(data.content)
+            elif attachment.name.split(".")[-1] == PAPER:
+                _, data = self.dropbox_client.download_paper_files(  # pyright: ignore
+                    path=attachment.path_display
+                )
+                await async_buffer.write(data.content)
+            else:
+                logger.info(f"Skipping the download of a file: {attachment_name}")
+
+            temp_filename = str(async_buffer.name)
+
+        logger.debug(f"Calling convert_to_b64 for file : {attachment_name}")
+        await asyncio.to_thread(convert_to_b64, source=temp_filename)
+        async with aiofiles.open(file=temp_filename, mode="r") as async_buffer:
+            # base64 on macOS will add a EOL, so we strip() here
+            document["_attachment"] = (await async_buffer.read()).strip()
+        try:
+            await remove(temp_filename)
         except Exception as exception:
-            raise Exception(f"Something went wrong. Error: {exception}")
+            logger.warning(
+                f"Could not remove file from: {temp_filename}. Error: {exception}"
+            )
+        return document
+
+    def _format_datetime(self, datetime):
+        return datetime.strftime("%Y-%m-%dT%H:%M:%S")
+
+    def _prepare_doc(self, response, shared_metadata=None):
+        is_file = isinstance(response, FileMetadata)
+        timestamp = (
+            self._format_datetime(datetime=response.server_modified)
+            if is_file
+            else iso_utc()
+        )
+        if isinstance(response, SharedFileMetadata):
+            return {
+                "_id": response.id,
+                "type": FILE,
+                "name": response.name,
+                "file path": response.path_display,
+                "url": response.preview_url,
+                "size": shared_metadata.size,
+                "_timestamp": self._format_datetime(datetime=response.time_invited),
+            }
+        return {
+            "_id": response.id,
+            "type": FILE if is_file else FOLDER,
+            "name": response.name,
+            "file path": response.path_display,
+            "size": response.size if is_file else 0,
+            "_timestamp": timestamp,
+        }
+
+    async def _fetch_files_folders(self):
+        async for response in self.dropbox_client.get_files_folders(
+            path=self.dropbox_client.path
+        ):
+            yield response, self._prepare_doc(response=response)
+
+    async def _fetch_shared_files(self):
+        async for response in self.dropbox_client.get_shared_files():
+            (
+                metadata,
+                shared_response,
+            ) = self.dropbox_client.download_shared_files(  # pyright: ignore
+                url=response.preview_url
+            )
+            yield metadata, shared_response, self._prepare_doc(
+                response=response, shared_metadata=metadata
+            )
 
     async def get_docs(self, filtering=None):
         """Executes the logic to fetch dropbox objects in async manner
@@ -176,4 +384,13 @@ class DropboxDataSource(BaseDataSource):
         Yields:
             dictionary: dictionary containing meta-data of the files.
         """
-        yield {"_id": "123", "_timestamp": iso_utc()}, None
+        async for response, document in self._fetch_files_folders():
+            if isinstance(response, FileMetadata):
+                yield document, partial(self.get_content, attachment=response)
+            else:
+                yield document, None
+
+        async for metadata, shared_response, document in self._fetch_shared_files():
+            yield document, partial(
+                self.get_content, attachment=metadata, response=shared_response
+            )
--- tests/sources/test_dropbox.py
+++ tests/sources/test_dropbox.py
@@ -4,17 +4,151 @@
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
 """Tests the Dropbox source class methods"""
-from unittest.mock import AsyncMock, patch
+import datetime
+from copy import copy
+from unittest.mock import AsyncMock, MagicMock, Mock, patch
 
 import pytest
 from dropbox.exceptions import ApiError, AuthError, BadInputError
+from dropbox.files import FileMetadata, FolderMetadata, ListFolderResult
+from dropbox.sharing import (
+    FileLinkMetadata,
+    FolderPolicy,
+    ListFilesResult,
+    SharedFileMetadata,
+)
+from freezegun import freeze_time
 
 from connectors.source import ConfigurableFieldValueError, DataSourceConfiguration
 from connectors.sources.dropbox import DropboxDataSource
+from tests.commons import AsyncIterator
 from tests.sources.support import create_source
 
 PATH = "/"
 DUMMY_VALUES = "abc#123"
+
+MOCK_FILE_DATA = FileMetadata(
+    id="file_1",
+    name="test_file.txt",
+    path_display="/test/test_file.txt",
+    is_downloadable=True,
+    server_modified=datetime.datetime(2023, 1, 1, 6, 6, 6),
+    size=200,
+)
+
+MOCK_PAPER_FILE_DATA = FileMetadata(
+    id="paper_1",
+    name="dummy_file.paper",
+    path_display="/test/dummy_file.paper",
+    is_downloadable=False,
+    server_modified=datetime.datetime(2023, 1, 1, 6, 6, 6),
+    size=200,
+)
+
+MOCK_FOLDER_DATA = FolderMetadata(
+    id="folder_1",
+    name="test",
+    path_display="/test",
+)
+
+MOCK_SHARED_FILE_DATA_1 = SharedFileMetadata(
+    id="id:shared_1",
+    name="shared_file_1.py",
+    path_display="/shared/shared_file_1.py",
+    preview_url="https://www.dropbox.com/scl/fi/12345/shared_file_1.py",
+    time_invited=datetime.datetime(2023, 1, 1, 6, 6, 6),
+    policy=FolderPolicy(),
+)
+
+MOCK_SHARED_FILE_DATA_2 = SharedFileMetadata(
+    id="id:shared_2",
+    name="shared_file_2.py",
+    path_display="/shared/shared_file_2.py",
+    preview_url="https://www.dropbox.com/scl/fi/12345/shared_file_2.py",
+    time_invited=datetime.datetime(2023, 1, 1, 6, 6, 6),
+    policy=FolderPolicy(),
+)
+
+FILE_LINK_METADATA = FileLinkMetadata(
+    id="id:shared_1",
+    name="shared_file_1.py",
+    size=200,
+    server_modified=datetime.datetime(2023, 1, 1, 6, 6, 6),
+)
+
+EXPECTED_FILE = {
+    "_id": "file_1",
+    "type": "File",
+    "name": "test_file.txt",
+    "file path": "/test/test_file.txt",
+    "size": 200,
+    "_timestamp": "2023-01-01T06:06:06",
+}
+
+EXPECTED_FOLDER = {
+    "_id": "folder_1",
+    "type": "Folder",
+    "name": "test",
+    "file path": "/test",
+    "size": 0,
+    "_timestamp": "2023-01-01T06:06:06+00:00",
+}
+
+EXPECTED_SHARED_FILE_1 = {
+    "_id": "id:shared_1",
+    "type": "File",
+    "name": "shared_file_1.py",
+    "file path": "/shared/shared_file_1.py",
+    "url": "https://www.dropbox.com/scl/fi/12345/shared_file_1.py",
+    'size': 200,
+    "_timestamp": "2023-01-01T06:06:06",
+}
+
+EXPECTED_SHARED_FILE_2 = {
+    "_id": "id:shared_2",
+    "type": "File",
+    "name": "shared_file_2.py",
+    "file path": "/shared/shared_file_2.py",
+    "url": "https://www.dropbox.com/scl/fi/12345/shared_file_2.py",
+    'size': 200,
+    "_timestamp": "2023-01-01T06:06:06",
+}
+
+EXPECTED_CONTENT_FOR_TEXT_FILE = {
+    "_id": "file_1",
+    "_timestamp": datetime.datetime(2023, 1, 1, 6, 6, 6),
+    "_attachment": "VGhpcyBpcyBhIHRleHQgY29udGVudA==",
+}
+
+EXPECTED_CONTENT_FOR_PAPER_FILE = {
+    "_id": "paper_1",
+    "_timestamp": datetime.datetime(2023, 1, 1, 6, 6, 6),
+    "_attachment": "IyBUaGlzIGlzIGEgbWFya2Rvd24gY29udGVudA==",
+}
+
+MOCK_FILES_LIST_FOLDER = ListFolderResult(
+    entries=[MOCK_FILE_DATA], has_more=True, cursor="cursor"
+)
+
+MOCK_FILES_LIST_FOLDER_CONTINUE = ListFolderResult(
+    entries=[MOCK_FOLDER_DATA], has_more=False, cursor=None
+)
+
+MOCK_SHARING_LIST_RECEIVED_FILES = ListFilesResult(
+    entries=[MOCK_SHARED_FILE_DATA_1],
+    cursor="cursor",
+)
+
+MOCK_SHARING_LIST_RECEIVED_FILES_CONTINUE = ListFilesResult(
+    entries=[MOCK_SHARED_FILE_DATA_2], cursor=None
+)
+
+
+class MockResponse:
+    def __init__(self):
+        self.name = "test name"
+        self.content = b"test content"
+        self.size = 200
 
 class MockError:
     def is_path(self):
@@ -120,6 +254,27 @@ async def test_validate_configuration_with_invalid_refresh_token_then_raise_exce
 
 
 @pytest.mark.asyncio
+async def test_validate_config_with_invalid_concurrent_downloads():
+    source = create_source(DropboxDataSource, concurrent_downloads=1000)
+
+    with pytest.raises(
+        ConfigurableFieldValueError,
+        match="Field validation errors: 'Maximum concurrent downloads' value '1000' should be less than 101.",
+    ):
+        await source.validate_config()
+
+
+def test_tweak_bulk_options():
+    source = create_source(DropboxDataSource)
+
+    source.concurrent_downloads = 10
+    options = {"concurrent_downloads": 5}
+
+    source.tweak_bulk_options(options)
+    assert options["concurrent_downloads"] == 10
+
+
+@pytest.mark.asyncio
 async def test_ping():
     source = create_source(DropboxDataSource)
     with patch.object(
@@ -183,3 +338,152 @@ async def test_close_without_client_session():
 
     await source.close()
     assert source.dropbox_client._session is None
+
+
+@freeze_time("2023-01-01T06:06:06")
+@pytest.mark.asyncio
+async def test_fetch_files_folders():
+    source = create_source(DropboxDataSource)
+    source.dropbox_client.path = "/test"
+
+    mock_connection = Mock()
+    source.dropbox_client._connection = mock_connection
+
+    source.dropbox_client._connection.files_list_folder = Mock(
+        return_value=MOCK_FILES_LIST_FOLDER
+    )
+
+    source.dropbox_client._connection.files_list_folder_continue = Mock(
+        return_value=MOCK_FILES_LIST_FOLDER_CONTINUE
+    )
+
+    expected_response = [EXPECTED_FILE, EXPECTED_FOLDER]
+    actual_response = []
+    async for _, result in source._fetch_files_folders():
+        actual_response.append(result)
+
+    assert actual_response == expected_response
+
+
+@pytest.mark.asyncio
+async def test_fetch_shared_files():
+    source = create_source(DropboxDataSource)
+
+    mock_connection = Mock()
+    source.dropbox_client._connection = mock_connection
+
+    source.dropbox_client._connection.sharing_list_received_files = Mock(
+        return_value=MOCK_SHARING_LIST_RECEIVED_FILES
+    )
+
+    source.dropbox_client._connection.sharing_list_received_files_continue = (
+        Mock(return_value=MOCK_SHARING_LIST_RECEIVED_FILES_CONTINUE)
+    )
+
+    source.dropbox_client._connection.sharing_get_shared_link_file = Mock(
+        return_value=(FILE_LINK_METADATA, MockResponse())
+    )
+
+    expected_response = [EXPECTED_SHARED_FILE_1, EXPECTED_SHARED_FILE_2]
+    actual_response = []
+    async for _, _, result in source._fetch_shared_files():
+        actual_response.append(result)
+
+    assert actual_response == expected_response
+
+
+@pytest.mark.asyncio
+async def test_get_content_with_text_file():
+    source = create_source(DropboxDataSource)
+
+    mock_connection = Mock()
+    source.dropbox_client._connection = mock_connection
+
+    source.dropbox_client._connection.files_download.return_value = (
+        FileMetadata("/test/test_file.txt"),
+        MagicMock(content=b"This is a text content"),
+    )
+
+    response = await source.get_content(attachment=MOCK_FILE_DATA, doit=True)
+    assert response == EXPECTED_CONTENT_FOR_TEXT_FILE
+
+
+@pytest.mark.asyncio
+async def test_get_content_with_paper_file():
+    source = create_source(DropboxDataSource)
+
+    mock_connection = Mock()
+    source.dropbox_client._connection = mock_connection
+
+    source.dropbox_client._connection.files_export.return_value = (
+        FileMetadata("/test/dummy_file.paper"),
+        MagicMock(content=b"# This is a markdown content"),
+    )
+
+    response = await source.get_content(attachment=MOCK_PAPER_FILE_DATA, doit=True)
+    assert response == EXPECTED_CONTENT_FOR_PAPER_FILE
+
+
+@pytest.mark.asyncio
+async def test_get_content_with_file_size_zero():
+    source = create_source(DropboxDataSource)
+
+    mock_file_with_size_zero = copy(MOCK_FILE_DATA)
+    mock_file_with_size_zero.size = 0
+    response = await source.get_content(attachment=mock_file_with_size_zero, doit=True)
+    assert response is None
+
+
+@pytest.mark.asyncio
+async def test_get_content_with_large_file_size():
+    source = create_source(DropboxDataSource)
+
+    mock_file_with_large_size = copy(MOCK_FILE_DATA)
+    mock_file_with_large_size.size = 23000000
+    response = await source.get_content(attachment=mock_file_with_large_size, doit=True)
+    assert response is None
+
+
+@pytest.mark.asyncio
+async def test_get_content_with_unsupported_tika_file_type_then_skip():
+    source = create_source(DropboxDataSource)
+
+    mock_file_with_unsupported_tika_extension = copy(MOCK_FILE_DATA)
+    mock_file_with_unsupported_tika_extension.name = "screenshot.png"
+    response = await source.get_content(
+        attachment=mock_file_with_unsupported_tika_extension, doit=True
+    )
+    assert response is None
+
+
+@pytest.mark.asyncio
+async def test_get_content_without_extension_then_skip():
+    source = create_source(DropboxDataSource)
+
+    mock_file_without_extension = copy(MOCK_FILE_DATA)
+    mock_file_without_extension.name = "ssh-new"
+    response = await source.get_content(
+        attachment=mock_file_without_extension, doit=True
+    )
+    assert response is None
+
+
+@pytest.mark.asyncio
+async def test_get_docs():
+    source = create_source(DropboxDataSource)
+
+    source._fetch_files_folders = AsyncIterator(
+        [(MOCK_FILE_DATA, EXPECTED_FILE), (MOCK_FOLDER_DATA, EXPECTED_FOLDER)]
+    )
+
+    source._fetch_shared_files = AsyncIterator(
+        [(FILE_LINK_METADATA, MockResponse(), EXPECTED_SHARED_FILE_1)]
+    )
+
+    source.get_content = AsyncMock(return_value=EXPECTED_CONTENT_FOR_TEXT_FILE)
+
+    response = []
+    async for document, _ in source.get_docs():
+        response.append(document)
+
+    assert response == [EXPECTED_FILE, EXPECTED_FOLDER, EXPECTED_SHARED_FILE_1]
