499ffb5fd2ecd1467f93b10d848c04088b330ebc
Sean Story <sean.j.story@gmail.com>
2023-06-09 10:13:10 -0500

    Fix typo
    introduced in https://github.com/elastic/enterprise-search-team/issues/4733

--- tests/fake_sources.py
+++ tests/fake_sources.py
@@ -157,7 +157,7 @@ class LargeFakeSource(FakeSource):
             doc_id = str(i + 1)
             yield {"_id": doc_id, "data": "big" * 4 * 1024}, partial(self._dl, doc_id)
 
-class PremiumFale(FakeSource):
+class PremiumFake(FakeSource):
     service_type = "premium_fake"
     @classmethod
     def is_premium():
