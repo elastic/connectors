5c47c3e738ebbf5ec968a66b26a312275b32e190
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-06-12 12:48:32 +0200

    Rebase fix

--- tests/sources/test_sharepoint_online.py
+++ tests/sources/test_sharepoint_online.py
@@ -635,7 +635,13 @@ class TestSharepointOnlineDataSource:
 
     @property
     def sites(self):
-        return [{"id": "1", "webUrl": "https://test.sharepoint.com/site-1", "name": "site-1"}]
+        return [
+            {
+                "id": "1",
+                "webUrl": "https://test.sharepoint.com/site-1",
+                "name": "site-1",
+            }
+        ]
 
     @property
     def site_drives(self):
@@ -655,18 +661,30 @@ class TestSharepointOnlineDataSource:
     @property
     def site_list_items(self):
         return [
-            {"id": "6", "contentType": {"name": "Item"}, "fields": {"Attachments": ""}, "lastModifiedDateTime": self.month_ago},
+            {
+                "id": "6",
+                "contentType": {"name": "Item"},
+                "fields": {"Attachments": ""},
+                "lastModifiedDateTime": self.month_ago,
+            },
             {
                 "id": "7",
                 "contentType": {"name": "Web Template Extensions"},
-                "fields": {}
+                "fields": {},
             },  # Will be ignored!!!
-            {"id": "8", "contentType": {"name": "Something without attachments"}, "fields": {}},
+            {
+                "id": "8",
+                "contentType": {"name": "Something without attachments"},
+                "fields": {},
+            },
         ]
 
     @property
     def site_list_item_attachments(self):
-        return [{"odata.id": "9", "name": "attachment 1.txt"}, { "odata.id": "10", "name": "attachment 2.txt"}]
+        return [
+            {"odata.id": "9", "name": "attachment 1.txt"},
+            {"odata.id": "10", "name": "attachment 2.txt"},
+        ]
 
     @property
     def site_pages(self):
@@ -682,14 +700,15 @@ class TestSharepointOnlineDataSource:
 
     @property
     def valid_tenant(self):
-        return { "NameSpaceType": "VALID" }
+        return {"NameSpaceType": "VALID"}
 
     @pytest_asyncio.fixture
     async def patch_sharepoint_client(self):
         client = AsyncMock()
 
         with patch(
-            'connectors.sources.sharepoint_online.SharepointOnlineClient', return_value=AsyncMock()
+            "connectors.sources.sharepoint_online.SharepointOnlineClient",
+            return_value=AsyncMock(),
         ) as new_mock:
             client = new_mock.return_value
             client.site_collections = AsyncIterator(self.site_collections)
@@ -698,7 +717,9 @@ class TestSharepointOnlineDataSource:
             client.drive_items = AsyncIterator(self.drive_items)
             client.site_lists = AsyncIterator(self.site_lists)
             client.site_list_items = AsyncIterator(self.site_list_items)
-            client.site_list_item_attachments = AsyncIterator(self.site_list_item_attachments)
+            client.site_list_item_attachments = AsyncIterator(
+                self.site_list_item_attachments
+            )
             client.site_pages = AsyncIterator(self.site_pages)
 
             client.graph_api_token = AsyncMock()
@@ -706,7 +727,7 @@ class TestSharepointOnlineDataSource:
             client.rest_api_token = AsyncMock()
             client.rest_api_token.get.return_value = self.rest_api_token
 
-            client.tenant_details=AsyncMock(return_value = self.valid_tenant)
+            client.tenant_details = AsyncMock(return_value=self.valid_tenant)
 
             yield client
 
@@ -723,14 +744,31 @@ class TestSharepointOnlineDataSource:
                 downloads.append(download_func)
 
         assert len(results) == 11
-        assert len([i for i in results if i["object_type"] == "site_collection"]) == len(self.site_collections)
-        assert len([i for i in results if i["object_type"] == "site"]) == len(self.sites)
-        assert len([i for i in results if i["object_type"] == "site_drive"]) == len(self.site_drives)
-        assert len([i for i in results if i["object_type"] == "drive_item"]) == len(self.drive_items)
-        assert len([i for i in results if i["object_type"] == "site_list"]) == len(self.site_lists)
-        assert len([i for i in results if i["object_type"] == "list_item"]) == len(self.site_list_items) - 1 # -1 because one of them is ignored!
-        assert len([i for i in results if i["object_type"] == "list_item_attachment"]) == len(self.site_list_item_attachments)
-        assert len([i for i in results if i["object_type"] == "site_page"]) == len(self.site_pages)
+        assert len(
+            [i for i in results if i["object_type"] == "site_collection"]
+        ) == len(self.site_collections)
+        assert len([i for i in results if i["object_type"] == "site"]) == len(
+            self.sites
+        )
+        assert len([i for i in results if i["object_type"] == "site_drive"]) == len(
+            self.site_drives
+        )
+        assert len([i for i in results if i["object_type"] == "drive_item"]) == len(
+            self.drive_items
+        )
+        assert len([i for i in results if i["object_type"] == "site_list"]) == len(
+            self.site_lists
+        )
+        assert (
+            len([i for i in results if i["object_type"] == "list_item"])
+            == len(self.site_list_items) - 1
+        )  # -1 because one of them is ignored!
+        assert len(
+            [i for i in results if i["object_type"] == "list_item_attachment"]
+        ) == len(self.site_list_item_attachments)
+        assert len([i for i in results if i["object_type"] == "site_page"]) == len(
+            self.site_pages
+        )
 
     def test_get_default_configuration(self):
         config = SharepointOnlineDataSource.get_default_configuration()
@@ -738,7 +776,7 @@ class TestSharepointOnlineDataSource:
         assert config is not None
 
     @pytest.mark.asyncio
-    async def test_validate_config(self, patch_sharepoint_client):      
+    async def test_validate_config(self, patch_sharepoint_client):
         source = create_source(SharepointOnlineDataSource, site_collections=WILDCARD)
 
         await source.validate_config()
@@ -750,24 +788,34 @@ class TestSharepointOnlineDataSource:
         patch_sharepoint_client.rest_api_token.get.assert_awaited()
 
     @pytest.mark.asyncio
-    async def test_validate_config_when_invalid_tenant(self, patch_sharepoint_client):      
+    async def test_validate_config_when_invalid_tenant(self, patch_sharepoint_client):
         invalid_tenant_name = "wat"
 
-        source = create_source(SharepointOnlineDataSource, tenant_name=invalid_tenant_name, site_collections=WILDCARD)
-        patch_sharepoint_client.tenant_details.return_value = {"NameSpaceType": "Unknown"}
+        source = create_source(
+            SharepointOnlineDataSource,
+            tenant_name=invalid_tenant_name,
+            site_collections=WILDCARD,
+        )
+        patch_sharepoint_client.tenant_details.return_value = {
+            "NameSpaceType": "Unknown"
+        }
 
         with pytest.raises(Exception) as e:
             await source.validate_config()
 
         assert e.match(invalid_tenant_name)
 
-
     @pytest.mark.asyncio
-    async def test_validate_config_non_existing_collection(self, patch_sharepoint_client):      
+    async def test_validate_config_non_existing_collection(
+        self, patch_sharepoint_client
+    ):
         non_existing_site = "something"
         another_non_existing_site = "something-something"
 
-        source = create_source(SharepointOnlineDataSource, site_collections=[non_existing_site, another_non_existing_site])
+        source = create_source(
+            SharepointOnlineDataSource,
+            site_collections=[non_existing_site, another_non_existing_site],
+        )
 
         with pytest.raises(Exception) as e:
             await source.validate_config()
@@ -778,10 +826,12 @@ class TestSharepointOnlineDataSource:
 
     @pytest.mark.asyncio
     async def test_get_attachment(self, patch_sharepoint_client):
-        attachment = { "odata.id": "1" }
+        attachment = {"odata.id": "1"}
         message = b"This is content of attachment"
+
         async def download_func(attachment_id, async_buffer):
             await async_buffer.write(message)
+
         patch_sharepoint_client.download_attachment = download_func
         source = create_source(SharepointOnlineDataSource)
 
@@ -791,10 +841,17 @@ class TestSharepointOnlineDataSource:
 
     @pytest.mark.asyncio
     async def test_get_content(self, patch_sharepoint_client):
-        drive_item = { "id": "1", "size": 15, "lastModifiedDateTime": datetime.now(timezone.utc), "parentReference": { "driveId": "drive-1" } }
+        drive_item = {
+            "id": "1",
+            "size": 15,
+            "lastModifiedDateTime": datetime.now(timezone.utc),
+            "parentReference": {"driveId": "drive-1"},
+        }
         message = b"This is content of drive item"
+
         async def download_func(drive_id, drive_item_id, async_buffer):
             await async_buffer.write(message)
+
         patch_sharepoint_client.download_drive_item = download_func
         source = create_source(SharepointOnlineDataSource)
 
