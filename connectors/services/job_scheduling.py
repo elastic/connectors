8647b9df0df3a66498cad6c7a3eabbeb905d2668
Chenhui Wang <wangch079@outlook.com>
2023-06-20 23:40:31 +0800

    Set logger for validator

--- connectors/services/job_scheduling.py
+++ connectors/services/job_scheduling.py
@@ -89,9 +89,9 @@ class JobSchedulingService(BaseService):
 
         source_klass = get_source_klass(self.source_list[connector.service_type])
         if connector.features.sync_rules_enabled():
-            await connector.validate_filtering(
-                validator=source_klass(connector.configuration)
-            )
+            validator = source_klass(connector.configuration)
+            validator.set_logger(connector.logger)
+            await connector.validate_filtering(validator=validator)
 
         if connector.features.document_level_security_enabled():
             (
