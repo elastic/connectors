cd67a1c7d7992ee63c9a2f501194325746ae30a3
Praveen Kukreja <praveen.kukreja@elastic.co>
2023-07-18 18:54:35 +0530

    add ping functionality

---
+++ connectors/sources/onedrive.py
@@ -0,0 +1,219 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+#
+"""OneDrive source module responsible to fetch documents from OneDrive.
+"""
+from datetime import datetime, timedelta
+from functools import cached_property
+from urllib import parse
+
+import aiohttp
+from aiohttp.client_exceptions import ClientResponseError
+
+from connectors.logger import logger
+from connectors.source import BaseDataSource
+from connectors.utils import (
+    CacheWithTimeout,
+    CancellableSleeps,
+    RetryStrategy,
+    iso_utc,
+    retryable,
+)
+
+RETRIES = 3
+RETRY_INTERVAL = 2
+BASE_URL = "https://graph.microsoft.com/v1.0/"
+ENDPOINTS = {"PING": "drives"}
+
+
+class TokenRetrievalError(Exception):
+    """Exception class to notify that fetching of access token was not successful."""
+
+    pass
+
+
+class AccessToken:
+    """Class for handling access token for Microsoft Graph APIs"""
+
+    def __init__(self, configuration):
+        self.tenant_id = configuration["tenant_id"]
+        self.client_id = configuration["client_id"]
+        self.client_secret = configuration["client_secret"]
+        self._token_cache = CacheWithTimeout()
+
+    async def get(self):
+        """Get bearer token required for API call.
+
+        If the token is not present in the cache or expired,
+        it calls _set_access_token that sends a POST request
+        to Microsoft for generating a new access token.
+
+        Returns:
+            str: Access Token
+        """
+        if cached_value := self._token_cache.get():
+            return cached_value
+        try:
+            await self._set_access_token()
+        except ClientResponseError as e:
+            match e.status:
+                case 400:
+                    raise TokenRetrievalError(
+                        "Failed to fetch access token. Please verify that provided Tenant ID, Client ID are correct."
+                    ) from e
+                case 401:
+                    raise TokenRetrievalError(
+                        "Failed to fetch access token. Please check if Client Secret is valid."
+                    ) from e
+                case _:
+                    raise TokenRetrievalError(
+                        f"Failed to fetch access token. Response Status: {e.status}, Message: {e.message}"
+                    ) from e
+        return self.access_token
+
+    @retryable(
+        retries=RETRIES,
+        interval=RETRY_INTERVAL,
+        strategy=RetryStrategy.EXPONENTIAL_BACKOFF,
+    )
+    async def _set_access_token(self):
+        """Generate access token with configuration fields and stores it in the cache"""
+        url = f"https://login.microsoftonline.com/{self.tenant_id}/oauth2/v2.0/token"
+        data = {
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "grant_type": "client_credentials",
+            "scope": "https://graph.microsoft.com/.default",
+        }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        async with aiohttp.request(
+            method="POST", url=url, data=data, headers=headers, raise_for_status=True
+        ) as response:
+            token_reponse = await response.json()
+            self.access_token = token_reponse["access_token"]
+            self.token_expires_at = datetime.utcnow() + timedelta(
+                seconds=int(token_reponse.get("expires_in", 0))
+            )
+            self._token_cache.set(self.access_token, self.token_expires_at)
+
+
+class OneDriveClient:
+    """Client Class for API calls to OneDrive"""
+
+    def __init__(self, configuration):
+        self._sleeps = CancellableSleeps()
+        self.configuration = configuration
+        self.retry_count = self.configuration["retry_count"]
+        self._logger = logger
+        self.token = AccessToken(configuration=configuration)
+
+    def set_logger(self, logger_):
+        self._logger = logger_
+
+    @cached_property
+    def _get_session(self):
+        """Generate base client session with configuration fields
+        Returns:
+            ClientSession: Base client session
+        """
+
+        timeout = aiohttp.ClientTimeout(total=None)
+        return aiohttp.ClientSession(
+            timeout=timeout,
+            raise_for_status=True,
+        )
+
+    async def close_session(self):
+        self._sleeps.cancel()
+        await self._get_session.close()
+        del self._get_session
+
+    @retryable(
+        retries=RETRIES,
+        interval=RETRY_INTERVAL,
+        strategy=RetryStrategy.EXPONENTIAL_BACKOFF,
+    )
+    async def api_call(self, url_name):
+        url = parse.urljoin(BASE_URL, url_name)
+        access_token = await self.token.get()
+        headers = {"authorization": f"Bearer {access_token}"}
+        async with self._get_session.get(url=url, headers=headers) as response:
+            yield response
+
+
+class OneDriveDataSource(BaseDataSource):
+    """OneDrive"""
+
+    name = "OneDrive"
+    service_type = "onedrive"
+
+    def __init__(self, configuration):
+        """Setup the connection to OneDrive
+
+        Args:
+            configuration (DataSourceConfiguration): Object of DataSourceConfiguration class.
+        """
+        super().__init__(configuration=configuration)
+        self.onedrive_client = OneDriveClient(configuration)
+
+    def _set_internal_logger(self):
+        self.onedrive_client.set_logger(self._logger)
+
+    @classmethod
+    def get_default_configuration(cls):
+        """Get the default configuration for OneDrive
+
+        Returns:
+            dictionary: Default configuration.
+        """
+        return {
+            "client_id": {
+                "label": "Azure application Client ID",
+                "order": 1,
+                "type": "str",
+                "value": "client#123",
+            },
+            "client_secret": {
+                "label": "Azure application Client Secret",
+                "order": 2,
+                "sensitive": True,
+                "type": "str",
+                "value": "secret#123",
+            },
+            "tenant_id": {
+                "label": "Azure application Tenant ID",
+                "order": 3,
+                "type": "str",
+                "value": "tenant#123",
+            },
+            "retry_count": {
+                "default_value": 3,
+                "display": "numeric",
+                "label": "Maximum retries per request",
+                "order": 4,
+                "required": False,
+                "type": "int",
+                "ui_restrictions": ["advanced"],
+                "value": 3,
+            },
+        }
+
+    async def close(self):
+        """Closes unclosed client session"""
+        await self.onedrive_client.close_session()
+
+    async def ping(self):
+        """Verify the connection with OneDrive"""
+        try:
+            await anext(self.onedrive_client.api_call(url_name=ENDPOINTS["PING"]))
+            self._logger.info("Successfully connected to OneDrive")
+        except Exception:
+            self._logger.exception("Error while connecting to OneDrive")
+            raise
+
+    async def get_docs(self, filtering=None):
+        # yield dummy document to make the chunked PR work, subsequent PR will replace it with actual implementation
+
+        yield {"_id": "123", "timestamp": iso_utc()}, None
---
+++ tests/sources/test_onedrive.py
@@ -0,0 +1,168 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+#
+"""Tests the OneDrive source class methods"""
+from unittest.mock import AsyncMock, patch
+
+import pytest
+from aiohttp.client_exceptions import ClientResponseError
+
+from connectors.source import ConfigurableFieldValueError, DataSourceConfiguration
+from connectors.sources.onedrive import (
+    AccessToken,
+    OneDriveClient,
+    OneDriveDataSource,
+    TokenRetrievalError,
+)
+from tests.commons import AsyncIterator
+from tests.sources.support import create_source
+
+
+def token_retrieval_errors(message, error_code):
+    error = ClientResponseError(None, None)
+    error.status = error_code
+    error.message = message
+    return error
+
+
+class JSONAsyncMock(AsyncMock):
+    def __init__(self, json, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._json = json
+
+    async def json(self):
+        return self._json
+
+
+def test_get_configuration():
+    config = DataSourceConfiguration(
+        config=OneDriveDataSource.get_default_configuration()
+    )
+
+    assert config["client_id"] == "client#123"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "extras",
+    [
+        (
+            {
+                "client_id": "",
+                "client_secret": "",
+                "tenant_id": "",
+            }
+        ),
+    ],
+)
+async def test_validate_configuration_with_invalid_dependency_fields_raises_error(
+    extras,
+):
+    source = create_source(OneDriveDataSource, **extras)
+
+    with pytest.raises(ConfigurableFieldValueError):
+        await source.validate_config()
+
+
+@pytest.mark.asyncio
+async def test_close_with_client_session():
+    source = create_source(OneDriveDataSource)
+    source.onedrive_client.access_token = "dummy"
+
+    await source.close()
+
+    assert hasattr(source.onedrive_client.__dict__, "_get_session") is False
+
+
+@pytest.mark.asyncio
+async def test_set_access_token():
+    source = create_source(OneDriveDataSource)
+    mock_token = {"access_token": "msgraphtoken", "expires_in": "1234555"}
+    async_response = AsyncMock()
+    async_response.__aenter__ = AsyncMock(return_value=JSONAsyncMock(mock_token, 200))
+
+    with patch("aiohttp.request", return_value=async_response):
+        await source.onedrive_client.token._set_access_token()
+
+        assert source.onedrive_client.token.access_token == "msgraphtoken"
+
+
+@pytest.mark.asyncio
+async def test_ping_for_successful_connection():
+    source = create_source(OneDriveDataSource)
+    DUMMY_RESPONSE = {}
+    source.onedrive_client.api_call = AsyncIterator([[DUMMY_RESPONSE]])
+
+    await source.ping()
+
+
+@pytest.mark.asyncio
+@patch("aiohttp.ClientSession.get")
+async def test_ping_for_failed_connection_exception(mock_get):
+    source = create_source(OneDriveDataSource)
+
+    with patch.object(
+        OneDriveClient, "api_call", side_effect=Exception("Something went wrong")
+    ):
+        with pytest.raises(Exception):
+            await source.ping()
+
+
+@pytest.mark.asyncio
+async def test_get_token_raises_correct_exception_when_400():
+    klass = OneDriveDataSource
+
+    config = DataSourceConfiguration(config=klass.get_default_configuration())
+    message = "Bad Request"
+    token = AccessToken(configuration=config)
+
+    with patch.object(
+        AccessToken,
+        "_set_access_token",
+        side_effect=token_retrieval_errors(message, 400),
+    ):
+        with pytest.raises(TokenRetrievalError) as e:
+            await token.get()
+
+        assert e.match("Tenant ID")
+        assert e.match("Client ID")
+
+
+@pytest.mark.asyncio
+async def test_get_token_raises_correct_exception_when_401():
+    klass = OneDriveDataSource
+
+    config = DataSourceConfiguration(config=klass.get_default_configuration())
+    message = "Unauthorized"
+    token = AccessToken(configuration=config)
+
+    with patch.object(
+        AccessToken,
+        "_set_access_token",
+        side_effect=token_retrieval_errors(message, 401),
+    ):
+        with pytest.raises(TokenRetrievalError) as e:
+            await token.get()
+
+        assert e.match("Client Secret")
+
+
+@pytest.mark.asyncio
+async def test_get_token_raises_correct_exception_when_any_other_status():
+    klass = OneDriveDataSource
+
+    config = DataSourceConfiguration(config=klass.get_default_configuration())
+    message = "Internal server error"
+    token = AccessToken(configuration=config)
+
+    with patch.object(
+        AccessToken,
+        "_set_access_token",
+        side_effect=token_retrieval_errors(message, 500),
+    ):
+        with pytest.raises(TokenRetrievalError) as e:
+            await token.get()
+
+        assert e.match(message)
