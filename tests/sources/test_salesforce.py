ca7e695abc994e710df34fbb4330523697da61c5
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2023-08-02 10:28:18 +0200

    [Salesforce] Add soql builder and ingest accounts (#1379)

--- tests/sources/test_salesforce.py
+++ tests/sources/test_salesforce.py
@@ -4,6 +4,7 @@
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
 """Tests the Salesforce source class methods"""
+import re
 import time
 from unittest import mock
 
@@ -11,7 +12,8 @@ import pytest
 from aiohttp.client_exceptions import ClientConnectionError
 
 from connectors.source import ConfigurableFieldValueError, DataSourceConfiguration
-from connectors.sources.salesforce import SalesforceDataSource
+from connectors.sources.salesforce import SalesforceDataSource, SalesforceSoqlBuilder
+from tests.commons import AsyncIterator
 from tests.sources.support import create_source
 
 TEST_DOMAIN = "fake"
@@ -68,10 +70,11 @@ async def test_get_token_with_successful_connection(mock_responses):
         f"{TEST_BASE_URL}/services/oauth2/token", status=200, payload=response_payload
     )
     await source.salesforce_client.get_token()
-    await source.close()
 
     assert source.salesforce_client.token == "foo"
     assert source.salesforce_client.token_issued_at == SECONDS_SINCE_EPOCH
+
+    await source.close()
 
 
 @pytest.mark.asyncio
@@ -102,3 +105,86 @@ async def test_get_token_with_bad_domain_raises_error(
     with pytest.raises(ClientConnectionError):
         await source.salesforce_client.get_token()
     await source.close()
+
+
+@pytest.mark.asyncio
+async def test_get_accounts_when_success(mock_responses):
+    expected_record = {
+        "attributes": {
+            "type": "Account",
+            "url": "/services/data/v58.0/sobjects/Account/1234",
+        },
+        "Id": "1234",
+    }
+    source = create_source(
+        SalesforceDataSource,
+        domain=TEST_DOMAIN,
+        client_id=TEST_CLIENT_ID,
+        client_secret=TEST_CLIENT_SECRET,
+    )
+    response_payload = {
+        "totalSize": "2",
+        "done": True,
+        "records": [expected_record],
+    }
+
+    source.salesforce_client.get_accounts = mock.Mock(
+        return_value=AsyncIterator([{**expected_record, "_id": "1234"}])
+    )
+    mock_responses.get(
+        re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
+        status=200,
+        payload=response_payload,
+    )
+    async for account in source.salesforce_client.get_accounts():
+        assert account["Id"] == "1234"
+        assert account["_id"] == "1234"
+
+    await source.close()
+
+
+@pytest.mark.asyncio
+@mock.patch("connectors.utils.apply_retry_strategy")
+async def test_get_accounts_when_invalid_request(apply_retry_strategy, mock_responses):
+    source = create_source(
+        SalesforceDataSource,
+        domain=TEST_DOMAIN,
+        client_id=TEST_CLIENT_ID,
+        client_secret=TEST_CLIENT_SECRET,
+    )
+    response_payload = [
+        {"message": "Unable to process query.", "errorCode": "INVALID_FIELD"}
+    ]
+
+    mock_responses.get(
+        re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
+        status=400,
+        payload=response_payload,
+    )
+    with pytest.raises(ClientConnectionError):
+        async for _ in source.salesforce_client.get_accounts():
+            # TODO confirm error message when error handling is improved
+            pass
+
+    await source.close()
+
+
+@pytest.mark.asyncio
+async def test_build_soql_query_with_fields():
+    expected_columns = [
+        "Id",
+        "CreatedDate",
+        "LastModifiedDate",
+        "FooField",
+        "BarField",
+    ]
+
+    builder = SalesforceSoqlBuilder("Test")
+    builder.with_id()
+    builder.with_default_metafields()
+    builder.with_fields(["FooField", "BarField"])
+    query = builder.build()
+
+    assert query.startswith("SELECT ")
+    assert all(col in query for col in expected_columns)
+    assert query.endswith("FROM Test")
