f7bcd63c3798c2ddd7491b62095bbaa8d172a5e8
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2023-08-02 17:18:47 +0200

    Add opportunities and format doc response

--- connectors/sources/salesforce.py
+++ connectors/sources/salesforce.py
@@ -19,6 +19,8 @@ BASE_URL = "https://<domain>.my.salesforce.com"
 API_VERSION = "v58.0"
 TOKEN_ENDPOINT = "/services/oauth2/token"
 QUERY_ENDPOINT = f"/services/data/{API_VERSION}/query"
+DESCRIBE_ENDPOINT = f"/services/data/{API_VERSION}/sobjects"
+DESCRIBE_SOBJECT_ENDPOINT = f"/services/data/{API_VERSION}/sobjects/<sobject>/describe"
 
 
 class SalesforceClient:
@@ -28,6 +30,7 @@ class SalesforceClient:
         self.token = None
         self.token_issued_at = None
         self.token_refresh_enabled = False
+        self.queryable_sobjects = []
 
         self.base_url = BASE_URL.replace("<domain>", configuration["domain"])
         self.client_id = configuration["client_id"]
@@ -37,6 +40,8 @@ class SalesforceClient:
             "client_id": self.client_id,
             "client_secret": self.client_secret,
         }
+
+        self.doc_builder = SalesforceDocBuilder(self.base_url)
 
     def set_logger(self, logger_):
         self._logger = logger_
@@ -79,32 +84,57 @@ class SalesforceClient:
         interval=RETRY_INTERVAL,
     )
     async def get_accounts(self):
-        # TODO prepare cache
-
-        query_builder = SalesforceSoqlBuilder("Account")
-        query_builder.with_id()
-        query_builder.with_default_metafields()
-        query_builder.with_fields(
-            self._select_queryable_fields(
-                [
-                    "Name",
-                    "Description",
-                    "BillingAddress",
-                    "Type",
-                    "Website",
-                    "Rating",
-                    "Department",
-                ]
-            )
-        )
-        query = query_builder.build()
+        if not await self._is_queryable("Account"):
+            return
 
         # TODO handle pagination
+        query = await self._accounts_query()
         resp = await self._yield_non_bulk_query_pages(query)
         resp_json = await resp.json()
         for record in resp_json.get("records", []):
-            record["_id"] = record["Id"]
-            yield record
+            yield self.doc_builder.build_account(record)
+
+    @retryable(
+        retries=RETRIES,
+        interval=RETRY_INTERVAL,
+    )
+    async def get_opportunities(self):
+        if not await self._is_queryable("Opportunity"):
+            return
+
+        # TODO handle pagination
+        query = await self._opportunities_query()
+        resp = await self._yield_non_bulk_query_pages(query)
+        resp_json = await resp.json()
+        for record in resp_json.get("records", []):
+            yield self.doc_builder.build_oppoortunity(record)
+
+    async def prepare_caches(self):
+        await self._cache_queryable_sobjects()
+
+    async def _cache_queryable_sobjects(self):
+        resp = await self._api_request(
+            f"{self.base_url}{DESCRIBE_ENDPOINT}", "get", headers=self._auth_headers()
+        )
+        resp_json = await resp.json()
+        self.queryable_sobjects = [
+            x["name"].lower() for x in resp_json["sobjects"] if x["queryable"] is True
+        ]
+
+    async def _is_queryable(self, sobject):
+        return sobject.lower() in self.queryable_sobjects
+
+    async def _select_queryable_fields(self, sobject, fields):
+        """User settings can cause fields to be non-queryable
+        This causes
+        """
+        endpoint = DESCRIBE_SOBJECT_ENDPOINT.replace("<sobject>", sobject)
+        resp = await self._api_request(
+            f"{self.base_url}{endpoint}", "get", headers=self._auth_headers()
+        )
+        resp_json = await resp.json()
+        queryable_fields = [f["name"] for f in resp_json["fields"]]
+        return [f for f in fields if f in queryable_fields]
 
     async def _yield_non_bulk_query_pages(self, soql_query):
         return await self._api_request(
@@ -113,10 +143,6 @@ class SalesforceClient:
             headers=self._auth_headers(),
             params={"q": soql_query},
         )
-
-    def _select_queryable_fields(self, fields):
-        # TODO check if fields queryable first from cache
-        return fields
 
     def _token_cleanup(self):
         self.token = None
@@ -132,11 +158,70 @@ class SalesforceClient:
             url=url, headers=headers, data=data, params=params
         )
 
+    async def _accounts_query(self):
+        queryable_fields = await self._select_queryable_fields(
+            "Account",
+            [
+                "Name",
+                "Description",
+                "BillingAddress",
+                "Type",
+                "Website",
+                "Rating",
+                "Department",
+            ],
+        )
+        query_builder = SalesforceSoqlBuilder("Account")
+        query_builder.with_id()
+        query_builder.with_default_metafields()
+        query_builder.with_fields(queryable_fields)
+        # TODO add uncommon_object_remote_fields
+        query_builder.with_fields(["Owner.Id", "Owner.Name", "Owner.Email"])
+        query_builder.with_fields(["Parent.Id", "Parent.Name"])
+
+        if await self._is_queryable("Opportunity"):
+            queryable_join_fields = await self._select_queryable_fields(
+                "Opportunity",
+                [
+                    "Name",
+                    "StageName",
+                ],
+            )
+            join_builder = SalesforceSoqlBuilder("Opportunities")
+            join_builder.with_id()
+            join_builder.with_fields(queryable_join_fields)
+            join_builder.with_order_by("CreatedDate DESC")
+            join_builder.with_limit(1)
+            query_builder.with_join(join_builder.build())
+
+        return query_builder.build()
+
+    async def _opportunities_query(self):
+        queryable_fields = await self._select_queryable_fields(
+            "Opportunity",
+            [
+                "Name",
+                "Description",
+                "StageName",
+            ],
+        )
+        query_builder = SalesforceSoqlBuilder("Opportunity")
+        query_builder.with_id()
+        query_builder.with_default_metafields()
+        query_builder.with_fields(queryable_fields)
+        # TODO add uncommon_object_remote_fields
+        query_builder.with_fields(["Owner.Id", "Owner.Name", "Owner.Email"])
+
+        return query_builder.build()
+
 
 class SalesforceSoqlBuilder:
     def __init__(self, table):
         self.table_name = table
         self.fields = []
+        self.where = ""
+        self.order_by = ""
+        self.limit = ""
 
     def with_id(self):
         self.fields.append("Id")
@@ -147,15 +232,100 @@ class SalesforceSoqlBuilder:
     def with_fields(self, fields):
         self.fields.extend(fields)
 
+    def with_where(self, where_string):
+        self.where = f"WHERE {where_string}"
+
+    def with_order_by(self, order_by_string):
+        self.order_by = f"ORDER BY {order_by_string}"
+
+    def with_limit(self, limit):
+        self.limit = f"LIMIT {limit}"
+
+    def with_join(self, join):
+        self.fields.append(f"(\n{join})\n")
+
     def build(self):
         select_columns = ",\n".join(set(self.fields))
 
         query_lines = []
         query_lines.append(f"SELECT {select_columns}")
         query_lines.append(f"FROM {self.table_name}")
-        # TODO expand functionality when needed (where, order_by, limit, etc)
+        query_lines.append(self.where)
+        query_lines.append(self.order_by)
+        query_lines.append(self.limit)
 
-        return "\n".join(query_lines)
+        return "\n".join([line for line in query_lines if line != ""])
+
+
+class SalesforceDocBuilder:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def build_account(self, account):
+        owner = account.get("Owner", {})
+
+        opportunities = account.get("Opportunities", {}).get("records", [])
+        opportunity = opportunities[0] if len(opportunities) > 0 else {}
+        opportunity_url = (
+            f"{self.base_url}/{opportunity.get('Id')}" if opportunity else ""
+        )
+        opportunity_status = opportunity.get("StageName", "")
+
+        return {
+            "_id": f"salesforce_connector_{account.get('Id')}",
+            "account_type": account.get("Type"),
+            "address": self._format_address(account.get("BillingAddress")),
+            "body": account.get("Description"),
+            "content_source_id": account.get("Id"),
+            "created_at": account.get("CreatedDate"),
+            "last_updated": account.get("LastModifiedDate"),
+            "open_activities": "",  # TODO
+            "open_activities_urls": "",  # TODO
+            "opportunity_name": opportunity.get("Name"),
+            "opportunity_status": opportunity_status,
+            "opportunity_url": opportunity_url or "",
+            "owner": owner.get("Name"),
+            "owner_email": owner.get("Email"),
+            "rating": account.get("Rating"),
+            "source": "salesforce",
+            "tags": [account.get("Type")],
+            "title": account.get("Name"),
+            "type": "account",
+            "url": f"{self.base_url}/{account.get('Id')}",
+            "website_url": account.get("Website"),
+        }
+
+    def build_oppoortunity(self, opportunity):
+        owner = opportunity.get("Owner", {})
+
+        return {
+            "_id": f"salesforce_connector_{opportunity.get('Id')}",
+            "body": opportunity.get("Description"),
+            "content_source_id": opportunity.get("Id"),
+            "created_at": opportunity.get("CreatedDate"),
+            "last_updated": opportunity.get("LastModifiedDate"),
+            "next_step": opportunity.get("NextStep"),
+            "owner": owner.get("Name"),
+            "owner_email": owner.get("Email"),
+            "source": "salesforce",
+            "status": opportunity.get("StageName", ""),
+            "title": opportunity.get("Name"),
+            "type": "opportunity",
+            "url": f"{self.base_url}/{opportunity.get('Id')}",
+        }
+
+    def _format_address(self, address):
+        if not address:
+            return ""
+
+        address_fields = [
+            address.get("street"),
+            address.get("city"),
+            address.get("state"),
+            str(address.get("postalCode", "")),
+            address.get("country"),
+        ]
+        return ", ".join([a for a in address_fields if a])
 
 
 class SalesforceDataSource(BaseDataSource):
@@ -210,9 +380,12 @@ class SalesforceDataSource(BaseDataSource):
         return
 
     async def get_docs(self, filtering=None):
-        # TODO rename
         await self.salesforce_client.get_token()
+        await self.salesforce_client.prepare_caches()
 
         # TODO filtering
         async for account in self.salesforce_client.get_accounts():
             yield account, None
+
+        async for opportunity in self.salesforce_client.get_opportunities():
+            yield opportunity, None
--- tests/sources/test_salesforce.py
+++ tests/sources/test_salesforce.py
@@ -6,7 +6,7 @@
 """Tests the Salesforce source class methods"""
 import re
 import time
-from unittest import mock
+from unittest import TestCase, mock
 
 import pytest
 from aiohttp.client_exceptions import ClientConnectionError
@@ -22,6 +22,100 @@ TEST_CLIENT_ID = "1234"
 TEST_CLIENT_SECRET = "9876"
 SECONDS_SINCE_EPOCH = int(time.time())
 
+ACCOUNT_RESPONSE_PAYLOAD = {
+    "totalSize": 1,
+    "done": True,
+    "records": [
+        {
+            "attributes": {
+                "type": "Account",
+                "url": "/services/data/v58.0/sobjects/Account/account_id",
+            },
+            "Type": "Customer - Direct",
+            "Owner": {
+                "attributes": {
+                    "type": "User",
+                    "url": "/services/data/v58.0/sobjects/User/user_id",
+                },
+                "Id": "user_id",
+                "Name": "Owner's Name",
+                "Email": "email@fake.com",
+            },
+            "Id": "account_id",
+            "Rating": "Cold",
+            "Website": "www.fake.com",
+            "LastModifiedDate": "",
+            "CreatedDate": "",
+            "Opportunities": {
+                "totalSize": 1,
+                "done": True,
+                "records": [
+                    {
+                        "attributes": {
+                            "type": "Opportunity",
+                            "url": "/services/data/v58.0/sobjects/Opportunity/opportunity_id",
+                        },
+                        "Id": "opportunity_id",
+                        "Name": "Opportunity Generator",
+                        "StageName": "Closed Won",
+                    }
+                ],
+            },
+            "Name": "Salesforce Account 1",
+            "BillingAddress": {
+                "city": "The Shire",
+                "country": "Middle Earth",
+                "postalCode": 111,
+                "state": "Eriador",
+                "street": "The Burrow under the Hill, Bag End, Hobbiton",
+            },
+            "Description": "A fantastic opportunity!",
+        }
+    ],
+}
+
+OPPORTUNITY_RESPONSE_PAYLOAD = {
+    "totalSize": 1,
+    "done": True,
+    "records": [
+        {
+            "attributes": {
+                "type": "Opportunity",
+                "url": "/services/data/v58.0/sobjects/Opportunity/opportunity_id",
+            },
+            "Description": "An Opportunity!",
+            "Owner": {
+                "attributes": {
+                    "type": "User",
+                    "url": "/services/data/v58.0/sobjects/User/user_id",
+                },
+                "Id": "user_id",
+                "Email": "email@fake.com",
+                "Name": "User's Name",
+            },
+            "LastModifiedDate": "",
+            "Name": "Another Opportunity Generator",
+            "StageName": "Qualification",
+            "CreatedDate": "",
+            "Id": "opportunity_id",
+        },
+    ],
+}
+
+
+def create_salesforce_source():
+    return create_source(
+        SalesforceDataSource,
+        domain=TEST_DOMAIN,
+        client_id=TEST_CLIENT_ID,
+        client_secret=TEST_CLIENT_SECRET,
+    )
+
+
+async def create_fake_coroutine(data):
+    """create a method for returning fake coroutine value"""
+    return data
+
 
 def test_get_default_configuration():
     config = DataSourceConfiguration(SalesforceDataSource.get_default_configuration())
@@ -33,7 +127,7 @@ def test_get_default_configuration():
 @pytest.mark.asyncio
 @pytest.mark.parametrize("field", ["client_id", "client_secret", "domain"])
 async def test_validate_config_missing_fields_then_raise(field):
-    source = create_source(SalesforceDataSource)
+    source = create_salesforce_source()
     source.configuration.set_field(name=field, value="")
 
     with pytest.raises(ConfigurableFieldValueError):
@@ -42,7 +136,7 @@ async def test_validate_config_missing_fields_then_raise(field):
 
 @pytest.mark.asyncio
 async def test_ping_with_successful_connection(mock_responses):
-    source = create_source(SalesforceDataSource, domain=TEST_DOMAIN)
+    source = create_salesforce_source()
     mock_responses.head(TEST_BASE_URL, status=200)
 
     await source.ping()
@@ -51,12 +145,7 @@ async def test_ping_with_successful_connection(mock_responses):
 
 @pytest.mark.asyncio
 async def test_get_token_with_successful_connection(mock_responses):
-    source = create_source(
-        SalesforceDataSource,
-        domain=TEST_DOMAIN,
-        client_id=TEST_CLIENT_ID,
-        client_secret=TEST_CLIENT_SECRET,
-    )
+    source = create_salesforce_source()
     response_payload = {
         "access_token": "foo",
         "signature": "bar",
@@ -82,12 +171,7 @@ async def test_get_token_with_successful_connection(mock_responses):
 async def test_get_token_with_bad_domain_raises_error(
     apply_retry_strategy, mock_responses
 ):
-    source = create_source(
-        SalesforceDataSource,
-        domain=TEST_DOMAIN,
-        client_id=TEST_CLIENT_ID,
-        client_secret=TEST_CLIENT_SECRET,
-    )
+    source = create_salesforce_source()
     apply_retry_strategy.return_value = mock.Mock()
 
     response_payload = {
@@ -108,37 +192,119 @@ async def test_get_token_with_bad_domain_raises_error(
 
 
 @pytest.mark.asyncio
-async def test_get_accounts_when_success(mock_responses):
-    expected_record = {
-        "attributes": {
-            "type": "Account",
-            "url": "/services/data/v58.0/sobjects/Account/1234",
-        },
-        "Id": "1234",
-    }
-    source = create_source(
-        SalesforceDataSource,
-        domain=TEST_DOMAIN,
-        client_id=TEST_CLIENT_ID,
-        client_secret=TEST_CLIENT_SECRET,
-    )
+@pytest.mark.parametrize(
+    "sobject, expected_result",
+    [
+        (
+            "FooField",
+            True,
+        ),
+        ("ArghField", False),
+    ],
+)
+async def test_get_queryable_sobjects(mock_responses, sobject, expected_result):
+    source = create_salesforce_source()
     response_payload = {
-        "totalSize": "2",
-        "done": True,
-        "records": [expected_record],
+        "sobjects": [
+            {
+                "queryable": True,
+                "name": "FooField",
+            },
+            {
+                "queryable": False,
+                "name": "BarField",
+            },
+        ],
     }
 
-    source.salesforce_client.get_accounts = mock.Mock(
-        return_value=AsyncIterator([{**expected_record, "_id": "1234"}])
+    mock_responses.get(
+        f"{TEST_BASE_URL}/services/data/v58.0/sobjects",
+        status=200,
+        payload=response_payload,
+    )
+
+    await source.salesforce_client._cache_queryable_sobjects()
+    queryable = await source.salesforce_client._is_queryable(sobject)
+    assert queryable == expected_result
+
+    await source.close()
+
+
+@pytest.mark.asyncio
+async def test_get_queryable_fields(mock_responses):
+    source = create_salesforce_source()
+    expected_fields = [
+        {
+            "name": "FooField",
+        },
+        {
+            "name": "BarField",
+        },
+        {"name": "ArghField"},
+    ]
+    response_payload = {
+        "fields": expected_fields,
+    }
+    mock_responses.get(
+        f"{TEST_BASE_URL}/services/data/v58.0/sobjects/Account/describe",
+        status=200,
+        payload=response_payload,
+    )
+
+    queryable_fields = await source.salesforce_client._select_queryable_fields(
+        "Account", ["FooField", "BarField", "NarghField"]
+    )
+    TestCase().assertCountEqual(queryable_fields, ["FooField", "BarField"])
+
+    await source.close()
+
+
+@pytest.mark.asyncio
+async def test_get_accounts_when_success(mock_responses):
+    source = create_salesforce_source()
+    expected_doc = {
+        "_id": f"salesforce_connector_account_id",
+        "account_type": "Customer - Direct",
+        "address": "The Burrow under the Hill, Bag End, Hobbiton, The Shire, Eriador, 111, Middle Earth",
+        "body": "A fantastic opportunity!",
+        "content_source_id": "account_id",
+        "created_at": "",
+        "last_updated": "",
+        "owner": "Owner's Name",
+        "owner_email": "email@fake.com",
+        "open_activities": "",
+        "open_activities_urls": "",
+        "opportunity_name": "Opportunity Generator",
+        "opportunity_status": "Closed Won",
+        "opportunity_url": f"{TEST_BASE_URL}/opportunity_id",
+        "rating": "Cold",
+        "source": "salesforce",
+        "tags": ["Customer - Direct"],
+        "title": "Salesforce Account 1",
+        "type": "account",
+        "url": f"{TEST_BASE_URL}/account_id",
+        "website_url": "www.fake.com",
+    }
+
+    source.salesforce_client._is_queryable = mock.AsyncMock(return_value=True)
+    source.salesforce_client._select_queryable_fields = mock.AsyncMock(
+        return_value=[
+            "Name",
+            "Description",
+            "BillingAddress",
+            "Type",
+            "Website",
+            "Rating",
+            "Department",
+        ]
     )
     mock_responses.get(
         re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
         status=200,
-        payload=response_payload,
+        payload=ACCOUNT_RESPONSE_PAYLOAD,
     )
     async for account in source.salesforce_client.get_accounts():
-        assert account["Id"] == "1234"
-        assert account["_id"] == "1234"
+        assert account == expected_doc
 
     await source.close()
 
@@ -146,16 +312,13 @@ async def test_get_accounts_when_success(mock_responses):
 @pytest.mark.asyncio
 @mock.patch("connectors.utils.apply_retry_strategy")
 async def test_get_accounts_when_invalid_request(apply_retry_strategy, mock_responses):
-    source = create_source(
-        SalesforceDataSource,
-        domain=TEST_DOMAIN,
-        client_id=TEST_CLIENT_ID,
-        client_secret=TEST_CLIENT_SECRET,
-    )
+    source = create_salesforce_source()
+
     response_payload = [
         {"message": "Unable to process query.", "errorCode": "INVALID_FIELD"}
     ]
 
+    source.salesforce_client._is_queryable = mock.AsyncMock(return_value=True)
     mock_responses.get(
         re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
         status=400,
@@ -165,6 +328,55 @@ async def test_get_accounts_when_invalid_request(apply_retry_strategy, mock_resp
         async for _ in source.salesforce_client.get_accounts():
             # TODO confirm error message when error handling is improved
             pass
+
+    await source.close()
+
+
+@pytest.mark.asyncio
+async def test_get_accounts_when_not_queryable_yields_nothing(mock_responses):
+    source = create_salesforce_source()
+
+    source.salesforce_client._is_queryable = mock.AsyncMock(return_value=False)
+    async for account in source.salesforce_client.get_accounts():
+        assert account == None
+
+    await source.close()
+
+
+@pytest.mark.asyncio
+async def test_get_opportunities_when_success(mock_responses):
+    source = create_salesforce_source()
+    expected_doc = {
+        "_id": f"salesforce_connector_opportunity_id",
+        "body": "An Opportunity!",
+        "content_source_id": "opportunity_id",
+        "created_at": "",
+        "last_updated": "",
+        "next_step": None,
+        "owner": "User's Name",
+        "owner_email": "email@fake.com",
+        "source": "salesforce",
+        "status": "Qualification",
+        "title": "Another Opportunity Generator",
+        "type": "opportunity",
+        "url": f"{TEST_BASE_URL}/opportunity_id",
+    }
+
+    source.salesforce_client._is_queryable = mock.AsyncMock(return_value=True)
+    source.salesforce_client._select_queryable_fields = mock.AsyncMock(
+        return_value=[
+            "Name",
+            "Description",
+            "StageName",
+        ]
+    )
+    mock_responses.get(
+        re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
+        status=200,
+        payload=OPPORTUNITY_RESPONSE_PAYLOAD,
+    )
+    async for account in source.salesforce_client.get_opportunities():
+        assert account == expected_doc
 
     await source.close()
 
