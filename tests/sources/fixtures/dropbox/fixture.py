1f22fd7b3b5cc0f05129644c5434c1b999753039
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-08-07 15:38:55 +0200

    Same for dropbox

--- tests/sources/fixtures/dropbox/fixture.py
+++ tests/sources/fixtures/dropbox/fixture.py
@@ -12,13 +12,85 @@ import os
 import random
 import string
 
+from random import choices
+from faker import Faker
+from functools import cached_property
 from flask import Flask, jsonify, make_response, request
 
-DATA_SIZE = os.environ.get("DATA_SIZE", "small").lower()
-_SIZES = {"small": 500000, "medium": 1000000, "large": 3000000}
-FILE_SIZE = _SIZES[DATA_SIZE]
-FILE_DATA = "".join([random.choice(string.ascii_letters) for _ in range(FILE_SIZE)])
+# TODO: make used generally
+class FakeProvider:
+    def __init__(self, seed=None):
+        self.seed = seed
+        self.fake = Faker()
+        if seed:
+            self.fake.seed_instance(seed)
 
+    @cached_property
+    def _cached_random_str(self):
+        return self.fake.pystr(min_chars=100 * 1024, max_chars=100 * 1024 + 1)
+
+    def small_text(self):
+        # Up to 1KB of text
+        return self.generate_text(1 * 1024)
+
+    def medium_text(self):
+        # Up to 1MB of text
+        return self.generate_text(1024 * 1024)
+
+    def large_text(self):
+        # Up to 4MB of text
+        return self.generate_text(4 * 1024 * 1024)
+
+    def extra_large_text(self):
+        return self.generate_text(20 * 1024 * 1024)
+
+    def small_html(self):
+        # Around 100KB
+        return self.generate_html(1)
+
+    def medium_html(self):
+        # Around 1MB
+        return self.generate_html(1 * 10)
+
+    def large_html(self):
+        # Around 8MB
+        return self.generate_html(8 * 10)
+
+    def extra_large_html(self):
+        # Around 25MB
+        return self.generate_html(25 * 10)
+
+    def generate_text(self, max_size):
+        return self.fake.text(max_nb_chars=max_size)
+
+    def generate_html(self, images_of_100kb):
+        img = self._cached_random_str  # 100kb
+        text = self.small_text()
+
+        images = []
+        for _ in range(images_of_100kb):
+            images.append(f"<img src='{img}'/>")
+
+        return f"<html><head></head><body><div>{text}</div><div>{'<br/>'.join(images)}</div></body></html>"
+
+fake_provider = FakeProvider()
+
+DATA_SIZE = os.environ.get("DATA_SIZE", "medium")
+
+# TODO: change number of files based on DATA_SIZE
+match DATA_SIZE:
+    case "small":
+        pass
+    case "medium":
+        pass
+    case "large":
+        pass
+
+population = [fake_provider.small_html(), fake_provider.medium_html(), fake_provider.large_html(), fake_provider.extra_large_html()]
+weights = [0.58, 0.3, 0.1, 0.02]
+
+def get_file():
+    return choices(population, weights)[0]
 
 class DropboxAPI:
     def __init__(self):
@@ -106,8 +178,8 @@ class DropboxAPI:
             file_entry = {
                 ".tag": "file",
                 "name": f"test_file{entry}.txt",
-                "path_lower": f"/test_folder{entry}/test_file{entry}.txt",
-                "path_display": f"/test_folder{entry}/test_file{entry}.txt",
+                "path_lower": f"/test_folder{entry}/test_file{entry}.html",
+                "path_display": f"/test_folder{entry}/test_file{entry}.html",
                 "id": f"id:file{entry}",
                 "client_modified": "2023-01-01T01:01:01Z",
                 "server_modified": "2023-01-01T01:01:01Z",
@@ -141,8 +213,8 @@ class DropboxAPI:
             file_entry = {
                 ".tag": "file",
                 "name": f"test_file{entry}.txt",
-                "path_lower": f"/test_folder{entry}/test_file{entry}.txt",
-                "path_display": f"/test_folder{entry}/test_file{entry}.txt",
+                "path_lower": f"/test_folder{entry}/test_file{entry}.html",
+                "path_display": f"/test_folder{entry}/test_file{entry}.html",
                 "id": f"id:file{entry}",
                 "client_modified": "2023-01-01T01:01:01Z",
                 "server_modified": "2023-01-01T01:01:01Z",
@@ -167,7 +239,7 @@ class DropboxAPI:
                         "shared_link_policy": {".tag": "anyone"},
                         "viewer_info_policy": {".tag": "enabled"},
                     },
-                    "preview_url": f"https://www.dropbox.com/scl/fi/{entry}/shared-file{entry}.txt",
+                    "preview_url": f"https://www.dropbox.com/scl/fi/{entry}/shared-file{entry}.html",
                     "time_invited": "2023-01-01T01:01:01Z",
                 }
             )
@@ -186,7 +258,7 @@ class DropboxAPI:
                         "shared_link_policy": {".tag": "anyone"},
                         "viewer_info_policy": {".tag": "enabled"},
                     },
-                    "preview_url": f"https://www.dropbox.com/scl/fi/{entry}/shared-file{entry}.txt",
+                    "preview_url": f"https://www.dropbox.com/scl/fi/{entry}/shared-file{entry}.html",
                     "time_invited": "2023-01-01T01:01:01Z",
                 }
             )
@@ -256,13 +328,13 @@ class DropboxAPI:
         return jsonify(res)
 
     def download_file(self):
-        return io.BytesIO(bytes(FILE_DATA, encoding="utf-8"))
+        return io.BytesIO(bytes(get_file(), encoding="utf-8"))
 
     def download_paper_file(self):
-        return io.BytesIO(bytes(FILE_DATA, encoding="utf-8"))
+        return io.BytesIO(bytes(get_file(), encoding="utf-8"))
 
     def download_shared_file(self):
-        return io.BytesIO(bytes(FILE_DATA, encoding="utf-8"))
+        return io.BytesIO(bytes(get_file(), encoding="utf-8"))
 
 
 if __name__ == "__main__":
