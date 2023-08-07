cb60bb5de17a59df9b6f4c3a3eb6d695b48e6f8c
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-08-07 16:34:14 +0200

    Same for github

--- tests/sources/fixtures/github/fixture.py
+++ tests/sources/fixtures/github/fixture.py
@@ -9,14 +9,86 @@ import base64
 import os
 import random
 import string
+from random import choices
+from faker import Faker
+from functools import cached_property
 
 from flask import Flask, make_response, request
 
-DATA_SIZE = os.environ.get("DATA_SIZE", "small").lower()
-_SIZES = {"small": 500000, "medium": 1000000, "large": 3000000}
-FILE_SIZE = _SIZES[DATA_SIZE]
-LARGE_DATA = "".join([random.choice(string.ascii_letters) for _ in range(FILE_SIZE)])
+# TODO: make used generally
+class FakeProvider:
+    def __init__(self, seed=None):
+        self.seed = seed
+        self.fake = Faker()
+        if seed:
+            self.fake.seed_instance(seed)
 
+    @cached_property
+    def _cached_random_str(self):
+        return self.fake.pystr(min_chars=100 * 1024, max_chars=100 * 1024 + 1)
+
+    def small_text(self):
+        # Up to 1KB of text
+        return self.generate_text(1 * 1024)
+
+    def medium_text(self):
+        # Up to 1MB of text
+        return self.generate_text(1024 * 1024)
+
+    def large_text(self):
+        # Up to 4MB of text
+        return self.generate_text(4 * 1024 * 1024)
+
+    def extra_large_text(self):
+        return self.generate_text(20 * 1024 * 1024)
+
+    def small_html(self):
+        # Around 100KB
+        return self.generate_html(1)
+
+    def medium_html(self):
+        # Around 1MB
+        return self.generate_html(1 * 10)
+
+    def large_html(self):
+        # Around 8MB
+        return self.generate_html(8 * 10)
+
+    def extra_large_html(self):
+        # Around 25MB
+        return self.generate_html(25 * 10)
+
+    def generate_text(self, max_size):
+        return self.fake.text(max_nb_chars=max_size)
+
+    def generate_html(self, images_of_100kb):
+        img = self._cached_random_str  # 100kb
+        text = self.small_text()
+
+        images = []
+        for _ in range(images_of_100kb):
+            images.append(f"<img src='{img}'/>")
+
+        return f"<html><head></head><body><div>{text}</div><div>{'<br/>'.join(images)}</div></body></html>"
+
+fake_provider = FakeProvider()
+
+DATA_SIZE = os.environ.get("DATA_SIZE", "medium")
+
+# TODO: change number of files based on DATA_SIZE
+match DATA_SIZE:
+    case "small":
+        pass
+    case "medium":
+        pass
+    case "large":
+        pass
+
+population = [fake_provider.small_html(), fake_provider.medium_html(), fake_provider.large_html(), fake_provider.extra_large_html()]
+weights = [0.58, 0.3, 0.1, 0.02]
+
+def get_file():
+    return choices(population, weights)[0]
 
 app = Flask(__name__)
 
@@ -34,13 +106,14 @@ class GitHubAPI:
             "/api/v3/repos/demo_user/demo_repo/git/trees/main", methods=["GET"]
         )(self.get_tree)
         self.app.route(
-            "/api/v3/repos/demo_user/demo_repo/git/blobs/<string:commit_id>",
+            "/api/v3/repos/demo_user/demo_repo/git/blobs/<string:file_id>",
             methods=["GET"],
         )(self.get_content)
         self.app.route("/api/v3/repos/demo_user/demo_repo/commits", methods=["GET"])(
             self.get_commits
         )
         self.app.route("/api/graphql", methods=["POST"])(self.mock_graphql_response)
+        self.files = {}
 
     def encode_cursor(self, value):
         return base64.b64encode(str(value).encode()).decode()
@@ -282,29 +355,31 @@ class GitHubAPI:
 
     def get_tree(self):
         args = request.args
+        tree_list = []
         if args.get("recursive") == "1":
-            tree_list = [
-                {
+            for file_number in range(self.file_count):
+                file = get_file()
+                self.files[str(file_number)] = file
+                tree_list.append({
                     "path": f"dummy_file_{file_number}.md",
                     "mode": "100644",
                     "type": "blob",
                     "sha": file_number,
-                    "size": FILE_SIZE,
+                    "size": len(file.encode("utf-8")),
                     "url": f"http://127.0.0.1:9091/api/v3/repos/demo_user/demo_repo/git/blobs/{file_number}",
-                }
-                for file_number in range(self.file_count)
-            ]
+                })
         self.file_count = 2000
         return {"tree": tree_list}
 
-    def get_content(self, commit_id):
+    def get_content(self, file_id):
+        file = self.files[file_id]
         return {
-            "name": f"dummy_file_{commit_id}.md",
-            "path": f"dummy_file_{commit_id}.md",
-            "sha": commit_id,
-            "size": FILE_SIZE,
+            "name": f"dummy_file_{file_id}.md",
+            "path": f"dummy_file_{file_id}.md",
+            "sha":file_id,
+            "size": len(file.encode("utf-8")),
             "type": "file",
-            "content": encode_data(LARGE_DATA),
+            "content": encode_data(file),
             "encoding": "base64",
         }
 
