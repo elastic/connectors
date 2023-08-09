90b14d9eff2a0fae1dc1ce52d325affcacce149e
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2023-08-09 12:49:59 +0200

    Fix test source building

--- tests/sources/test_salesforce.py
+++ tests/sources/test_salesforce.py
@@ -6,6 +6,7 @@
 """Tests the Salesforce source class methods"""
 import re
 import time
+from contextlib import asynccontextmanager
 from unittest import TestCase, mock
 
 import pytest
@@ -102,13 +103,15 @@ OPPORTUNITY_RESPONSE_PAYLOAD = {
 }
 
 
-def create_salesforce_source():
-    return create_source(
+@asynccontextmanager
+async def create_salesforce_source():
+    async with create_source(
         SalesforceDataSource,
         domain=TEST_DOMAIN,
         client_id=TEST_CLIENT_ID,
         client_secret=TEST_CLIENT_SECRET,
-    )
+    ) as source:
+        yield source
 
 
 async def create_fake_coroutine(data):
@@ -124,45 +127,56 @@ def test_get_default_configuration():
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("field", ["client_id", "client_secret", "domain"])
-async def test_validate_config_missing_fields_then_raise(field):
-    source = create_salesforce_source()
-    source.configuration.set_field(name=field, value="")
-
-    with pytest.raises(ConfigurableFieldValueError):
-        await source.validate_config()
+@pytest.mark.parametrize(
+    "domain, client_id, client_secret",
+    [
+        ("", TEST_CLIENT_ID, TEST_CLIENT_SECRET),
+        (TEST_DOMAIN, "", TEST_CLIENT_SECRET),
+        (TEST_DOMAIN, TEST_CLIENT_ID, ""),
+    ],
+)
+async def test_validate_config_missing_fields_then_raise(
+    domain, client_id, client_secret
+):
+    async with create_source(
+        SalesforceDataSource,
+        domain=domain,
+        client_id=client_id,
+        client_secret=client_secret,
+    ) as source:
+        with pytest.raises(ConfigurableFieldValueError):
+            await source.validate_config()
 
 
 @pytest.mark.asyncio
 async def test_ping_with_successful_connection(mock_responses):
-    source = create_salesforce_source()
-    mock_responses.head(TEST_BASE_URL, status=200)
+    async with create_salesforce_source() as source:
+        mock_responses.head(TEST_BASE_URL, status=200)
 
-    await source.ping()
-    await source.close()
+        await source.ping()
 
 
 @pytest.mark.asyncio
 async def test_get_token_with_successful_connection(mock_responses):
-    source = create_salesforce_source()
-    response_payload = {
-        "access_token": "foo",
-        "signature": "bar",
-        "instance_url": "https://fake.my.salesforce.com",
-        "id": "https://login.salesforce.com/id/1234",
-        "token_type": "Bearer",
-        "issued_at": SECONDS_SINCE_EPOCH,
-    }
+    async with create_salesforce_source() as source:
+        response_payload = {
+            "access_token": "foo",
+            "signature": "bar",
+            "instance_url": "https://fake.my.salesforce.com",
+            "id": "https://login.salesforce.com/id/1234",
+            "token_type": "Bearer",
+            "issued_at": SECONDS_SINCE_EPOCH,
+        }
 
-    mock_responses.post(
-        f"{TEST_BASE_URL}/services/oauth2/token", status=200, payload=response_payload
-    )
-    await source.salesforce_client.get_token()
+        mock_responses.post(
+            f"{TEST_BASE_URL}/services/oauth2/token",
+            status=200,
+            payload=response_payload,
+        )
+        await source.salesforce_client.get_token()
 
-    assert source.salesforce_client.token == "foo"
-    assert source.salesforce_client.token_issued_at == SECONDS_SINCE_EPOCH
-
-    await source.close()
+        assert source.salesforce_client.token == "foo"
+        assert source.salesforce_client.token_issued_at == SECONDS_SINCE_EPOCH
 
 
 @pytest.mark.asyncio
@@ -170,24 +184,25 @@ async def test_get_token_with_successful_connection(mock_responses):
 async def test_get_token_with_bad_domain_raises_error(
     apply_retry_strategy, mock_responses
 ):
-    source = create_salesforce_source()
-    apply_retry_strategy.return_value = mock.Mock()
+    async with create_salesforce_source() as source:
+        apply_retry_strategy.return_value = mock.Mock()
 
-    response_payload = {
-        "access_token": "foo",
-        "signature": "bar",
-        "instance_url": "https://fake.my.salesforce.com",
-        "id": "https://login.salesforce.com/id/1234",
-        "token_type": "Bearer",
-        "issued_at": SECONDS_SINCE_EPOCH,
-    }
+        response_payload = {
+            "access_token": "foo",
+            "signature": "bar",
+            "instance_url": "https://fake.my.salesforce.com",
+            "id": "https://login.salesforce.com/id/1234",
+            "token_type": "Bearer",
+            "issued_at": SECONDS_SINCE_EPOCH,
+        }
 
-    mock_responses.post(
-        f"{TEST_BASE_URL}/services/oauth2/token", status=400, payload=response_payload
-    )
-    with pytest.raises(ClientConnectionError):
-        await source.salesforce_client.get_token()
-    await source.close()
+        mock_responses.post(
+            f"{TEST_BASE_URL}/services/oauth2/token",
+            status=400,
+            payload=response_payload,
+        )
+        with pytest.raises(ClientConnectionError):
+            await source.salesforce_client.get_token()
 
 
 @pytest.mark.asyncio
@@ -206,30 +221,28 @@ async def test_get_token_with_bad_domain_raises_error(
     ["FooField", "BarField", "ArghField"],
 )
 async def test_get_queryable_sobjects(mock_responses, sobject, expected_result):
-    source = create_salesforce_source()
-    response_payload = {
-        "sobjects": [
-            {
-                "queryable": True,
-                "name": "FooField",
-            },
-            {
-                "queryable": False,
-                "name": "BarField",
-            },
-        ],
-    }
+    async with create_salesforce_source() as source:
+        response_payload = {
+            "sobjects": [
+                {
+                    "queryable": True,
+                    "name": "FooField",
+                },
+                {
+                    "queryable": False,
+                    "name": "BarField",
+                },
+            ],
+        }
 
-    mock_responses.get(
-        f"{TEST_BASE_URL}/services/data/v58.0/sobjects",
-        status=200,
-        payload=response_payload,
-    )
+        mock_responses.get(
+            f"{TEST_BASE_URL}/services/data/v58.0/sobjects",
+            status=200,
+            payload=response_payload,
+        )
 
-    queryable = await source.salesforce_client._is_queryable(sobject)
-    assert queryable == expected_result
-
-    await source.close()
+        queryable = await source.salesforce_client._is_queryable(sobject)
+        assert queryable == expected_result
 
 
 @pytest.mark.asyncio
@@ -239,153 +252,141 @@ async def test_get_queryable_sobjects(mock_responses, sobject, expected_result):
     ["FooField", "BarField", "ArghField"],
 )
 async def test_get_queryable_fields(mock_responses):
-    source = create_salesforce_source()
-    expected_fields = [
-        {
-            "name": "FooField",
-        },
-        {
-            "name": "BarField",
-        },
-        {"name": "ArghField"},
-    ]
-    response_payload = {
-        "fields": expected_fields,
-    }
-    mock_responses.get(
-        f"{TEST_BASE_URL}/services/data/v58.0/sobjects/Account/describe",
-        status=200,
-        payload=response_payload,
-    )
+    async with create_salesforce_source() as source:
+        expected_fields = [
+            {
+                "name": "FooField",
+            },
+            {
+                "name": "BarField",
+            },
+            {"name": "ArghField"},
+        ]
+        response_payload = {
+            "fields": expected_fields,
+        }
+        mock_responses.get(
+            f"{TEST_BASE_URL}/services/data/v58.0/sobjects/Account/describe",
+            status=200,
+            payload=response_payload,
+        )
 
-    queryable_fields = await source.salesforce_client._select_queryable_fields(
-        "Account", ["FooField", "BarField", "NarghField"]
-    )
-    TestCase().assertCountEqual(queryable_fields, ["FooField", "BarField"])
-
-    await source.close()
+        queryable_fields = await source.salesforce_client._select_queryable_fields(
+            "Account", ["FooField", "BarField", "NarghField"]
+        )
+        TestCase().assertCountEqual(queryable_fields, ["FooField", "BarField"])
 
 
 @pytest.mark.asyncio
 async def test_get_accounts_when_success(mock_responses):
-    source = create_salesforce_source()
-    expected_doc = {
-        "_id": "account_id",
-        "account_type": "Customer - Direct",
-        "address": "The Burrow under the Hill, Bag End, Hobbiton, The Shire, Eriador, 111, Middle Earth",
-        "body": "A fantastic opportunity!",
-        "content_source_id": "account_id",
-        "created_at": "",
-        "last_updated": "",
-        "owner": "Owner's Name",
-        "owner_email": "email@fake.com",
-        "open_activities": "",
-        "open_activities_urls": "",
-        "opportunity_name": "Opportunity Generator",
-        "opportunity_status": "Closed Won",
-        "opportunity_url": f"{TEST_BASE_URL}/opportunity_id",
-        "rating": "Cold",
-        "source": "salesforce",
-        "tags": ["Customer - Direct"],
-        "title": "Salesforce Account 1",
-        "type": "account",
-        "url": f"{TEST_BASE_URL}/account_id",
-        "website_url": "www.fake.com",
-    }
+    async with create_salesforce_source() as source:
+        expected_doc = {
+            "_id": "account_id",
+            "account_type": "Customer - Direct",
+            "address": "The Burrow under the Hill, Bag End, Hobbiton, The Shire, Eriador, 111, Middle Earth",
+            "body": "A fantastic opportunity!",
+            "content_source_id": "account_id",
+            "created_at": "",
+            "last_updated": "",
+            "owner": "Owner's Name",
+            "owner_email": "email@fake.com",
+            "open_activities": "",
+            "open_activities_urls": "",
+            "opportunity_name": "Opportunity Generator",
+            "opportunity_status": "Closed Won",
+            "opportunity_url": f"{TEST_BASE_URL}/opportunity_id",
+            "rating": "Cold",
+            "source": "salesforce",
+            "tags": ["Customer - Direct"],
+            "title": "Salesforce Account 1",
+            "type": "account",
+            "url": f"{TEST_BASE_URL}/account_id",
+            "website_url": "www.fake.com",
+        }
 
-    source.salesforce_client._is_queryable = mock.AsyncMock(return_value=True)
-    source.salesforce_client._select_queryable_fields = mock.AsyncMock(
-        return_value=[
-            "Name",
-            "Description",
-            "BillingAddress",
-            "Type",
-            "Website",
-            "Rating",
-            "Department",
-        ]
-    )
-    mock_responses.get(
-        re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
-        status=200,
-        payload=ACCOUNT_RESPONSE_PAYLOAD,
-    )
-    async for account in source.salesforce_client.get_accounts():
-        assert account == expected_doc
-
-    await source.close()
+        source.salesforce_client._is_queryable = mock.AsyncMock(return_value=True)
+        source.salesforce_client._select_queryable_fields = mock.AsyncMock(
+            return_value=[
+                "Name",
+                "Description",
+                "BillingAddress",
+                "Type",
+                "Website",
+                "Rating",
+                "Department",
+            ]
+        )
+        mock_responses.get(
+            re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
+            status=200,
+            payload=ACCOUNT_RESPONSE_PAYLOAD,
+        )
+        async for account in source.salesforce_client.get_accounts():
+            assert account == expected_doc
 
 
 @pytest.mark.asyncio
 @mock.patch("connectors.utils.apply_retry_strategy")
 async def test_get_accounts_when_invalid_request(apply_retry_strategy, mock_responses):
-    source = create_salesforce_source()
+    async with create_salesforce_source() as source:
+        response_payload = [
+            {"message": "Unable to process query.", "errorCode": "INVALID_FIELD"}
+        ]
 
-    response_payload = [
-        {"message": "Unable to process query.", "errorCode": "INVALID_FIELD"}
-    ]
-
-    source.salesforce_client._is_queryable = mock.AsyncMock(return_value=True)
-    mock_responses.get(
-        re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
-        status=400,
-        payload=response_payload,
-    )
-    with pytest.raises(ClientConnectionError):
-        async for _ in source.salesforce_client.get_accounts():
-            # TODO confirm error message when error handling is improved
-            pass
-
-    await source.close()
+        source.salesforce_client._is_queryable = mock.AsyncMock(return_value=True)
+        mock_responses.get(
+            re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
+            status=400,
+            payload=response_payload,
+        )
+        with pytest.raises(ClientConnectionError):
+            async for _ in source.salesforce_client.get_accounts():
+                # TODO confirm error message when error handling is improved
+                pass
 
 
 @pytest.mark.asyncio
 async def test_get_accounts_when_not_queryable_yields_nothing(mock_responses):
-    source = create_salesforce_source()
-
-    source.salesforce_client._is_queryable = mock.AsyncMock(return_value=False)
-    async for account in source.salesforce_client.get_accounts():
-        assert account is None
-
-    await source.close()
+    async with create_salesforce_source() as source:
+        source.salesforce_client._is_queryable = mock.AsyncMock(return_value=False)
+        async for account in source.salesforce_client.get_accounts():
+            assert account is None
 
 
 @pytest.mark.asyncio
 async def test_get_opportunities_when_success(mock_responses):
-    source = create_salesforce_source()
-    expected_doc = {
-        "_id": "opportunity_id",
-        "body": "An Opportunity!",
-        "content_source_id": "opportunity_id",
-        "created_at": "",
-        "last_updated": "",
-        "next_step": None,
-        "owner": "User's Name",
-        "owner_email": "email@fake.com",
-        "source": "salesforce",
-        "status": "Qualification",
-        "title": "Another Opportunity Generator",
-        "type": "opportunity",
-        "url": f"{TEST_BASE_URL}/opportunity_id",
-    }
+    async with create_salesforce_source() as source:
+        expected_doc = {
+            "_id": "opportunity_id",
+            "body": "An Opportunity!",
+            "content_source_id": "opportunity_id",
+            "created_at": "",
+            "last_updated": "",
+            "next_step": None,
+            "owner": "User's Name",
+            "owner_email": "email@fake.com",
+            "source": "salesforce",
+            "status": "Qualification",
+            "title": "Another Opportunity Generator",
+            "type": "opportunity",
+            "url": f"{TEST_BASE_URL}/opportunity_id",
+        }
 
-    source.salesforce_client._is_queryable = mock.AsyncMock(return_value=True)
-    source.salesforce_client._select_queryable_fields = mock.AsyncMock(
-        return_value=[
-            "Name",
-            "Description",
-            "StageName",
-        ]
-    )
-    mock_responses.get(
-        re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
-        status=200,
-        payload=OPPORTUNITY_RESPONSE_PAYLOAD,
-    )
-    async for account in source.salesforce_client.get_opportunities():
-        assert account == expected_doc
-
-    await source.close()
+        source.salesforce_client._is_queryable = mock.AsyncMock(return_value=True)
+        source.salesforce_client._select_queryable_fields = mock.AsyncMock(
+            return_value=[
+                "Name",
+                "Description",
+                "StageName",
+            ]
+        )
+        mock_responses.get(
+            re.compile(f"{TEST_BASE_URL}/services/data/v58.0/query*"),
+            status=200,
+            payload=OPPORTUNITY_RESPONSE_PAYLOAD,
+        )
+        async for account in source.salesforce_client.get_opportunities():
+            assert account == expected_doc
 
 
 @pytest.mark.asyncio
