222c0c792f71e160db0be13bfe42b1801b952e29
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2023-08-10 11:20:28 +0200

    [Salesforce] Ingest Contacts and Leads (#1407)

--- connectors/sources/salesforce.py
+++ connectors/sources/salesforce.py
@@ -17,7 +17,6 @@ from connectors.utils import CancellableSleeps, retryable
 
 RETRIES = 3
 RETRY_INTERVAL = 1
-QUERY_PAGINATION_LIMIT = 100  # TODO: maybe make the limit configurable
 
 BASE_URL = "https://<domain>.my.salesforce.com"
 API_VERSION = "v58.0"
@@ -26,16 +25,29 @@ QUERY_ENDPOINT = f"/services/data/{API_VERSION}/query"
 DESCRIBE_ENDPOINT = f"/services/data/{API_VERSION}/sobjects"
 DESCRIBE_SOBJECT_ENDPOINT = f"/services/data/{API_VERSION}/sobjects/<sobject>/describe"
 
-RELEVANT_SOBJECTS = ["Account", "Opportunity"]
+RELEVANT_SOBJECTS = ["Account", "Contact", "Lead", "Opportunity", "User"]
 RELEVANT_SOBJECT_FIELDS = [
-    "Name",
-    "Description",
+    "AccountId",
     "BillingAddress",
+    "Company",
+    "ConvertedAccountId",
+    "ConvertedContactId",
+    "ConvertedDate",
+    "ConvertedOpportunityId",
+    "Department",
+    "Description",
+    "Email",
+    "LeadSource",
+    "Name",
+    "OwnerId",
+    "Phone",
+    "PhotoUrl",
+    "Rating",
+    "StageName",
+    "Status",
+    "Title",
     "Type",
     "Website",
-    "Rating",
-    "Department",
-    "StageName",
 ]
 
 
@@ -63,8 +75,14 @@ class TokenFetchException(Exception):
     pass
 
 
-class RequestError(Exception):
-    """Notifies that a general uncaught 400 error occurred during a request"""
+class ConnectorRequestError(Exception):
+    """Notifies that a general uncaught 400 error occurred during a request, usually this is caused by the connector"""
+
+    pass
+
+
+class SalesforceServerError(Exception):
+    """Notifies that an internal server error occurred in Salesforce"""
 
     pass
 
@@ -82,6 +100,7 @@ class SalesforceClient:
 
         self._queryable_sobjects = None
         self._queryable_sobject_fields = None
+        self._sobjects_cache_by_type = None
 
         self.base_url = BASE_URL.replace("<domain>", configuration["domain"])
         self.api_token = SalesforceAPIToken(
@@ -127,8 +146,24 @@ class SalesforceClient:
             await self.session.close()
             del self.session
 
+    async def get_docs(self):
+        async for account in self.get_accounts():
+            yield account, None
+
+        async for opportunity in self.get_opportunities():
+            yield opportunity, None
+
+        async for contact in self.get_contacts():
+            yield contact, None
+
+        async for lead in self.get_leads():
+            yield lead, None
+
     async def get_accounts(self):
         if not await self._is_queryable("Account"):
+            self._logger.info(
+                "Object Account is not queryable, so they won't be ingested."
+            )
             return
 
         query = await self._accounts_query()
@@ -138,12 +173,55 @@ class SalesforceClient:
 
     async def get_opportunities(self):
         if not await self._is_queryable("Opportunity"):
+            self._logger.info(
+                "Object Opportunity is not queryable, so they won't be ingested."
+            )
             return
 
         query = await self._opportunities_query()
         async for records in self._yield_non_bulk_query_pages(query):
             for record in records:
                 yield self.doc_mapper.map_opportunity(record)
+
+    async def get_contacts(self):
+        if not await self._is_queryable("Contact"):
+            self._logger.info(
+                "Object Contact is not queryable, so they won't be ingested."
+            )
+            return
+
+        query = await self._contacts_query()
+        async for records in self._yield_non_bulk_query_pages(query):
+            for record in records:
+                sobjects_by_id = await self.sobjects_cache_by_type()
+                record["Account"] = sobjects_by_id["Account"].get(
+                    record.get("AccountId"), {}
+                )
+                record["Owner"] = sobjects_by_id["User"].get(record.get("OwnerId"), {})
+                yield self.doc_mapper.map_contact(record)
+
+    async def get_leads(self):
+        if not await self._is_queryable("Lead"):
+            self._logger.info(
+                "Object Lead is not queryable, so they won't be ingested."
+            )
+            return
+
+        query = await self._leads_query()
+        async for records in self._yield_non_bulk_query_pages(query):
+            for record in records:
+                sobjects_by_id = await self.sobjects_cache_by_type()
+                record["Owner"] = sobjects_by_id["User"].get(record.get("OwnerId"), {})
+                record["ConvertedAccount"] = sobjects_by_id["Account"].get(
+                    record.get("ConvertedAccountId"), {}
+                )
+                record["ConvertedContact"] = sobjects_by_id["Contact"].get(
+                    record.get("ConvertedContactId"), {}
+                )
+                record["ConvertedOpportunity"] = sobjects_by_id["Opportunity"].get(
+                    record.get("ConvertedOpportunityId"), {}
+                )
+                yield self.doc_mapper.map_lead(record)
 
     async def queryable_sobjects(self):
         """Cached async property"""
@@ -179,12 +257,59 @@ class SalesforceClient:
 
         return self._queryable_sobject_fields
 
+    async def sobjects_cache_by_type(self):
+        """Cached async property
+
+        Many sobjects require extra data that is taxing on the rate limiter
+        to repeatedly fetch each request.
+        Instead we cache them on the first request for re-use later.
+        """
+        if self._sobjects_cache_by_type is not None:
+            return self._sobjects_cache_by_type
+
+        self._sobjects_cache_by_type = {}
+        self._sobjects_cache_by_type["Account"] = await self._prepare_sobject_cache(
+            "Account"
+        )
+        self._sobjects_cache_by_type["Contact"] = await self._prepare_sobject_cache(
+            "Contact"
+        )
+        self._sobjects_cache_by_type["Opportunity"] = await self._prepare_sobject_cache(
+            "Opportunity"
+        )
+        self._sobjects_cache_by_type["User"] = await self._prepare_sobject_cache("User")
+        return self._sobjects_cache_by_type
+
+    async def _prepare_sobject_cache(self, sobject):
+        if not await self._is_queryable(sobject):
+            self._logger.info(f"{sobject} is not queryable, so they won't be cached.")
+            return {}
+
+        queryable_fields = ["Name"]
+        if sobject in ["User", "Contact", "Lead"]:
+            queryable_fields.append("Email")
+
+        sobjects = {}
+        query_builder = SalesforceSoqlBuilder(sobject)
+        query_builder.with_id()
+        query_builder.with_fields(queryable_fields)
+        query = query_builder.build()
+
+        async for records in self._yield_non_bulk_query_pages(query):
+            for record in records:
+                sobjects[record["Id"]] = record
+
+        return sobjects
+
     async def _is_queryable(self, sobject):
+        """User settings can cause sobjects to be non-queryable
+        Querying these causes errors, so we try to filter those out in advance
+        """
         return sobject.lower() in await self.queryable_sobjects()
 
     async def _select_queryable_fields(self, sobject, fields):
         """User settings can cause fields to be non-queryable
-        This causes
+        Querying these causes errors, so we try to filter those out in advance
         """
         sobject_fields = await self.queryable_sobject_fields()
         queryable_fields = sobject_fields.get(sobject, [])
@@ -192,7 +317,6 @@ class SalesforceClient:
 
     async def _yield_non_bulk_query_pages(self, soql_query):
         """loops through query response pages and yields lists of records"""
-
         url = f"{self.base_url}{QUERY_ENDPOINT}"
         params = {"q": soql_query}
 
@@ -209,7 +333,7 @@ class SalesforceClient:
             params = None
 
     def _auth_headers(self):
-        return {"authorization": f"Bearer {self.api_token.token}"}
+        return {"authorization": f"Bearer {self.api_token.token()}"}
 
     @retryable(
         retries=RETRIES,
@@ -232,6 +356,7 @@ class SalesforceClient:
                 raise e
 
     async def _get(self, url, params=None):
+        self._logger.debug(f"Sending request. Url: {url}, params: {params}")
         return await self.session.get(
             url,
             headers=self._auth_headers(),
@@ -261,26 +386,25 @@ class SalesforceClient:
                 raise RateLimitedException(
                     f"Salesforce is rate limiting this account. {exception_details}, details: {', '.join(error_codes)}"
                 ) from e
-            elif (
-                any(
-                    error in error_codes
-                    for error in [
-                        "INVALID_FIELD",
-                        "INVALID_TERM",
-                        "MALFORMED_QUERY",
-                    ]
-                )
-                in error_codes
+            elif any(
+                error in error_codes
+                for error in [
+                    "INVALID_FIELD",
+                    "INVALID_TERM",
+                    "MALFORMED_QUERY",
+                ]
             ):
                 raise InvalidQueryException(
                     f"The query was rejected by Salesforce. {exception_details}, details: {', '.join(error_codes)}, query: {', '.join([x['message'] for x in errors])}"
                 ) from e
             else:
-                raise RequestError(
+                raise ConnectorRequestError(
                     f"The request to Salesforce failed. {exception_details}, details: {', '.join(error_codes)}"
                 ) from e
         else:
-            raise e
+            raise SalesforceServerError(
+                f"Salesforce experienced an internal server error. {exception_details}."
+            )
 
     def _handle_response_body_error(self, error_list):
         if error_list is None or len(error_list) < 1:
@@ -342,6 +466,56 @@ class SalesforceClient:
         # TODO add uncommon_object_remote_fields
         query_builder.with_fields(["Owner.Id", "Owner.Name", "Owner.Email"])
 
+        return query_builder.build()
+
+    async def _contacts_query(self):
+        queryable_fields = await self._select_queryable_fields(
+            "Contact",
+            [
+                "Name",
+                "Description",
+                "Email",
+                "Phone",
+                "Title",
+                "PhotoUrl",
+                "LeadSource",
+                "AccountId",
+                "OwnerId",
+            ],
+        )
+        query_builder = SalesforceSoqlBuilder("Contact")
+        query_builder.with_id()
+        query_builder.with_default_metafields()
+        query_builder.with_fields(queryable_fields)
+        # TODO add uncommon_object_remote_fields
+        return query_builder.build()
+
+    async def _leads_query(self):
+        queryable_fields = await self._select_queryable_fields(
+            "Lead",
+            [
+                "Company",
+                "ConvertedAccountId",
+                "ConvertedContactId",
+                "ConvertedDate",
+                "ConvertedOpportunityId",
+                "Description",
+                "Email",
+                "LeadSource",
+                "Name",
+                "OwnerId",
+                "Phone",
+                "PhotoUrl",
+                "Rating",
+                "Status",
+                "Title",
+            ],
+        )
+        query_builder = SalesforceSoqlBuilder("Lead")
+        query_builder.with_id()
+        query_builder.with_default_metafields()
+        query_builder.with_fields(queryable_fields)
+        # TODO add uncommon_object_remote_fields
         return query_builder.build()
 
 
@@ -500,6 +674,92 @@ class SalesforceDocMapper:
             "url": f"{self.base_url}/{opportunity.get('Id')}",
         }
 
+    def map_contact(self, contact):
+        account = contact.get("Account", {})
+        account_id = contact.get("AccountId", "")
+        account_url = f"{self.base_url}/{account_id}" if account_id else ""
+
+        owner = contact.get("Owner", {})
+        owner_id = contact.get("OwnerId", "")
+        owner_url = f"{self.base_url}/{owner_id}" if owner_id else ""
+
+        photo_url = contact.get("PhotoUrl")
+        thumbnail = f"{self.base_url}{photo_url}" if photo_url else ""
+
+        return {
+            "_id": contact.get("Id"),
+            "account": account.get("Name"),
+            "account_url": account_url,
+            "body": contact.get("Description"),
+            "email": contact.get("Email"),
+            "job_title": contact.get("Title"),
+            "last_updated": contact.get("LastModifiedDate"),
+            "lead_source": contact.get("LeadSource"),
+            "owner": owner.get("Name"),
+            "owner_url": owner_url,
+            "phone": contact.get("Phone"),
+            "source": "salesforce",
+            "thumbnail": thumbnail,
+            "title": contact.get("Name"),
+            "type": "contact",
+            "url": f"{self.base_url}/{contact.get('Id')}",
+        }
+
+    def map_lead(self, lead):
+        owner = lead.get("Owner", {})
+        owner_id = lead.get("OwnerId", "")
+        owner_url = f"{self.base_url}/{owner_id}" if owner_id else ""
+
+        converted_account = lead.get("ConvertedAccount", {})
+        converted_account_id = converted_account.get("Id")
+        converted_account_url = (
+            f"{self.base_url}/{converted_account_id}" if converted_account_id else None
+        )
+
+        converted_contact = lead.get("ConvertedContact", {})
+        converted_contact_id = converted_account.get("Id")
+        converted_contact_url = (
+            f"{self.base_url}/{converted_contact_id}" if converted_contact_id else None
+        )
+
+        converted_opportunity = lead.get("ConvertedOpportunity", {})
+        converted_opportunity_id = converted_opportunity.get("Id")
+        converted_opportunity_url = (
+            f"{self.base_url}/{converted_opportunity_id}"
+            if converted_opportunity_id
+            else None
+        )
+
+        photo_url = lead.get("PhotoUrl")
+        thumbnail = f"{self.base_url}{photo_url}" if photo_url else None
+
+        return {
+            "_id": lead.get("Id"),
+            "body": lead.get("Description"),
+            "company": lead.get("Company"),
+            "converted_account": converted_account.get("Name"),
+            "converted_account_url": converted_account_url,
+            "converted_at": lead.get("ConvertedDate"),  # TODO convert
+            "converted_contact": converted_contact.get("Name"),
+            "converted_contact_url": converted_contact_url,
+            "converted_opportunity": converted_opportunity.get("Name"),
+            "converted_opportunity_url": converted_opportunity_url,
+            "email": lead.get("Email"),
+            "job_title": lead.get("Title"),
+            "last_updated": lead.get("LastModifiedDate"),
+            "lead_source": lead.get("LeadSource"),
+            "owner": owner.get("Name"),
+            "owner_url": owner_url,
+            "phone": lead.get("Phone"),
+            "rating": lead.get("Rating"),
+            "source": "salesforce",
+            "status": lead.get("Status"),
+            "title": lead.get("Name"),
+            "thumbnail": thumbnail,
+            "type": "lead",
+            "url": f"{self.base_url}/{lead.get('Id')}",
+        }
+
     def _format_address(self, address):
         if not address:
             return ""
@@ -569,8 +829,5 @@ class SalesforceDataSource(BaseDataSource):
         await self.salesforce_client.get_token()
 
         # TODO filtering
-        async for account in self.salesforce_client.get_accounts():
-            yield account, None
-
-        async for opportunity in self.salesforce_client.get_opportunities():
-            yield opportunity, None
+        async for doc in self.salesforce_client.get_docs():
+            yield doc
