efb0feda9847ba52d863fd5d138fc4793b4ad985
Artem Shelkovnikov <lavatroublebubble@gmail.com>
2023-08-15 14:09:31 +0200

    Populate webUrl for site pages and list item attachments (#1440)

--- tests/sources/fixtures/sharepoint_online/fixture.py
+++ tests/sources/fixtures/sharepoint_online/fixture.py
@@ -306,6 +306,7 @@ class RandomDataStorage:
                     "webUrl": f"{ROOT}/sites/{site['name']}",
                     "createdDateTime": "2023-05-31T16:08:46Z",
                     "lastModifiedDateTime": "2023-05-31T16:08:48Z",
+                    "siteCollection": {"hostname": ROOT},
                 }
             )
 
