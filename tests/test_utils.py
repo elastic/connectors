9864715f60e60d3c6a383ef92fe03223a745df22
Chenhui Wang <wangch079@outlook.com>
2023-08-17 15:57:04 +0800

    revert changes to exception logging

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -324,7 +324,7 @@ async def test_concurrent_runner_canceled_with_waiting_task():
 
 
 @pytest.mark.asyncio
-async def test_concurrent_runner_fails(patch_logger):
+async def test_concurrent_runner_fails():
     results = []
 
     def _results_callback(result):
@@ -343,7 +343,6 @@ async def test_concurrent_runner_fails(patch_logger):
 
     await runner.join()
     assert 5 not in results
-    patch_logger.assert_present("I FAILED")
 
 
 @pytest.mark.asyncio
