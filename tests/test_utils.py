f6ee044c9fca3d3bea5e0f30d59662946dbd6ca9
Chenhui Wang <wangch079@outlook.com>
2023-08-17 15:38:53 +0800

    Use Semaphore instead of asyncio.Event to control the max concurrency

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -324,7 +324,7 @@ async def test_concurrent_runner_canceled_with_waiting_task():
 
 
 @pytest.mark.asyncio
-async def test_concurrent_runner_fails():
+async def test_concurrent_runner_fails(patch_logger):
     results = []
 
     def _results_callback(result):
@@ -343,6 +343,7 @@ async def test_concurrent_runner_fails():
 
     await runner.join()
     assert 5 not in results
+    patch_logger.assert_present("I FAILED")
 
 
 @pytest.mark.asyncio
