3d9b55be5165af7330ebbd78f0894019f57917e3
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2023-08-25 15:54:05 +0200

    Fix lint

--- tests/test_source.py
+++ tests/test_source.py
@@ -114,7 +114,6 @@ def test_field_convert():
         Field("name", value={"foo", "bar"}, field_type="list").value, ["foo", "bar"]
     )
 
-
     # unsupported cases that aren't converted
     assert Field("name", value={"foo": "bar"}, field_type="dict").value == {
         "foo": "bar"
