2cc0597ab7afdf488bdfb174205927131e09fc38
Tim Grein <tim.grein@elastic.co>
2023-09-06 14:07:42 +0200

    Remove retryable

--- tests/sources/fixtures/fixture.py
+++ tests/sources/fixtures/fixture.py
@@ -19,7 +19,6 @@ from requests.adapters import HTTPAdapter, Retry
 from requests.auth import HTTPBasicAuth
 
 from connectors.logger import set_extra_logger
-from connectors.utils import retryable
 
 CONNECTORS_INDEX = ".elastic-connectors"
 
@@ -27,7 +26,6 @@ logger = logging.getLogger("ftest")
 set_extra_logger(logger, log_level=logging.DEBUG, prefix="FTEST")
 
 
-@retryable(retries=3, interval=1.0)
 def wait_for_es(url="http://localhost:9200", user="elastic", password="changeme"):
     print("Waiting for Elasticsearch to be up and running")
     basic = HTTPBasicAuth(user, password)
