27a647afa6f092fdb61f4d0b4e3004dd51b05255
Dmitrii Burlutckii <dmitrii.burlutckii@elastic.co>
2023-09-08 15:17:08 +0200

    Remove raise

--- connectors/utils.py
+++ connectors/utils.py
@@ -352,7 +352,6 @@ class ConcurrentTasks:
             logger.error(
                 f"Exception found for task {task.get_name()}: {task.exception()}"
             )
-            raise task.exception()
         if result_callback is not None:
             result_callback(task.result())
         # global callback
