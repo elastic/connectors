0f765305a65f48d80193b8e043b79f5d5793dc97
Jedr Blaszyk <jedrazb@gmail.com>
2023-09-18 10:33:49 +0200

    Some changes

--- connectors/sources/kafka.py
+++ connectors/sources/kafka.py
@@ -4,29 +4,49 @@
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
 
-from connectors.logger import logger
-from connectors.source import BaseDataSource
-
 import json
+from itertools import chain
 
 from aiokafka import AIOKafkaConsumer
 
+from connectors.source import BaseDataSource
 
 
 class KafkaClient:
+    def __init__(self, kafka_topic, kafka_bootstrap_servers, kafka_consumer_group):
+        self.kafka_topic = kafka_topic
+        self.kafka_bootstrap_servers = kafka_bootstrap_servers
+        self.kafka_consumer_group = kafka_consumer_group
 
-    def __init__(self, kafka_topic, kafka_bootstrap_servers, kafka_consumer_group, auto_offset_reset='latest'):
-        self.kafka_consumer = AIOKafkaConsumer(
-            kafka_topic,
-            bootstrap_servers=kafka_bootstrap_servers,
-            group_id=kafka_consumer_group,
-            enable_auto_commit=True,
+    def _get_consumer(self, auto_offset_reset="earliest"):
+        kafka_consumer = AIOKafkaConsumer(
+            self.kafka_topic,
+            bootstrap_servers=self.kafka_bootstrap_servers,
+            group_id=self.kafka_consumer_group,
+            enable_auto_commit=False,
             auto_offset_reset=auto_offset_reset,
-            value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+            key_deserializer=lambda k: json.loads(k.decode("utf-8")),
+            value_deserializer=lambda v: json.loads(v.decode("utf-8")),
         )
+        return kafka_consumer
 
-    async def connect_to_kafka(self):
-        self.kafka_consumer.start()
+    def set_logger(self, logger_):
+        self._logger = logger_
+
+    async def ping(self):
+        async with self._get_consumer() as consumer:
+            await consumer.topics()
+
+    async def consume_all_messages(self):
+        async with self._get_consumer() as consumer:
+            msgs = True
+            while msgs:
+                msgs = await consumer.getmany(timeout_ms=2500, max_records=10)
+                msg_list = msgs.values()
+                # same pkey in same parition, so no need to worry about global ordering here
+                for msg in list(chain.from_iterable(msg_list)):
+                    # todo: check schema for pkey
+                    yield msg.key, msg.value, msg.timestamp
 
 
 class KafkaDataSource(BaseDataSource):
@@ -38,6 +58,12 @@ class KafkaDataSource(BaseDataSource):
     def __init__(self, configuration):
         super().__init__(configuration=configuration)
 
+        self.client = KafkaClient(
+            kafka_topic=self.configuration["kafka_topic"],
+            kafka_bootstrap_servers=f'{self.configuration["kafka_host"]}:{self.configuration["kafka_port"]}',
+            kafka_consumer_group=self.configuration["kafka_consumer_group"],
+        )
+
     @classmethod
     def get_default_configuration(cls):
         """Get the default configuration for the GMail connector.
@@ -47,36 +73,59 @@ class KafkaDataSource(BaseDataSource):
 
         return {
             "kafka_host": {
-                "display": "textarea",
+                "display": "text",
                 "label": "Kafka cluster host",
                 "order": 1,
                 "required": True,
                 "type": "str",
             },
             "kafka_port": {
-                "display": "textarea",
+                "display": "text",
                 "label": "Kafka cluster port",
                 "order": 2,
                 "required": True,
                 "type": "int",
             },
             "kafka_topic": {
-                "display": "textarea",
+                "display": "text",
                 "label": "Kafka topic",
                 "order": 3,
                 "required": True,
                 "type": "str",
             },
             "kafka_consumer_group": {
-                "display": "textarea",
+                "display": "next",
                 "label": "Kafka consumer group",
                 "order": 4,
                 "required": True,
                 "tooltip": "Kafka consumer group defined by user, make sure it's unique",
                 "type": "str",
-            }
+            },
         }
 
     def _set_internal_logger(self):
-        # self.client.set_logger(self._logger)
-        pass
+        self.client.set_logger(self._logger)
+
+    async def ping(self):
+        """Verify the connection with Kafka"""
+        try:
+            await self.client.ping()
+            self._logger.info("Successfully connected to Kafka.")
+        except Exception:
+            self._logger.exception("Error while connecting to Kafka.")
+            raise
+
+    def _process_message(self, message_key, message_value, message_timestamp):
+        return {
+            "_id": message_key.get("payload").get("id"),
+            "_timestamp": message_timestamp,
+            **message_value.get("payload").get("after", {}),
+        }
+
+    async def get_docs(self, filtering=None):
+        async for message_key, message_value, message_timestamp in self.client.consume_all_messages():
+            processed_message = self._process_message(
+                message_key, message_value, message_timestamp
+            )
+            self._logger.info([processed_message])
+            yield processed_message, None
