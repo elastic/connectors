789a157a31cd64e5a3ee81b01bd6df091b92242a
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-10-02 13:05:20 +0200

    Make Azure Blob Storage ftest use shared setup

--- tests/sources/fixtures/azure_blob_storage/fixture.py
+++ tests/sources/fixtures/azure_blob_storage/fixture.py
@@ -3,55 +3,52 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
-import random
-import string
+import os
 
 from azure.storage.blob import BlobServiceClient
+from tests.commons import WeightedFakeProvider
 
-CONTAINER = 2
-LARGE_CONTAINER = 3
-SMALL_CONTAINER = 7
-BLOB_COUNT = 1000
 CONNECTION_STRING = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
 
+fake_provider = WeightedFakeProvider()
 
-def random_text(k=1024 * 20):
-    return "".join(random.choices(string.ascii_uppercase + string.digits, k=k))
+DATA_SIZE = os.environ.get("DATA_SIZE", "medium")
+
+CONTAINERS_TO_DELETE = 1
+
+match DATA_SIZE:
+    case "small":
+        CONTAINER_COUNT = 4
+        BLOB_COUNT = 50
+    case "medium":
+        CONTAINER_COUNT = 6
+        BLOB_COUNT = 200
+    case "large":
+        CONTAINER_COUNT = 10
+        BLOB_COUNT = 1000
 
 
-BIG_TEXT = random_text()
+def get_num_docs():
+    print((CONTAINER_COUNT - CONTAINERS_TO_DELETE) * BLOB_COUNT)
 
 
 def load():
-    """Method for generating 10k document for azurite emulator"""
+    """Method for generating document for azurite emulator"""
     try:
         blob_service_client = BlobServiceClient.from_connection_string(
             CONNECTION_STRING
         )
 
-        for container_id in range(0, SMALL_CONTAINER):
+        for container_id in range(0, CONTAINER_COUNT):
             container_client = blob_service_client.get_container_client(
-                f"containersmall{container_id}"
+                f"container{container_id}"
             )
             container_client.create_container()
 
             for blob_id in range(0, BLOB_COUNT):
-                blob_client = container_client.get_blob_client(f"file{blob_id}.txt")
+                blob_client = container_client.get_blob_client(f"file{blob_id}.html")
                 blob_client.upload_blob(
-                    f"Testing blob{blob_id} document for container{container_id}",
-                    blob_type="BlockBlob",
-                )
-
-        for container_id in range(0, LARGE_CONTAINER):
-            container_client = blob_service_client.get_container_client(
-                f"containerlarge{container_id}"
-            )
-            container_client.create_container()
-
-            for blob_id in range(0, BLOB_COUNT):
-                blob_client = container_client.get_blob_client(f"file{blob_id}.txt")
-                blob_client.upload_blob(
-                    BIG_TEXT,
+                    fake_provider.get_html(),
                     blob_type="BlockBlob",
                 )
     except Exception as exception:
@@ -65,9 +62,9 @@ def remove():
             CONNECTION_STRING
         )
 
-        for container_id in range(0, CONTAINER):
+        for container_id in range(CONTAINERS_TO_DELETE):
             container_client = blob_service_client.get_container_client(
-                f"containersmall{container_id}"
+                f"container{container_id}"
             )
             container_client.delete_container()
     except Exception as exception:
