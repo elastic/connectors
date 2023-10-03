86279f2c629b5d72b1f813c85077fff1c1e77d00
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-10-03 11:35:57 +0200

    Fix linters

--- tests/sources/fixtures/azure_blob_storage/fixture.py
+++ tests/sources/fixtures/azure_blob_storage/fixture.py
@@ -6,6 +6,7 @@
 import os
 
 from azure.storage.blob import BlobServiceClient
+
 from tests.commons import WeightedFakeProvider
 
 CONNECTION_STRING = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
