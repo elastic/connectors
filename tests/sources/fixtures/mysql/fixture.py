b4888c4644d60afa982e19247f2775747621cc7e
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-10-12 12:58:11 +0200

    Force it do the check for all connectors

--- tests/sources/fixtures/mysql/fixture.py
+++ tests/sources/fixtures/mysql/fixture.py
@@ -11,7 +11,7 @@ from mysql.connector import connect
 
 DATA_SIZE = os.environ.get("DATA_SIZE", "small").lower()
 DATABASE_NAME = "customerinfo"
-_SIZES = {"small": 5, "medium": 10, "large": 300}
+_SIZES = {"small": 5, "medium": 10, "large": 30}
 NUM_TABLES = _SIZES[DATA_SIZE]
 
 
