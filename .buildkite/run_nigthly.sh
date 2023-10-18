8a11d3769b2b51d1c774eb5d06e14e4152871989
Tim Grein <tim.grein@elastic.co>
2023-10-18 09:20:56 +0200

    Correct secret paths.

--- .buildkite/run_nigthly.sh
+++ .buildkite/run_nigthly.sh
@@ -34,8 +34,8 @@ if [ -v BUILDKITE ]; then
   VAULT_ADDR=${VAULT_ADDR:-https://vault-ci-prod.elastic.dev}
   VAULT_USER="docker-swiftypeadmin"
   echo "Fetching Docker credentials for '$VAULT_USER' from Vault..."
-  DOCKER_USER=$(vault read -address "${VAULT_ADDR}" -field user_20230609 secret/ci/elastic-connectors-python/${VAULT_USER})
-  DOCKER_PASSWORD=$(vault read -address "${VAULT_ADDR}" -field secret_20230609 secret/ci/elastic-connectors-python/${VAULT_USER})
+  DOCKER_USER=$(vault read -address "${VAULT_ADDR}" -field user_20230609 secret/ci/elastic-connectors/${VAULT_USER})
+  DOCKER_PASSWORD=$(vault read -address "${VAULT_ADDR}" -field secret_20230609 secret/ci/elastic-connectors/${VAULT_USER})
   echo "Done!"
 
   # required by serverless
