f81bc8d7e7f60890e727efa98751bbd1fd81406b
Chenhui Wang <54903978+wangch079@users.noreply.github.com>
2023-11-02 13:52:59 +0800

    Use BoundedSemaphore instead of asyncio.Event to control the max concurrency (#1477)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -260,6 +260,56 @@ async def test_concurrent_runner():
 
 
 @pytest.mark.asyncio
+async def test_concurrent_runner_canceled():
+    results = []
+    tasks = []
+
+    def _results_callback(result):
+        results.append(result)
+
+    async def coroutine(i):
+        await asyncio.sleep(1)
+        return i
+
+    runner = ConcurrentTasks(max_concurrency=10, results_callback=_results_callback)
+    for i in range(10):
+        tasks.append(await runner.put(functools.partial(coroutine, i)))
+
+    runner.cancel()
+    await runner.join()
+    assert len(tasks) == 10
+    for task in tasks:
+        assert task.cancelled()
+    assert len(results) == 0
+
+
+@pytest.mark.asyncio
+async def test_concurrent_runner_canceled_with_waiting_task():
+    results = []
+
+    def _results_callback(result):
+        results.append(result)
+
+    async def coroutine(i, sleep_time):
+        await asyncio.sleep(sleep_time)
+        return i
+
+    runner = ConcurrentTasks(max_concurrency=10, results_callback=_results_callback)
+    for i in range(10):
+        await runner.put(functools.partial(coroutine, i, 1))  # long-running task
+
+    # create a task to put a waiting task so that it won't block
+    asyncio.create_task(runner.put(functools.partial(coroutine, 100, 0.1)))
+    runner.cancel()
+    # wait for the first 10 tasks to be canceled
+    await runner.join()
+    # wait for the 11th task to complete
+    await runner.join()
+    assert len(results) == 1
+    assert results[0] == 100
+
+
+@pytest.mark.asyncio
 async def test_concurrent_runner_fails():
     results = []
 
