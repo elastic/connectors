9a606a269b82f698d8514a8dcc0f849d0913bfb6
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-11-03 13:15:20 +0100

    Update asserts in test_get_docs_incrementally to make it easier to spot problems (#1867)

--- tests/sources/test_sharepoint_online.py
+++ tests/sources/test_sharepoint_online.py
@@ -2358,17 +2358,29 @@ class TestSharepointOnlineDataSource:
 
             operations[operation] += 1
 
-        assert len(docs) == sum(
-            [
-                len(self.site_collections),
-                sum([len(i) for i in self.drive_items_delta]),
-                len(self.site_drives),
-                len(self.site_pages),
-                len(self.site_lists),
-                len(self.site_list_items) - 1,  # one is too old
-                len(self.site_list_item_attachments),
-            ]
+        assert len(self.site_collections) == len(
+            [doc for doc in docs if doc["object_type"] == "site_collection"]
         )
+        assert len(self.site_drives) == len(
+            [doc for doc in docs if doc["object_type"] == "site_drive"]
+        )
+        assert len([doc for doc in docs if doc["object_type"] == "drive_item"]) == sum(
+            len(i) for i in self.drive_items_delta
+        )
+        assert len([doc for doc in docs if doc["object_type"] == "site_page"]) == len(
+            self.site_pages
+        )
+        assert len([doc for doc in docs if doc["object_type"] == "site_list"]) == len(
+            self.site_lists
+        )
+        # -2 because one item is too old (2 months ago), one item is Web Template Extensions and is always ignored
+        assert (
+            len([doc for doc in docs if doc["object_type"] == "list_item"])
+            == len(self.site_list_items) - 2
+        )
+        assert len(
+            [doc for doc in docs if doc["object_type"] == "list_item_attachment"]
+        ) == len(self.site_list_item_attachments)
         assert sync_cursor["cursor_timestamp"] == self.today  # cursor was updated
 
         assert (operations["delete"]) == deleted
