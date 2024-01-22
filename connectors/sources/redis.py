b4579ed11545bae054080893f6d1350ab6b2cf41
akanshi-elastic <125074214+akanshi-elastic@users.noreply.github.com>
2024-01-22 20:04:46 +0530

    Code of Redis connector (#2044)

---
+++ connectors/sources/redis.py
@@ -0,0 +1,275 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+#
+import json
+from enum import Enum
+from functools import cached_property
+
+import redis.asyncio as redis
+
+from connectors.logger import logger
+from connectors.source import BaseDataSource, ConfigurableFieldValueError
+from connectors.utils import hash_id, iso_utc
+
+PAGE_SIZE = 1000
+
+
+class KeyType(Enum):
+    HASH = "hash"
+    STREAM = "stream"
+    LIST = "list"
+    SET = "set"
+    ZSET = "zset"
+    JSON = "ReJSON-RL"
+
+
+class RedisClient:
+    """Redis client to handle method calls made to Redis"""
+
+    def __init__(self, configuration):
+        self.configuration = configuration
+        self._logger = logger
+        self.host = self.configuration["host"]
+        self.port = self.configuration["port"]
+        self.database = self.configuration["database"]
+        self.username = self.configuration["username"]
+        self.password = self.configuration["password"]
+        self._redis_client = None
+
+    def set_logger(self, logger_):
+        self._logger = logger_
+
+    @cached_property
+    def _client(self):
+        self._redis_client = redis.from_url(
+            f"redis://{self.username}:{self.password}@{self.host}:{self.port}",
+            decode_responses=True,
+        )
+        return self._redis_client
+
+    async def close(self):
+        if self._redis_client:
+            await self._client.aclose()  # pyright: ignore
+
+    async def validate_database(self, db):
+        try:
+            await self._client.execute_command("SELECT", db)
+            await self._client.ping()
+            return True
+        except Exception as exception:
+            self._logger.warning(f"Database {db} not found. Error: {exception}")
+            return False
+
+    async def get_databases(self):
+        """Returns number of databases from config_get response
+
+        Returns:
+            list: List of databases
+        """
+        if self.database != ["*"]:
+            for database in set(self.database):
+                yield database
+        else:
+            try:
+                databases = await self._client.config_get("databases")
+                for database in list(range(int(databases.get("databases", 1)))):
+                    yield database
+            except Exception as exception:
+                self._logger.warning(
+                    f"Something went wrong while fetching database list. Error: {exception}",
+                    exc_info=True,
+                )
+
+    async def get_paginated_key(self, db, pattern, _type=None):
+        """Make a paginated call for fetching database keys.
+
+        Args:
+            db: Index of database
+            pattern: keyname or pattern
+            _type: Datatype for filter data
+
+        Yields:
+            string: key in database
+        """
+        await self._client.execute_command("SELECT", db)
+        async for key in self._client.scan_iter(
+            match=pattern, count=PAGE_SIZE, _type=_type
+        ):
+            yield key
+
+    async def get_key_value(self, key, key_type):
+        """Fetch value of key for database.
+
+        Args:
+            key: Name of key
+            key_type: Type of key
+
+        Yields:
+            Value of key
+        """
+        try:
+            if key_type == KeyType.HASH.value:
+                return await self._client.hgetall(key)
+            elif key_type == KeyType.STREAM.value:
+                return await self._client.xread({key: "0"})
+            elif key_type == KeyType.LIST.value:
+                return await self._client.lrange(key, 0, -1)
+            elif key_type == KeyType.SET.value:
+                return await self._client.smembers(key)
+            elif key_type == KeyType.ZSET.value:
+                return await self._client.zrange(key, 0, -1, withscores=True)
+            elif key_type == KeyType.JSON.value:
+                value = await self._client.execute_command("JSON.GET", key)
+                return json.loads(value)
+            else:
+                value = await self._client.get(key)
+                if value is not None:
+                    try:
+                        value_json = json.loads(value)
+                        return value_json
+                    except json.JSONDecodeError:
+                        return value
+        except UnicodeDecodeError as exception:
+            self._logger.warning(
+                f"Something went wrong while fetching value for key {key}. Error: {exception}",
+                exc_info=True,
+            )
+            return ""
+
+    async def get_key_metadata(self, key):
+        key_type = await self._client.type(key)
+        key_value = await self.get_key_value(key=key, key_type=key_type)
+        key_size = await self._client.memory_usage(key)
+        return key_type, key_value, key_size
+
+    async def ping(self):
+        await self._client.ping()
+
+
+class RedisDataSource(BaseDataSource):
+    """Redis"""
+
+    name = "Redis"
+    service_type = "redis"
+
+    def __init__(self, configuration):
+        super().__init__(configuration=configuration)
+        self.client = RedisClient(configuration=configuration)
+
+    @classmethod
+    def get_default_configuration(cls):
+        return {
+            "host": {"label": "Host", "order": 1, "type": "str"},
+            "port": {"label": "Port", "order": 2, "type": "int"},
+            "username": {
+                "label": "Username",
+                "order": 3,
+                "required": False,
+                "type": "str",
+            },
+            "password": {
+                "label": "Password",
+                "order": 4,
+                "required": False,
+                "sensitive": True,
+                "type": "str",
+            },
+            "database": {
+                "display": "textarea",
+                "label": "Comma-separated list of databases",
+                "order": 5,
+                "tooltip": "Databases are ignored when Advanced Sync Rules are used.",
+                "type": "list",
+            },
+        }
+
+    def _set_internal_logger(self):
+        self.client.set_logger(self._logger)
+
+    async def close(self):
+        await self.client.close()
+
+    async def _remote_validation(self):
+        """Validate configured databases
+        Raises:
+            ConfigurableFieldValueError: Unavailable services error.
+        """
+        invalid_type = []
+        invalid_db = []
+        msg = ""
+        if self.client.database != ["*"]:
+            for db in self.client.database:
+                if not db.isdigit() or int(db) < 0:
+                    invalid_type.append(db)
+                    continue
+                check_db = await self.client.validate_database(db=db)
+                if not check_db:
+                    invalid_db.append(db)
+            if invalid_type and invalid_db:
+                msg = f"Database {', '.join(invalid_db)} are not available. Also database element should be integer. Please correct database name: {', '.join(invalid_type)}"
+            elif invalid_db:
+                msg = f"Database {', '.join(invalid_db)} are not available."
+            elif invalid_type:
+                msg = f"All database element should be integer. Please correct database name: {', '.join(invalid_type)}"
+            if msg:
+                raise ConfigurableFieldValueError(msg)
+
+    async def validate_config(self):
+        """Validates whether user input is empty or not for configuration fields
+        Also validate, if user configured databases are available in Redis."""
+        await super().validate_config()
+        await self._remote_validation()
+
+    async def format_document(self, **kwargs):
+        """Prepare document for database records.
+
+        Returns:
+            document: Modified document.
+        """
+        doc_id = hash_id(f"{kwargs.get('db')}/{kwargs.get('key')}")
+        document = {
+            "_id": doc_id,
+            "key": kwargs.get("key"),
+            "value": str(kwargs.get("value")),
+            "size_in_bytes": kwargs.get("size"),
+            "database": kwargs.get("db"),
+            "key_type": kwargs.get("key_type"),
+            "_timestamp": iso_utc(),
+        }
+        return document
+
+    async def ping(self):
+        try:
+            await self.client.ping()
+            self._logger.info("Successfully connected to Redis.")
+        except Exception:
+            self._logger.exception("Error while connecting to Redis.")
+            raise
+
+    async def get_db_records(self, db, pattern="*", _type=None):
+        async for key in self.client.get_paginated_key(
+            db=db, pattern=pattern, _type=_type
+        ):
+            key_type, key_value, key_size = await self.client.get_key_metadata(key=key)
+            yield await self.format_document(
+                key=key,
+                value=key_value,
+                key_type=key_type,
+                size=key_size,
+                db=db,
+            )
+
+    async def get_docs(self, filtering=None):
+        """Get documents from Redis
+
+        Returns:
+            dictionary: Document of database content
+
+        Yields:
+            dictionary: Document from Redis.
+        """
+        async for db in self.client.get_databases():
+            async for document in self.get_db_records(db=db):
+                yield document, None
---
+++ tests/sources/test_redis.py
@@ -0,0 +1,215 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+#
+import json
+from contextlib import asynccontextmanager
+from unittest import mock
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+import redis
+from freezegun import freeze_time
+
+from connectors.source import ConfigurableFieldValueError
+from connectors.sources.redis import (
+    RedisDataSource,
+)
+from tests.commons import AsyncIterator
+from tests.sources.support import create_source
+
+DOCUMENT = [
+    {
+        "_id": "aa00c4c0f44c5cb7cad68df40e8f8877",
+        "key": "0",
+        "value": "this is value",
+        "size_in_bytes": 10,
+        "database": 0,
+        "key_type": "string",
+        "_timestamp": "2023-01-24T04:07:19+00:00",
+    }
+]
+
+
+class RedisClientMock:
+    async def execute_command(self, SELECT="JSON.GET", key="json_key"):
+        return json.dumps({"1": "1", "2": "2"})
+
+    async def zrange(self, key, start, skip, withscores=True):
+        return {1, 2, 3}
+
+    async def smembers(self, key):
+        return {1, 2, 3}
+
+    async def get(self, key):
+        return "this is value"
+
+    async def hgetall(self, key):
+        return "hash"
+
+    async def xread(self, key):
+        return "stream"
+
+    async def lrange(self, key, start, skip):
+        return [1, 2, 3]
+
+    async def config_get(self, databases):
+        return {"databases": "1"}
+
+    async def ping(self):
+        return False
+
+    async def aclose(self):
+        return True
+
+    async def type(self, key):  # NOQA
+        return "string"
+
+    async def memory_usage(self, key):
+        return 10
+
+    async def validate_database(self, db=0):
+        await self.execute_command()
+        await self.ping()
+
+
+@asynccontextmanager
+async def create_redis_source():
+    async with create_source(
+        RedisDataSource,
+        host="localhost",
+        port=6379,
+        database="0",
+        username="username",
+        password="password",
+    ) as source:
+        yield source
+
+
+@pytest.mark.asyncio
+async def test_ping_positive():
+    async with create_redis_source() as source:
+        source.client.ping = AsyncMock()
+        await source.ping()
+
+
+@pytest.mark.asyncio
+async def test_ping_negative():
+    async with create_redis_source() as source:
+        mocked_client = Mock()
+        with mock.patch("redis.from_url", return_value=mocked_client):
+            mocked_client.ping = AsyncMock(
+                side_effect=redis.exceptions.AuthenticationError
+            )
+            with pytest.raises(Exception):
+                await source.ping()
+
+
+@pytest.mark.asyncio
+async def test_validate_config_when_database_type():
+    async with create_redis_source() as source:
+        source.client.database = ["1", "db123", "123"]
+        mocked_client = Mock()
+        mocked_client.client.validate_database = AsyncMock(return_value=True)
+        with mock.patch("redis.from_url", return_value=mocked_client):
+            with pytest.raises(ConfigurableFieldValueError):
+                await source.validate_config()
+
+
+@pytest.mark.asyncio
+async def test_validate_config_when_database_is_invalid():
+    async with create_redis_source() as source:
+        source.client.database = ["123"]
+        mocked_client = Mock()
+        mocked_client.client.validate_database = AsyncMock(return_value=True)
+        with mock.patch("redis.from_url", return_value=mocked_client):
+            with pytest.raises(ConfigurableFieldValueError):
+                await source.validate_config()
+
+
+@pytest.mark.asyncio
+@freeze_time("2023-01-24T04:07:19+00:00")
+async def test_get_docs():
+    async with create_redis_source() as source:
+        source.client.database = [1]
+
+        with mock.patch(
+            "redis.from_url",
+            return_value=AsyncMock(),
+        ):
+            source.get_db_records = AsyncIterator(items=DOCUMENT)
+            async for (doc, _) in source.get_docs():
+                assert doc in DOCUMENT
+
+
+@pytest.mark.asyncio
+async def test_get_databases_for_multiple_db():
+    async with create_redis_source() as source:
+        source.client.database = [1, 2]
+        async for database in source.client.get_databases():
+            assert database in [1, 2]
+
+
+@pytest.mark.asyncio
+async def test_get_databases_with_astric():
+    async with create_redis_source() as source:
+        source.client.database = ["*"]
+        source.client._client = RedisClientMock()
+        async for database in source.client.get_databases():
+            assert database == 0
+
+
+@pytest.mark.asyncio
+async def test_get_databases_negative():
+    async with create_redis_source() as source:
+        source.client.database = ["*"]
+        mocked_client = Mock()
+        with mock.patch("redis.from_url", return_value=mocked_client):
+            mocked_client.ping = AsyncMock(
+                side_effect=redis.exceptions.AuthenticationError
+            )
+            async for database in source.client.get_databases():
+                assert database == []
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "key, key_type, expected_response",
+    [
+        ("json_key", "ReJSON-RL", {"1": "1", "2": "2"}),
+        ("string_key", "string", "this is value"),
+        ("list_key", "list", [1, 2, 3]),
+        ("set_key", "set", {1, 2, 3}),
+        ("sorted_set_key", "zset", {1, 2, 3}),
+        ("hash_key", "hash", "hash"),
+        ("stream_key", "stream", "stream"),
+    ],
+)
+async def test_get_key_value(key, key_type, expected_response):
+    async with create_redis_source() as source:
+        source.client.database = ["*"]
+        source.client._client = RedisClientMock()
+        value = await source.client.get_key_value(key=key, key_type=key_type)
+        assert value == expected_response
+
+
+@pytest.mark.asyncio
+@freeze_time("2023-01-24T04:07:19+00:00")
+async def test_get_key_metadata():
+    async with create_redis_source() as source:
+        source.client._client = RedisClientMock()
+        key_type, value, size = await source.client.get_key_metadata(key="0")
+        assert key_type == "string"
+        assert value == "this is value"
+        assert size == 10
+
+
+@pytest.mark.asyncio
+@freeze_time("2023-01-24T04:07:19+00:00")
+async def test_get_db_records():
+    async with create_redis_source() as source:
+        source.client._client = RedisClientMock()
+        source.client.get_paginated_key = AsyncIterator(["0"])
+        async for record in source.get_db_records(db=0):
+            assert record == DOCUMENT[0]
