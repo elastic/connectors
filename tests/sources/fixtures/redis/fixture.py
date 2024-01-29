bc0eb10b9dc0a87e1bab7de58a33de5f1d936f82
akanshi-elastic <akanshi.kumari@elastic.co>
2024-01-29 14:16:36 +0530

    update ftest with suggested changes

--- tests/sources/fixtures/redis/fixture.py
+++ tests/sources/fixtures/redis/fixture.py
@@ -12,36 +12,41 @@ import redis.asyncio as redis
 from tests.commons import WeightedFakeProvider
 
 DATA_SIZE = os.environ.get("DATA_SIZE", "small").lower()
-_NUM_TABLES = {"small": 2, "medium": 4, "large": 6}
-NUM_TABLES = _NUM_TABLES[DATA_SIZE]
+_NUM_DB = {"small": 2, "medium": 4, "large": 16}
+NUM_DB = _NUM_DB[DATA_SIZE]
+RECORDS_TO_DELETE = 10
+EACH_ROW_ITEMS = 500
 ENDPOINT = "redis://localhost:6379/"
 
 fake_provider = WeightedFakeProvider(weights=[0.65, 0.3, 0.05, 0])
 
 
-async def inject_lines(redis_client, table, start, lines):
+async def inject_lines(redis_client, start, lines):
+    text = fake_provider.get_text()
     rows = {}
     for row_id in range(lines):
-        row_id += start
-        key = f"user_{row_id}"
-        rows[key] = fake_provider.get_text()
+        key = f"user_{row_id + start}"
+        rows[key] = text
     await redis_client.mset(rows)
 
 
 async def load():
-    """N tables of 10001 rows each. each row is ~ 1024*20 bytes"""
-    for table in range(NUM_TABLES):
-        print(f"Adding data in {table}...")
-        redis_client = await redis.from_url(f"{ENDPOINT}{table}")
-        for i in range(7):
-            await inject_lines(redis_client, table, i * 1000, 100)
+    """N databases of 500 rows each. each row is ~ 1024*20 bytes"""
+    redis_client = await redis.from_url(f"{ENDPOINT}")
+    for db in range(NUM_DB):
+        print(f"Adding data in {db}...")
+        await redis_client.execute_command("SELECT", db)
+        await inject_lines(redis_client, db, EACH_ROW_ITEMS)
 
 
 async def remove():
-    """Removes 10 random items per table"""
-    for table in range(NUM_TABLES):
-        print(f"Working on table {table}...")
-        redis_client = await redis.from_url(f"{ENDPOINT}{table}")
-        keys = [f"user_{row_id}" for row_id in random.sample(range(1, 100), 10)]
-        print(keys)
+    """Removes 10 random items per db"""
+    redis_client = await redis.from_url(f"{ENDPOINT}")
+    for db in range(NUM_DB):
+        print(f"Working on db {db}...")
+        await redis_client.execute_command("SELECT", db)
+        keys = [
+            f"user_{row_id}"
+            for row_id in random.sample(range(1, 100), RECORDS_TO_DELETE)
+        ]
         await redis_client.delete(*keys)
