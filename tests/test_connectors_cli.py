c9fb4766604b3a7210507e14a623de84b0689dbb
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2024-03-05 13:40:01 +0100

    Fix problem with cancel jobs action using closed client (#2228)

--- tests/test_connectors_cli.py
+++ tests/test_connectors_cli.py
@@ -651,9 +651,7 @@ def test_job_cancel():
 
     job = SyncJobObject(job_index, doc)
 
-    with patch(
-        "connectors.cli.job.Job._Job__async_list_jobs", AsyncMock(return_value=[job])
-    ):
+    with patch("connectors.protocol.SyncJobIndex.get_all_docs", AsyncIterator([job])):
         with patch.object(job, "_terminate") as mocked_method:
             result = runner.invoke(cli, ["job", "cancel", job_id])
 
@@ -667,7 +665,7 @@ def test_job_cancel_error():
     runner = CliRunner()
     job_id = "test_job_id"
     with patch(
-        "connectors.cli.job.Job._Job__async_list_jobs",
+        "connectors.protocol.SyncJobIndex.get_all_docs",
         side_effect=ApiError(500, meta="meta", body="error"),
     ):
         result = runner.invoke(cli, ["job", "cancel", job_id])
