fe5bad56187eac64eb24a6114b936367a5dc8844
Tim Grein <tim.grein@elastic.co>
2024-03-22 15:31:58 +0100

    [Sink] Fix typos (#2283)

--- connectors/es/sink.py
+++ connectors/es/sink.py
@@ -681,7 +681,7 @@ class SyncOrchestrator:
         return await self.es_management_client.has_active_license_enabled(license_)
 
     async def prepare_content_index(self, index_name, language_code=None):
-        """Creates the index, given a mapping/settings if it does not exists."""
+        """Creates the index, given a mapping/settings if it does not exist."""
         self._logger.debug(f"Checking index {index_name}")
 
         result = await self.es_management_client.get_index(
@@ -696,7 +696,7 @@ class SyncOrchestrator:
             # Update the index mappings if needed
             self._logger.debug(f"{index_name} exists")
 
-            # Settings contain analizers which are being used in the index mappings
+            # Settings contain analyzers which are being used in the index mappings
             # Therefore settings must be applied before mappings
             await self.es_management_client.ensure_content_index_settings(
                 index_name=index_name, index=index, language_code=language_code
