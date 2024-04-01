970e5656a737af5b1cbf96b3182287d73502b974
Praveen Kukreja <praveen.kukreja@elastic.co>
2024-04-01 23:55:49 +0530

    address comments on tests

--- tests/sources/test_notion.py
+++ tests/sources/test_notion.py
@@ -799,20 +799,30 @@ async def test_async_iterate_paginated_api():
 @pytest.mark.asyncio
 async def test_fetch_results_rate_limit_exceeded():
     async def mock_function_with_429(**kwargs):
-        raise APIResponseError(
-            code="rate_limited",
-            message="Rate limit exceeded.",
-            response=Response(status_code=429, text="Rate limit exceeded."),
-        )
+        mock_function_with_429.call_count += 1
+
+        # Initial call and first retry would raise rate-limited error
+        if mock_function_with_429.call_count <= 2:
+            raise APIResponseError(
+                code="rate_limited",
+                message="Rate limit exceeded.",
+                response=Response(status_code=429, text="Rate limit exceeded."),
+            )
+        else:
+            return {"success": True}
 
     async with create_source(
-        NotionDataSource, notion_secret_key="secert_key"
+        NotionDataSource, notion_secret_key="secret_key"
     ) as source:
         with patch("connectors.sources.notion.DEFAULT_RETRY_SECONDS", 0.3):
-            with pytest.raises(Exception) as exc_info:
-                await source.notion_client.fetch_results(mock_function_with_429)
+            mock_function_with_429.call_count = 0
+            result = await source.notion_client.fetch_results(mock_function_with_429)
 
-            assert "Rate limit exceeded." in str(exc_info.value)
+            # assert it responds successfully on 2nd retry and does not raise error
+            assert result == {"success": True}
+
+            # assert it did not go for 3rd retry
+            assert mock_function_with_429.call_count == 3  # initial call + 2 retries
 
 
 @patch("connectors.utils.time_to_sleep_between_retries", Mock(return_value=0))
@@ -826,18 +836,31 @@ async def test_fetch_results_other_errors_not_retried():
         )
 
     async with create_source(
-        NotionDataSource, notion_secret_key="secert_key"
+        NotionDataSource, notion_secret_key="secret_key"
     ) as source:
-        with pytest.raises(APIResponseError) as exc_info:
-            await source.notion_client.fetch_results(mock_function_with_other_error)
+        with patch.object(
+            source.notion_client,
+            "fetch_results",
+            wraps=source.notion_client.fetch_results,
+        ) as mock_fetch_results:
+            with pytest.raises(APIResponseError) as exc_info:
+                await source.notion_client.fetch_results(mock_function_with_other_error)
 
-        assert exc_info.value.code == "object_not_found"
-        assert exc_info.value.status == 404
+            assert exc_info.value.code == "object_not_found"
+            assert exc_info.value.status == 404
+
+            # assert that fetch_results is called exactly once and not retried for status code 404
+            mock_fetch_results.assert_called_once()
 
 
 @pytest.mark.asyncio
 async def test_original_async_iterate_paginated_api_not_called():
-    with patch(
-        "notion_client.helpers.async_iterate_paginated_api"
-    ) as mock_async_iterate_paginated_api:
-        assert not mock_async_iterate_paginated_api.called
+    with patch.object(NotionClient, "async_iterate_paginated_api"):
+        with patch(
+            "notion_client.helpers.async_iterate_paginated_api"
+        ) as mock_async_iterate_paginated_api:
+            async with create_source(
+                NotionDataSource, notion_secret_key="secret_key"
+            ) as source:
+                async for _ in source.notion_client.query_database("database_id"):
+                    assert not mock_async_iterate_paginated_api.called
