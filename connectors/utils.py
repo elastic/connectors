07a39bcdf82e689ef3864bb63ea1da572c99fd1f
Tim Grein <tim.grein@elastic.co>
2024-04-08 18:11:23 +0200

    [Framework] Use Cancellable sleeps in retryable (#2359)

--- connectors/utils.py
+++ connectors/utils.py
@@ -153,7 +153,12 @@ class CancellableSleeps:
 
         await _sleep(delay, result=result, loop=loop)
 
-    def cancel(self):
+    def cancel(self, sig=None):
+        if sig:
+            logger.debug(f"Caught {sig}. Cancelling sleeps...")
+        else:
+            logger.debug("Cancelling sleeps...")
+
         for task in self._sleeps:
             task.cancel()
 
@@ -470,6 +475,9 @@ class UnknownRetryStrategyError(Exception):
     pass
 
 
+sleeps_for_retryable = CancellableSleeps()
+
+
 def retryable(
     retries=3,
     interval=1.0,
@@ -516,7 +524,7 @@ def retryable_async_function(func, retries, interval, strategy, skipped_exceptio
                 logger.debug(
                     f"Retrying ({retry} of {retries}) with interval: {interval} and strategy: {strategy.name}"
                 )
-                await asyncio.sleep(
+                await sleeps_for_retryable.sleep(
                     time_to_sleep_between_retries(strategy, interval, retry)
                 )
                 retry += 1
@@ -540,7 +548,7 @@ def retryable_async_generator(func, retries, interval, strategy, skipped_excepti
                 logger.debug(
                     f"Retrying ({retry} of {retries}) with interval: {interval} and strategy: {strategy.name}"
                 )
-                await asyncio.sleep(
+                await sleeps_for_retryable.sleep(
                     time_to_sleep_between_retries(strategy, interval, retry)
                 )
                 retry += 1
