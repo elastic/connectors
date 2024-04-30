721afe22c0cf8b53f83649f9eba74ede41e5ed3f
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2024-04-30 13:07:16 +0200

    Fix concurrent scheduling (#2470)

--- connectors/services/job_scheduling.py
+++ connectors/services/job_scheduling.py
@@ -198,9 +198,11 @@ class JobSchedulingService(BaseService):
                 job_type
             )
 
+            self.logger.debug(f"Last sync was scheduled at {last_sync_scheduled_at}")
+
             if (
                 last_sync_scheduled_at is not None
-                and last_sync_scheduled_at > this_wake_up_time
+                and last_sync_scheduled_at > last_wake_up_time
             ):
                 connector.log_debug(
                     f"A scheduled '{job_type_value}' sync is created by another connector instance, skipping..."
