0587ce6394f3bb4f3332f5f547cf8cb3264e38b0
akanshi-elastic <akanshi.kumari@elastic.co>
2024-05-06 17:29:39 +0530

    fix ftest

--- tests/sources/fixtures/sharepoint_server/fixture.py
+++ tests/sources/fixtures/sharepoint_server/fixture.py
@@ -199,6 +199,8 @@ def get_list_and_items(parent_site_url, list_id):
                     "Modified": "2023-01-30T10:02:40Z",
                     "AuthorId": 12345,
                     "EditorId": 12345,
+                    "Editor": {"Title": "system"},
+                    "Author": {"Title": "system"},
                     "Title": f"list-item-{list_id}",
                 }
             ]
@@ -223,6 +225,8 @@ def get_list_and_items(parent_site_url, list_id):
                     "Modified": "2023-01-30T10:02:40Z",
                     "AuthorId": 12345,
                     "EditorId": 12345,
+                    "Editor": {"Title": "system"},
+                    "Author": {"Title": "system"},
                     "Title": f"list-item-{list_id}",
                     "Id": adjust_document_id_size(
                         f"{parent_site_url}-list-id1-{list_id}"
@@ -239,6 +243,8 @@ def get_list_and_items(parent_site_url, list_id):
                     "Modified": "2023-01-30T10:02:40Z",
                     "AuthorId": 12345,
                     "EditorId": 12345,
+                    "Editor": {"Title": "system"},
+                    "Author": {"Title": "system"},
                     "Title": f"list-item-{list_id}",
                     "Id": adjust_document_id_size(
                         f"{parent_site_url}-list-id2-{list_id}"
