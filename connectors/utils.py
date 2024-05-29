9a56ef0fe439345fc91528e5b17d9dcc7ea77d72
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2024-05-29 17:05:46 +0200

    Switch to tzcron for schedule parsing + add tests for supported cron expressions in Kibana (#2594)
    (cherry picked from commit b8b66aa31dec2d48396c7e79e604e9290695c82f)
    
    # Conflicts:
    #	connectors/utils.py
    #	tests/test_utils.py

--- connectors/utils.py
+++ connectors/utils.py
@@ -21,9 +21,11 @@ from datetime import datetime, timedelta, timezone
 from enum import Enum
 from time import strftime
 
+import dateutil.parser as parser
+import pytz
+import tzcron
 from base64io import Base64IO
 from bs4 import BeautifulSoup
-from cstriggers.core.trigger import QuartzCron
 from pympler import asizeof
 
 from connectors.logger import logger
@@ -99,9 +101,33 @@ def epoch_timestamp_zulu():
 
 
 def next_run(quartz_definition, now):
-    """Returns the datetime of the next run."""
-    cron_obj = QuartzCron(quartz_definition, now)
-    return cron_obj.next_trigger()
+    """Returns the datetime in UTC timezone of the next run."""
+    # Year is optional and is never present.
+    seconds, minutes, hours, day_of_month, month, day_of_week, year = (
+        quartz_definition.split(" ") + [None]
+    )[:7]
+
+    # Day of week is 1-7 starting from Sunday in Quartz and from Monday in regular Cron, adjust
+    # Days before: 1 - SUN, 2 - MON ... 7 - SAT
+    # Days after: 1 - MON, 2 - TUE ... 7 - SUN
+    if day_of_week.isnumeric():
+        day_of_week = (int(day_of_week) - 2) % 7 + 1
+
+    if not year:
+        year = "*"
+
+    # tzcron always expects year
+    repackaged_definition = (
+        f"{minutes} {hours} {day_of_month} {month} {day_of_week} {year}"
+    )
+
+    # ? comes from Quartz Cron, regular cron doesn't handle it well
+    repackaged_definition = repackaged_definition.replace("?", "*")
+
+    schedule = tzcron.Schedule(repackaged_definition, pytz.utc, now)
+
+    next_occurrence = next(schedule)
+    return next_occurrence
 
 
 INVALID_CHARS = "\\", "/", "*", "?", '"', "<", ">", "|", " ", ",", "#"
