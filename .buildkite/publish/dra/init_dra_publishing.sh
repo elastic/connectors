f300b8e6fbb6e3471e72826b714091638c838e93
Sean Story <sean.j.story@gmail.com>
2024-08-14 16:04:19 -0500

    Make some progress

--- .buildkite/publish/dra/init_dra_publishing.sh
+++ .buildkite/publish/dra/init_dra_publishing.sh
@@ -11,7 +11,21 @@ set -o pipefail
 export DRA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CURDIR="$(dirname "$DRA_DIR")"
 source $CURDIR/publish-common.sh
+RELEASE_DIR="${PROJECT_ROOT}"
 
 # Mock out creating an artifact
-echo "Hello, world"
-echo "The version is: ${VERSION}"
+rm -rf $RELEASE_DIR/dist
+mkdir -p $RELEASE_DIR/dist
+cd $RELEASE_DIR/dist
+zip test.zip -r . -i *
+cd -
+
+echo "The artifacts are: $(ls $RELEASE_DIR/dist)"
+
+
+# ensure JQ is installed...
+if ! which jq > /dev/null; then
+  wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x jq
+fi
+
+# TODO keep copying from ent-search init_dra_publishing.sh
