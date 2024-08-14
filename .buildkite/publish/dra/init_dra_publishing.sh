d6b5f91c57d561ac4963e05148b16be6c67ac9b7
Sean Story <sean.j.story@gmail.com>
2024-08-14 15:57:49 -0500

    fix relative pathing

--- .buildkite/publish/dra/init_dra_publishing.sh
+++ .buildkite/publish/dra/init_dra_publishing.sh
@@ -10,7 +10,7 @@ set -o pipefail
 # Load our common environment variables for publishing
 export DRA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CURDIR="$(dirname "$DRA_DIR")"
-source $CURDIR/../publish-common.sh
+source $CURDIR/publish-common.sh
 
 # Mock out creating an artifact
 echo "Hello, world"
