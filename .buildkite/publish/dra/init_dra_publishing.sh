ae4df170162d505341100b955fd2b61c3cf74caa
Sean Story <sean.j.story@gmail.com>
2024-08-15 14:34:16 -0500

    Finish copying important-looking bits from ent-search

--- .buildkite/publish/dra/init_dra_publishing.sh
+++ .buildkite/publish/dra/init_dra_publishing.sh
@@ -28,4 +28,105 @@ if ! which jq > /dev/null; then
   wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x jq
 fi
 
-# TODO keep copying from ent-search init_dra_publishing.sh
+# Need to export the revision, name and version and others
+# for the dependency report and publishing scripts
+export REVISION="${BUILDKITE_COMMIT}"
+export BRANCH_NAME="${BUILDKITE_BRANCH}"
+export PRODUCT_NAME="enterprise-search"
+export GIT_REPO="ent-search"
+
+# set PUBLISH_SNAPSHOT and PUBLISH_STAGING based on the branch
+if [[ "${BUILDKITE_BRANCH:-}" =~ (main|[0-9]\.[0-9x]*$) ]]; then
+  export PUBLISH_SNAPSHOT="true"
+fi
+if [[ "${BUILDKITE_BRANCH:-}" =~ ([0-9]\.[0-9x]*$) ]]; then
+  export PUBLISH_STAGING="true"
+fi
+
+# Sanity check in the logs to list the downloaded artifacts
+chmod -R a+rw "${RELEASE_DIR}/dist"
+echo "---- listing artifact directory: ${RELEASE_DIR}/dist:"
+ls -al $RELEASE_DIR/dist
+
+# function to set our vault creds for DRA publishing
+function setDraVaultCredentials() {
+  set +x
+  echo "-- setting DRA vault credentials --"
+
+  # need to keep original vault addr to reset it
+  # after we're done or else it won't be set
+  # if we call this fxn again
+  export ORIGINAL_VAULT_ADDR="${VAULT_ADDR:-}"
+
+  DRA_CREDS=$(vault kv get -field=data -format=json kv/ci-shared/release/dra-role)
+  if [[ "${DRA_CREDS:-}" == "" ]]; then
+    echo "Could not get DRA credentials from vault"
+    exit 2
+  fi
+
+  export VAULT_ADDR="$(echo $DRA_CREDS | jq -r '.vault_addr')"
+  if [[ "${VAULT_ADDR:-}" == "" ]]; then
+    echo "Could not get VAULT_ADDR from DRA credentials"
+    exit 2
+  fi
+
+  export VAULT_ROLE_ID="$(echo $DRA_CREDS | jq -r '.role_id')"
+  if [[ "${VAULT_ROLE_ID:-}" == "" ]]; then
+    echo "Could not get VAULT_ADDR from DRA credentials"
+    exit 2
+  fi
+
+  export VAULT_SECRET_ID="$(echo $DRA_CREDS | jq -r '.secret_id')"
+  if [[ "${VAULT_SECRET_ID:-}" == "" ]]; then
+    echo "Could not get VAULT_ADDR from DRA credentials"
+    exit 2
+  fi
+
+  unset DRA_CREDS
+  set -x
+}
+
+# function to unset the vault credentials.
+function unsetDraVaultCredentials() {
+  set +x
+  unset VAULT_ROLE_ID
+  unset VAULT_SECRET_ID
+
+  # and reset our original vault address
+  export VAULT_ADDR="${ORIGINAL_VAULT_ADDR:-}"
+
+  set -x
+}
+
+# publish SNAPSHOT artifacts
+if [[ "${PUBLISH_SNAPSHOT:-}" == "true" ]]; then
+
+  echo "-------- Publishing SNAPSHOT DRA Artifacts"
+  setDraVaultCredentials
+  export WORKFLOW="snapshot"
+
+  # TODO - when ready, remove the gate and ungate test vars
+  if [[ "${DRA_PUBLISH_GATE:-}" == "" || "${DRA_UNGATE_TEST:-}" == "true" ]]; then
+    source "${PROJECT_ROOT}/.buildkite/publish/dra/publish-daily-release-artifact.sh"
+  else
+    echo "DRA_PUBLISH_GATE is locked. This is where we would call 'source ${PROJECT_ROOT}/.buildkite/publish/dra/publish-daily-release-artifact.sh'"
+  fi
+  unsetDraVaultCredentials
+fi
+
+# publish STAGING artifacts
+if [[ "${PUBLISH_STAGING:-}" == "true" ]]; then
+
+  echo "-------- Publishing STAGING DRA Artifacts"
+  setDraVaultCredentials
+  export WORKFLOW="staging"
+
+  # TODO - when ready, remove the gate var
+  if [[ "${DRA_PUBLISH_GATE:-}" == "" ]]; then
+    source "${PROJECT_ROOT}/.buildkite/publish/dra/publish-daily-release-artifact.sh"
+  else
+    echo "DRA_PUBLISH_GATE is locked. This is where we would call 'source ${PROJECT_ROOT}/.buildkite/publish/dra/publish-daily-release-artifact.sh'"
+  fi
+  unsetDraVaultCredentials
+  rm -rf "${DEPENDENCIES_REPORTS_DIR}/*"
+fi
---
+++ .buildkite/publish/dra/publish-daily-release-artifact.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+set -exu
+set -o pipefail
+
+#---------------------------------------------------------------------------------------------------
+
+# Ensure all required VAULT variables are set.
+# and ensure we're not leaking credentials...
+# These are hidden in Jenkins, but not necessarily in Buildkite
+set +x
+if [[ "${VAULT_ADDR:-}" == "" ]]; then
+  echo "ERROR: VAULT_ADDR required!"
+  exit 2
+else
+  export "VAULT_ADDR=${VAULT_ADDR}"
+fi
+
+if [[ "${VAULT_ROLE_ID:-}" == "" ]]; then
+  echo "ERROR: VAULT_ROLE_ID required!"
+  exit 2
+else
+  export "VAULT_ROLE_ID=${VAULT_ROLE_ID}"
+fi
+
+if [[ "${VAULT_SECRET_ID:-}" == "" ]]; then
+  echo "ERROR: VAULT_SECRET_ID required!"
+  exit 2
+else
+  export "VAULT_SECRET_ID=${VAULT_SECRET_ID}"
+fi
+set -x
+
+# Release dir required. If this is not an absolute path the Docker command will error.
+if [[ "${RELEASE_DIR:-}" == "" ]]; then
+  echo "ERROR: RELEASE_DIR required!"
+  exit 2
+fi
+
+# Git Repo
+if [[ "${GIT_REPO:-}" == "" ]]; then
+  echo "ERROR: GIT_REPO required!"
+  exit 2
+fi
+
+# Revision Git commit hash
+if [[ "${REVISION:-}" == "" ]]; then
+  echo "ERROR: REVISION required!"
+  exit 2
+fi
+
+# Workflow. Must be either `staging` or `snapshot`
+if [[ "${WORKFLOW:-}" != "staging" && "${WORKFLOW:-}" != "snapshot" ]]; then
+  echo "ERROR: WORKFLOW required! Value must be staging or snapshot"
+  exit 2
+fi
+
+# Version. This is pulled from config/product_version.
+if [[ "${VERSION:-}" == "" ]]; then
+  echo "ERROR: VERSION required!"
+  exit 2
+fi
+
+# Branch Name. Required for the cli collect artifact publish
+if [[ "${BRANCH_NAME:-}" == "" ]]; then
+  echo "ERROR: BRANCH_NAME required!"
+  exit 2
+fi
+
+# ensure JQ is installed...
+if ! which jq > /dev/null; then
+  wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x jq
+fi
+
+DEPENDENCY_VERSION="$VERSION"
+ARTIFACT_BASE_URL="https://artifacts-staging.elastic.co"
+if [[ "${WORKFLOW:-}" == "snapshot" ]]; then
+  DEPENDENCY_VERSION="$VERSION-SNAPSHOT"
+  ARTIFACT_BASE_URL="https://artifacts-snapshot.elastic.co"
+fi
+
+#---------------------------------------------------------------------------------------------------
+echo "Made it to 'docker run'" # TODO, replace this with actual docker run
+#docker run --rm \
+#  --name release-manager \
+#  -e VAULT_ADDR \
+#  -e VAULT_ROLE_ID \
+#  -e VAULT_SECRET_ID \
+#  --mount type=bind,readonly=false,src="${RELEASE_DIR}",target=/artifacts \
+#  docker.elastic.co/infra/release-manager:latest \
+#  cli collect \
+#      --project "${GIT_REPO}" \
+#      --branch "${BRANCH_NAME}" \
+#      --commit "${REVISION}" \
+#      --workflow "${WORKFLOW}" \
+#      --version "${VERSION}" \
+#      --artifact-set main
