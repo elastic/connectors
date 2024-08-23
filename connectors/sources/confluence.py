9ce1bddd017d06df2f468b534bb6f77d3273c4dc
moxarth-elastic <moxarth.rathod@elastic.co>
2024-08-23 10:35:04 +0530

    increase queue size

--- connectors/sources/confluence.py
+++ connectors/sources/confluence.py
@@ -78,8 +78,8 @@ URLS = {
 PING_URL = "rest/api/space?limit=1"
 MAX_CONCURRENT_DOWNLOADS = 50  # Max concurrent download supported by confluence
 MAX_CONCURRENCY = 50
-QUEUE_SIZE = 2024
-QUEUE_MEM_SIZE = 25 * 1024 * 1024  # Size in Megabytes
+QUEUE_SIZE = 2048
+QUEUE_MEM_SIZE = 50 * 1024 * 1024  # Size in Megabytes
 SERVER_USER_BATCH = 1000
 DATACENTER_USER_BATCH = 200
 END_SIGNAL = "FINISHED_TASK"
