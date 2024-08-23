3d69296865c71fda085907e1a1b336de02e54bf6
Sean Story <sean.j.story@gmail.com>
2024-08-23 16:10:36 -0500

    Comment out release-manager (#2772)

--- .buildkite/publish/dra/publish-daily-release-artifact.sh
+++ .buildkite/publish/dra/publish-daily-release-artifact.sh
@@ -70,17 +70,18 @@ fi
 #---------------------------------------------------------------------------------------------------
 echo "Contents of 'src' for the release-manager command:"
 ls -lah ${RELEASE_DIR}/dist
-docker run --rm \
-  --name release-manager \
-  -e VAULT_ADDR \
-  -e VAULT_ROLE_ID \
-  -e VAULT_SECRET_ID \
-  --mount type=bind,readonly=false,src="${RELEASE_DIR}/dist",target="/artifacts" \
-  docker.elastic.co/infra/release-manager:latest \
-  cli collect \
-      --project "${GIT_REPO}" \
-      --branch "${BRANCH_NAME}" \
-      --commit "${REVISION}" \
-      --workflow "${WORKFLOW}" \
-      --version "${VERSION}" \
-      --artifact-set main
+# TODO: put this back once https://github.com/elastic/crawler/pull/103 is approved
+#docker run --rm \
+#  --name release-manager \
+#  -e VAULT_ADDR \
+#  -e VAULT_ROLE_ID \
+#  -e VAULT_SECRET_ID \
+#  --mount type=bind,readonly=false,src="${RELEASE_DIR}/dist",target="/artifacts" \
+#  docker.elastic.co/infra/release-manager:latest \
+#  cli collect \
+#      --project "${GIT_REPO}" \
+#      --branch "${BRANCH_NAME}" \
+#      --commit "${REVISION}" \
+#      --workflow "${WORKFLOW}" \
+#      --version "${VERSION}" \
+#      --artifact-set main
