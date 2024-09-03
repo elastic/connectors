428b8ad5d61b167f61430cf52fc590931e6dea87
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2024-09-03 16:30:46 +0200

    Add missing license headers

--- connectors/agent/protocol.py
+++ connectors/agent/protocol.py
@@ -1,3 +1,8 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+#
 from elastic_agent_client.generated import elastic_agent_client_pb2 as proto
 from elastic_agent_client.handler.action import BaseActionHandler
 from elastic_agent_client.handler.checkin import BaseCheckinHandler
