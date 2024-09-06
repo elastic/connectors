526098a400684c13ad659ea269c7f3847a99ccfd
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2024-09-06 16:55:40 +0200

    Delete README + add comment to test_cli (#2807)

--- tests/agent/test_cli.py
+++ tests/agent/test_cli.py
@@ -12,7 +12,7 @@ from connectors.agent.cli import main
 
 
 @patch("connectors.agent.cli.ConnectorsAgentComponent", return_value=AsyncMock())
-def test_main(patch_component):
+def test_main_responds_to_sigterm(patch_component):
     async def kill():
         await asyncio.sleep(0.2)
         os.kill(os.getpid(), signal.SIGTERM)
@@ -20,6 +20,9 @@ def test_main(patch_component):
     loop = asyncio.new_event_loop()
     loop.create_task(kill())
 
+    # No asserts here.
+    # main() will block forever unless it's killed with a signal
+    # This test succeeds if it exits, if it hangs it'll be killed by a timeout
     main()
 
     loop.close()
