6d8b0f05548c0401253a237a2e1662b4e1ced022
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-06 18:00:26 +0100

    Add retry

--- .buildkite/run_notice_check.sh
+++ .buildkite/run_notice_check.sh
@@ -9,6 +9,27 @@ pyenv global $PYTHON_VERSION
 echo "Python version:"
 pyenv global
 
+retry() {
+  local retries=$1; shift
+  local delay=$1; shift
+  local attempts=1
+
+  until "$@"; do
+    retry_exit_status=$?
+    echo "Exited with $retry_exit_status" >&2
+    if (( retries == "0" )); then
+      return $retry_exit_status
+    elif (( attempts == retries )); then
+      echo "Failed $attempts retries" >&2
+      return $retry_exit_status
+    else
+      echo "Retrying $((retries - attempts)) more times..." >&2
+      attempts=$((attempts + 1))
+      sleep "$delay"
+    fi
+  done
+}
+
 vault_get() {
   key_path=${1:-}
   field=${2:-}
