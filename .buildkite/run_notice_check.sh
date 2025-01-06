0d65bc5bbd2a6790f6b2422dd961546fafd4524f
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-06 17:07:36 +0100

    Add git user

--- .buildkite/run_notice_check.sh
+++ .buildkite/run_notice_check.sh
@@ -14,6 +14,10 @@ make notice
 if [ -z "$(git status --porcelain | grep NOTICE.txt)" ]; then
   exit 0
 else 
+  MACHINE_USERNAME="entsearchmachine"
+  git config --global user.name "$MACHINE_USERNAME"
+  git config --global user.email '90414788+entsearchmachine@users.noreply.github.com'
+
   git add NOTICE.txt
   git commit -m"Update NOTICE.txt"
   git push
