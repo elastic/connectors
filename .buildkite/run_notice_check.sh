d174ecaabeee80b7a3c9c556ecd96128faea4274
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-07 15:59:36 +0100

    Fix typo

--- .buildkite/run_notice_check.sh
+++ .buildkite/run_notice_check.sh
@@ -12,8 +12,7 @@ make notice
 if [ -z "$(git status --porcelain | grep NOTICE.txt)" ]; then
   exit 0
 else 
-
-  if us_pr && ! is_auto_commit_disabled; then
+  if is_pr && ! is_auto_commit_disabled; then
     source .buildkite/publish/git-setup.sh
     export GH_TOKEN="$VAULT_GITHUB_TOKEN"
 
