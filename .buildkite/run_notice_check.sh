2e22143e41de21f4e99b15a7426439443f819d90
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-07 14:26:55 +0100

    Make a CI step to verify and update NOTICE.txt file (#3072)
    Co-authored-by: entsearchmachine <90414788+entsearchmachine@users.noreply.github.com>

---
+++ .buildkite/run_notice_check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# !!! WARNING DO NOT add -x to avoid leaking vault passwords
+set -euo pipefail
+
+source .buildkite/shared.sh
+
+init_python
+
+make notice
+
+if [ -z "$(git status --porcelain | grep NOTICE.txt)" ]; then
+  exit 0
+else 
+  MACHINE_USERNAME="entsearchmachine"
+  git config --global user.name "$MACHINE_USERNAME"
+  git config --global user.email '90414788+entsearchmachine@users.noreply.github.com'
+  export GH_TOKEN="$VAULT_GITHUB_TOKEN"
+
+  if us_pr && ! is_auto_commit_disabled; then
+    gh pr checkout "${BUILDKITE_PULL_REQUEST}"
+    git add NOTICE.txt
+    git commit -m"Update NOTICE.txt"
+    git push
+    sleep 15
+  fi
+
+  exit 1
+fi
+
+exit 0
