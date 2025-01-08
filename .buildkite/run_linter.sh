4f86b936a606270464678e67e68753f4923f877a
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-08 10:23:15 +0100

    More echoes + exit status on bad make autoformat

--- .buildkite/run_linter.sh
+++ .buildkite/run_linter.sh
@@ -10,14 +10,18 @@ init_python
 
 if is_pr; then
   echo "We're on PR, running autoformat"
-  make autoformat
+  if ! make autoformat ; then
+    echo "make autoformat ran with errors, exiting"
+    exit 1
+  fi
 
   if [ -z "$(git status --porcelain)" ]; then
+    echo "Nothing to be fixed by autoformat"
     exit 0
   else
     # We lint and if it succeeds, we can push the change
-    set +e
     if make lint ; then
+      echo "linting errors are fixed, pushing the diff"
       export GH_TOKEN="$VAULT_GITHUB_TOKEN"
 
       git add .
@@ -25,6 +29,7 @@ if is_pr; then
       git push
     else
       # Needs manual work
+      echo "make autoformat did not fix all linting errors, exiting"
       exit 1
     fi
   fi
