be899aae7b27eb2df678dbe11a7fe2cd22c4a408
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-09 17:14:37 +0100

    Swap conditions + remove duplicate retry

--- .buildkite/shared.sh
+++ .buildkite/shared.sh
@@ -29,29 +29,8 @@ retry() {
 
 is_pr() {
   if [ "${BUILDKITE_PULL_REQUEST-}" ] || [ "${BUILDKITE_PULL_REQUEST}" = "false"]; then
-    return true
-  else
     return false
+  else
+    return true
   fi
-}
-
-retry() {
-  local retries=$1; shift
-  local delay=$1; shift
-  local attempts=1
-
-  until "$@"; do
-    retry_exit_status=$?
-    echo "Exited with $retry_exit_status" >&2
-    if (( retries == "0" )); then
-      return $retry_exit_status
-    elif (( attempts == retries )); then
-      echo "Failed $attempts retries" >&2
-      return $retry_exit_status
-    else
-      echo "Retrying $((retries - attempts)) more times..." >&2
-      attempts=$((attempts + 1))
-      sleep "$delay"
-    fi
-  done
 }
