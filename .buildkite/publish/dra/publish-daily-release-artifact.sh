d2ff0014f23a419b342c5c714e92ec3255e6989f
Sean Story <sean.j.story@gmail.com>
2025-01-13 10:18:30 -0600

    Revert "DRA release pipeline, pass qualifier to release cli (#3101)"
    This reverts commit c1ef211453701346a84113b90940ee9295547ed2.

--- .buildkite/publish/dra/publish-daily-release-artifact.sh
+++ .buildkite/publish/dra/publish-daily-release-artifact.sh
@@ -87,5 +87,4 @@ docker run --rm \
       --commit "${REVISION}" \
       --workflow "${WORKFLOW}" \
       --version "${VERSION}" \
-      --qualifier "${VERSION_QUALIFIER}" \
       --artifact-set main
