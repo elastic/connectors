73188a234b560ec62af2df902a9d28b0582b1301
mattnowzari <matt.nowzari@elastic.co>
2025-02-06 14:27:26 -0500

    Adding GA warning filter for ES client

--- connectors/es/__init__.py
+++ connectors/es/__init__.py
@@ -3,9 +3,15 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
+import warnings
+
 from connectors.es.client import ESClient  # NOQA
 from connectors.es.document import ESDocument, InvalidDocumentSourceError  # NOQA
 from connectors.es.index import ESIndex  # NOQA
+
+from elasticsearch.exceptions import GeneralAvailabilityWarning
+
+warnings.filterwarnings("ignore", category=GeneralAvailabilityWarning)
 
 TIMESTAMP_FIELD = "_timestamp"
 DEFAULT_LANGUAGE = "en"
