b927e8a3f563e73053297c3a35513f455373b259
Matt Nowzari <matt.nowzari@elastic.co>
2025-02-07 10:20:21 -0500

    [8.18] Adding warning filter to suppress GA warning from ES Python client (#3189) (#3192)

--- connectors/es/__init__.py
+++ connectors/es/__init__.py
@@ -3,7 +3,13 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
+import warnings
+
+from elasticsearch.exceptions import GeneralAvailabilityWarning
+
 from connectors.es.client import ESClient  # NOQA
 from connectors.es.document import ESDocument, InvalidDocumentSourceError  # NOQA
 from connectors.es.index import ESIndex  # NOQA
 from connectors.es.settings import DEFAULT_LANGUAGE, Mappings  # NOQA
+
+warnings.filterwarnings("ignore", category=GeneralAvailabilityWarning)
