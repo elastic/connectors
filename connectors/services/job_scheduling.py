935d35b060211c257bab57559d00779e7bad5668
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-04-03 10:18:58 +0200

    Update log message

--- connectors/services/job_scheduling.py
+++ connectors/services/job_scheduling.py
@@ -181,7 +181,7 @@ class JobSchedulingService(BaseService):
                             functools.partial(self._schedule, connector)
                         ):
                             connector.log_debug(
-                                f"{self.display_name.capitalize()} service is already running {self.max_concurrency} concurrent sync jobs and can't run more at this point. Increase '{self.max_concurrency_config}' in config if you want the service to run more concurrent sync jobs."  # pyright: ignore
+                                f"{self.display_name.capitalize()} service is already running {self.max_concurrency} concurrent scheduling jobs and can't run more at this point. Increase 'max_concurrent_scheduling_tasks' in config if you want the service to run more concurrent scheduling jobs."  # pyright: ignore
                             )
 
                 except Exception as e:
