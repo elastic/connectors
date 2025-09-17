ea44ceffc8e55227979b998257e3c56295b4299f
Lorena Balan <lorena.balan@elastic.co>
2025-09-17 16:52:33 +0200

    Fix CI

--- connectors/__init__.py
+++ connectors/__init__.py
@@ -3,5 +3,7 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
+import os
 
-__version__ = "9.2.0"
+with open(os.path.join(os.path.dirname(__file__), "VERSION")) as f:
+    __version__ = f.read().strip()
