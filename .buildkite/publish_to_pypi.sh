a6599618ea68715ce3414c5be560a32a1d0c8af8
Lorena Balan <lorena.balan@elastic.co>
2025-10-06 18:33:24 +0200

    Use script to publish to PyPIs

---
+++ .buildkite/publish_to_pypi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# !!! WARNING DO NOT add -x to avoid leaking vault passwords
+set -euo pipefail
+
+source .buildkite/shared.sh
+
+init_python
+cd "$PACKAGE_PATH"
+python -m pip install --upgrade build twine
+python -m build
+
+export TWINE_USERNAME="__token__"
+
+# upload to test or real PyPI based on TEST_PYPI=1 env var or arg
+if [[ "${1:-}" == "TEST_PYPI=1" ]] || [[ "${TEST_PYPI:-}" =~ ^(1|TRUE|true)$ ]]; then
+  TWINE_PASSWORD=$(vault read -field publishing-api-key secret/ent-search-team/test-pypi-ent-search-dev)
+  export TWINE_PASSWORD
+  python -m twine upload --repository testpypi dist/*
+else
+  TWINE_PASSWORD=$(vault read -field publishing-api-key secret/ent-search-team/pypi-ent-search-dev)
+  export TWINE_PASSWORD
+  python -m twine upload --repository pypi dist/*
+fi
