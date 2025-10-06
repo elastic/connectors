5d78c0a5b64810db08522deea71015eea5c6a976
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-10-06 15:25:20 +0000

    make autoformat

--- app/connectors_service/connectors/sources/directory/__init__.py
+++ app/connectors_service/connectors/sources/directory/__init__.py
@@ -3,6 +3,6 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
-from .datasource import DirectoryDataSource, DEFAULT_DIR
+from .datasource import DEFAULT_DIR, DirectoryDataSource
 
 __all__ = ["DirectoryDataSource", "DEFAULT_DIR"]
