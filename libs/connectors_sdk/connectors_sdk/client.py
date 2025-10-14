b64a4a8fb428fa38e2415f147be259695c792772
Lorena Balan <lorena.balan@elastic.co>
2025-10-14 15:55:33 +0200

    Client abstraction and copy retry logic to sdk

--- libs/connectors_sdk/connectors_sdk/client.py
+++ libs/connectors_sdk/connectors_sdk/client.py
@@ -5,11 +5,34 @@
 #
 import abc
 
+from connectors_sdk.retry import retryable
+
 
 class BaseClient(abc.ABC):
     def __init__(self, configuration):
         self.configuration = configuration
 
+    @abc.abstractmethod
     def ping(self):
         """Check the connection to the 3rd party data source."""
         raise NotImplementedError("ping method not implemented")
+
+    @abc.abstractmethod
+    def close(self):
+        """Close the client connection."""
+        raise NotImplementedError("close method not implemented")
+
+    @retryable
+    async def api_call(self, *args, **kwargs):
+        """Make an API call to the 3rd party data source."""
+        self._api_call(*args, **kwargs)
+
+    @abc.abstractmethod
+    def _api_call(self, *args, **kwargs):
+        raise NotImplementedError("_api_call method not implemented")
+
+    @abc.abstractmethod
+    def api_call_paginated(self, *args, **kwargs):
+        """Make a paginated API call to the 3rd party data source."""
+        raise NotImplementedError("api_call_paginated method not implemented")
+
