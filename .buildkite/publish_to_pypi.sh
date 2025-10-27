df3f4d68777a38a0d484661785d88aa529662cfa
Lorena Balan <lorena.balan@elastic.co>
2025-10-27 13:32:09 +0100

    Update Vault path

--- .buildkite/publish_to_pypi.sh
+++ .buildkite/publish_to_pypi.sh
@@ -14,11 +14,11 @@ export TWINE_USERNAME="__token__"
 
 # upload to test or real PyPI based on TEST_PYPI=1 env var or arg
 if [[ "${1:-}" == "TEST_PYPI=1" ]] || [[ "${TEST_PYPI:-}" =~ ^(1|TRUE|true)$ ]]; then
-  TWINE_PASSWORD=$(vault read -field publishing-api-key secret/ent-search-team/test-pypi-ent-search-dev)
+  TWINE_PASSWORD=$(vault read -field publishing-api-key secret/ci/elastic-connectors/test-pypi)
   export TWINE_PASSWORD
   python -m twine upload --repository testpypi dist/*
 else
-  TWINE_PASSWORD=$(vault read -field publishing-api-key secret/ent-search-team/pypi-ent-search-dev)
+  TWINE_PASSWORD=$(vault read -field publishing-api-key secret/ci/elastic-connectors/pypi)
   export TWINE_PASSWORD
   python -m twine upload --repository pypi dist/*
 fi
