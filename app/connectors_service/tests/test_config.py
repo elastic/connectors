35443d9c78a308bebc3d5872ce23e6eebd0737ee
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2026-07-14 17:04:14 +0200

    Drop _ent_search_config (#4165)
    Since Enterprise Search is deprecated this code is essentially a no-op
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    (cherry picked from commit 6f881eaffeb034d2b0ff81ab53780a8a2cf14104)

--- app/connectors_service/tests/test_config.py
+++ app/connectors_service/tests/test_config.py
@@ -5,7 +5,6 @@
 #
 
 import os
-from unittest import mock
 
 import pytest
 
@@ -15,10 +14,6 @@ HERE = os.path.dirname(__file__)
 FIXTURES_DIR = os.path.abspath(os.path.join(HERE, "fixtures"))
 
 CONFIG_FILE = os.path.join(FIXTURES_DIR, "config.yml")
-ES_CONFIG_FILE = os.path.join(FIXTURES_DIR, "entsearch.yml")
-ES_CONFIG_INVALID_LOG_LEVEL_FILE = os.path.join(
-    FIXTURES_DIR, "entsearch_invalid_log_level.yml"
-)
 
 
 def test_bad_config_file():
@@ -31,23 +26,6 @@ def test_config(set_env):
     assert isinstance(config, dict)
     assert config["elasticsearch"]["host"] == "http://nowhere.com:9200"
     assert config["elasticsearch"]["user"] == "elastic"
-
-
-def test_config_with_ent_search(set_env):
-    with mock.patch.dict(os.environ, {"ENT_SEARCH_CONFIG_PATH": ES_CONFIG_FILE}):
-        config = load_config(CONFIG_FILE)
-        assert config["elasticsearch"]["headers"]["X-Elastic-Auth"] == "SomeYeahValue"
-        assert config["service"]["log_level"] == "DEBUG"
-
-
-def test_config_with_invalid_log_level(set_env):
-    with mock.patch.dict(
-        os.environ, {"ENT_SEARCH_CONFIG_PATH": ES_CONFIG_INVALID_LOG_LEVEL_FILE}
-    ):
-        with pytest.raises(ValueError) as e:
-            _ = load_config(CONFIG_FILE)
-
-        assert e.match("Unexpected log level.*")
 
 
 def test_nest_config_when_nested_field_does_not_exist():
